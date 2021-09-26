4a6ed0601e89960b9382b0f0f53c0395f48ad3e5
aclandmark <mark.osborne15@btinternet.com>
2021-09-26 20:17:31 +0100

    Create Proj_1A1.ino

---
+++ User_projects/Favorites/Proj_1A1/Proj_1A1.ino
@@ -0,0 +1,27 @@
+
+#include "FPN_project.h"                              //Contains resources used by template and shared with programmer
+
+int main (void){ 
+ 
+unsigned int PORT_1, PORT_2;
+
+setup_328_HW;                                           //see "Resources\ATMEGA_Programmer
+
+sei();                                                  //needed for intensity control
+User_prompt_proj_1A1;
+//cli();
+
+while(1){
+PORT_1 = 1;
+PORT_2 = 0x8000;
+for(int m = 0; m <=15; m++){
+One_wire_Tx_2_integers(PORT_1, PORT_2);
+_delay_ms(30);
+PORT_1 = PORT_1 << 1;
+PORT_2 = PORT_2 >> 1;}}
+
+SW_reset;
+return 1;}
+
+
+ 
