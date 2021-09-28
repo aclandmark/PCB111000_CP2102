93ec8944e54e3f8daae7fe1622287931fb4fb52f
aclandmark <mark.osborne15@btinternet.com>
2021-09-28 16:08:19 +0100

    Create Proj_2C1_header.h

---
+++ User_projects/Favorites/Proj_2C1/Proj_2C1_header.h
@@ -0,0 +1,12 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+#include "Resources_2C1\Proj_2C1_header_file.h"
+#include "Resources_2C1\Proj_2C1_subroutines.c"
+#include "Resources_2C1\One_wire_transactions.c"
+#include "Resources_2C1\Basic_IO_and_Timer.c"
