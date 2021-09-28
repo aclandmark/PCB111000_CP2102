333a70b6edc89e0f7b035799e0ed84ed9f81f15b
aclandmark <mark.osborne15@btinternet.com>
2021-09-28 16:07:42 +0100

    Create Proj_1A1_header.h

---
+++ User_projects/Favorites/Proj_1A1/Proj_1A1_header.h
@@ -0,0 +1,11 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+#include "Resources_1A1\Proj_1A1_header_file.h"
+#include "Resources_1A1\One_wire_transactions.c"
+#include "Resources_1A1\Basic_IO_and_Timer.c"
