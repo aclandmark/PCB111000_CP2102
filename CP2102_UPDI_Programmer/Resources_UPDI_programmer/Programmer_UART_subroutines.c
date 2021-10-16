f3ff6bc707192beddf2d74dc3b34be86294d2a32
aclandmark <mark.osborne15@btinternet.com>
2021-10-16 20:04:09 +0100

    Update Programmer_UART_subroutines.c

--- CP2102_UPDI_Programmer/Resources_UPDI_programmer/Programmer_UART_subroutines.c
+++ CP2102_UPDI_Programmer/Resources_UPDI_programmer/Programmer_UART_subroutines.c
@@ -7,18 +7,18 @@
 /*******************************************************************************************************************************/
 void UART_Tx(unsigned int data_byte_T){               //starts Hi Z
   unsigned char parity = 0;
-  DDRB |= (1 << DDB3);                                //start bit
+  Prog_DD_Reg |= (1 << Prog_DD_bit);                                //start bit
   clock_delay_T;
 
 for (int n = 0; n <= 7; n++){
   if (data_byte_T & (1 << n))
-  {DDRB &= (~(1 << DDB3)); parity += 1;}
- else {DDRB |= (1 << DDB3);}clock_delay_T;}           //clock data out
+  {Prog_DD_Reg &= (~(1 << Prog_DD_bit)); parity += 1;}
+ else {Prog_DD_Reg |= (1 << Prog_DD_bit);}clock_delay_T;}           //clock data out
 
-if (parity%2){DDRB &= (~(1 << DD3));} 
-else {DDRB |= (1 << DDB3);}
+if (parity%2){Prog_DD_Reg &= (~(1 << Prog_DD_bit));} 
+else {Prog_DD_Reg |= (1 << Prog_DD_bit);}
   clock_delay_T;                                      //clock parity bit
-  DDRB &= (~(1 << DDB3));                             //initiate stop bit 
+  Prog_DD_Reg &= (~(1 << Prog_DD_bit));                             //initiate stop bit 
   clock_delay_T;                                      //Stop bits
   clock_delay_T;}
 
@@ -28,14 +28,14 @@ unsigned char UART_Rx(void){
   unsigned char data_byte_R = 0;
     char parity = 0;
                           
-  while (PINB & (1 << PINB3));                        //wait for start bit
+  while (Prog_IO_Port & (1 << Prog_pin));                        //wait for start bit
   clock_delay_R;                                      //The full delay is required 
   for (int n= 0; n <= 7; n++){clock_delay_R;
-    if (PINB & (1 << PINB3))
+    if (Prog_IO_Port & (1 << Prog_pin))
     {data_byte_R |= (1 << n); parity += 1;}}
     clock_delay_R;
 
- if ((PINB & (1 << PINB3)) && (parity%2)); 
+ if ((Prog_IO_Port & (1 << Prog_pin)) && (parity%2)); 
  else P_counter++;                                      //Parity often received in error?????
  
  clock_delay_R;                                         //stop bit
