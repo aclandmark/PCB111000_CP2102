ae55a3fa0372569ac47a1c2d3ad77d1d828d688b
aclandmark <mark.osborne15@btinternet.com>
2021-10-21 20:55:34 +0100

    Create Resources_shared.h

---
+++ CP2102_UPDI_Programmer/Resources_User_app/Resources_shared.h
@@ -0,0 +1,43 @@
+
+char User_response;
+
+
+/************************************************************************************************************************************/
+#define setup_328_HW \
+\
+setup_watchdog;\
+ADMUX |= (1 << REFS0);\
+initialise_IO;\
+\
+USART_init(0,68);
+
+
+
+/************************************************************************************************************************************/
+#define wdr()  __asm__ __volatile__("wdr")
+
+
+#define setup_watchdog \
+wdr();\
+MCUSR &= ~(1<<WDRF);\
+WDTCSR |= (1 <<WDCE) | (1<< WDE);\
+WDTCSR = 0;
+
+
+#define SW_reset {wdt_enable(WDTO_30MS);while(1);}
+
+
+/************************************************************************************************************************************/
+
+#define initialise_IO \
+MCUCR &= (~(1 << PUD));\
+DDRB = 0;\
+DDRC = 0;\
+DDRD = 0;\
+PORTB = 0xFF;\
+PORTC = 0xFF;\
+PORTD = 0xFF;\
+Prog_Port &= (~(1 << Prog_pin));
+
+//Prog pin stays as Hi Z input
+
