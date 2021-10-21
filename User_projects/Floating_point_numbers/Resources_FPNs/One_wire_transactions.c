5a1e76f7ffa2672023374087c609956f0d972d97
aclandmark <mark.osborne15@btinternet.com>
2021-10-21 20:54:02 +0100

    Update One_wire_transactions.c

--- User_projects/Floating_point_numbers/Resources_FPNs/One_wire_transactions.c
+++ User_projects/Floating_point_numbers/Resources_FPNs/One_wire_transactions.c
@@ -36,7 +36,7 @@ One_wire_Rx_char = 0;
 Tx_complete = 0;
 One_wire_mode = 1;									//Tx mode
   
-PCMSK1 |= 1 << PCINT12;				       			//set up Interrupt on pin change (IPC)
+PCMSK0 |= 1 << PCINT4;				       			//set up Interrupt on pin change (IPC)
 sei();												//to detect start pulse and initiate transmision
 while(!(Tx_complete));								//Wait here untill transmisson is complete 
 cli();}
@@ -49,7 +49,7 @@ One_wire_Rx_char = 0;
 Rx_complete = 0;
 One_wire_mode = 2;									//Rx mode
    
-PCMSK1 |= 1 << PCINT12;		       					//set up IPC to detect start pulse
+PCMSK0 |= 1 << PCINT4;		       					//set up IPC to detect start pulse
 sei();												//which intiates reception
 while (!(Rx_complete));
 cli();}
@@ -57,29 +57,29 @@ cli();}
 
 
 /*************************************************************************************************************/
-ISR(PCINT1_vect){                                   //Detect low on PINC4
-if(PINC4_down){										//If start bit:
+ISR(PCINT0_vect){                                   //Detect low on PINC4
+if(PINB4_down){										//If start bit:
 
-PCMSK1 &= (~(1 << PCINT12));                         //clear IPC
+PCMSK0 &= (~(1 << PCINT4));                         //clear IPC
 
 if(One_wire_mode == 1){								//Transmit character
 Start_clock_1;
 wait_for_half_comms_tick;							//Used to set the baud rate
 
-PORTC |= (1 << PORTC4);                               //WPU
+PORTB |= (1 << PORTB4);                               //WPU
 
 wait_for_comms_tick;
 
-DDRC |= (1 << DDC4);                                  //OH
+DDRB |= (1 << DDB4);                                  //OH
 wait_for_comms_tick;
  for(int m = 0; m <= 7; m++){                        //Eight data bits
     if (One_wire_Tx_char & (1 << (7-m)))
-    PORTC |= (1 << PORTC4);                          //OH
-    else PORTC &= ~(1 << PORTC4);                    //OL
+    PORTB |= (1 << PORTB4);                          //OH
+    else PORTB &= ~(1 << PORTB4);                    //OL
    wait_for_comms_tick;}
 
-DDRC &= ~(1 << PINC4);
-PORTC &= ~(1 << PORTC4);                             //Restore Tri
+DDRB &= ~(1 << PINB4);
+PORTB &= ~(1 << PORTB4);                             //Restore Tri
 Tx_complete = 1;
 TCCR0B = 0;}
 
@@ -91,16 +91,45 @@ for (int m = 0; m <= 7;m++){
   wait_for_comms_tick;
   
 One_wire_Rx_char = One_wire_Rx_char << 1;			//Assemble bits as they are received
-if (PINC & (1 << PINC4))
+if (PINB & (1 << PINB4))
 {One_wire_Rx_char |= 1;}}
 TCCR0B = 0;
 Rx_complete = 1;}}
 
 
-if(PINC3_down){										//The interrupt is due to a low on PINC3					
+/*if(PINC3_down){										//The interrupt is due to a low on PINC3					
 _delay_ms(100);										//Swich bounce delay
 while(PINC3_down);									//Wait for switch to be released
 _delay_ms(100);
-One_wire_Tx_char = 'G'; UART_Tx_1_wire();}}			//Intensity command: This cannot be issued when PINC3 is low.
+One_wire_Tx_char = 'G'; UART_Tx_1_wire();}*/
+
+}			//Intensity command: This cannot be issued when PINC3 is low.
 	
+
+/*ISR(PCINT1_vect){ 
+if(PINC5_down){
+_delay_ms(100);										//Switch bounce delay
+while(PINC5_down);									//Wait for switch to be released
+_delay_ms(100);
+One_wire_Tx_char = 'G'; UART_Tx_1_wire();}}*/
+
+ISR(PCINT1_vect){ 
+if(PINC5_down){
+_delay_ms(250);										//Switch bounce delay
+if(PINC5_up){setRunBL_bit; SW_reset;}
+LED_1_on; LED_2_on;
+
+
+_delay_ms(250);
+
+if(PINC5_up)
+{Reset_ATtiny1606;//One_rwire_Tx_char = 'F'; UART_Tx_1_wire();
+SW_reset;}
+_delay_ms(250);
+
+
+if(PINC5_down)								//Wait for switch to be released
+//_delay_ms(500);
+{One_wire_Tx_char = 'G'; UART_Tx_1_wire();
+SW_reset;}}}
 
