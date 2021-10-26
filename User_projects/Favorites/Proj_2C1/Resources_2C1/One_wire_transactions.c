2a67ef1d5c24196b235e4cde3bd8b17047f9c4ab
aclandmark <mark.osborne15@btinternet.com>
2021-10-26 21:01:48 +0100

    Update One_wire_transactions.c

--- User_projects/Favorites/Proj_2C1/Resources_2C1/One_wire_transactions.c
+++ User_projects/Favorites/Proj_2C1/Resources_2C1/One_wire_transactions.c
@@ -4,15 +4,6 @@ void sendChar(char);
 void sendString(const char*);
 
 
-void I2C_Tx_any_segment(char letter, char digit_num){
-One_wire_Tx_char = 'b';  UART_Tx_1_wire();
-One_wire_Tx_char = letter;  UART_Tx_1_wire(); 
-One_wire_Tx_char = digit_num;  UART_Tx_1_wire();}
-
-
-
-
-
 
 /*************************************************************************************************************/
 void UART_Tx_1_wire(void){
@@ -21,9 +12,7 @@ Tx_complete = 0;
 One_wire_mode = 1;									//Tx mode
   
 PCMSK0 |= 1 << PCINT4;				       			//set up Interrupt on pin change (IPC)
-sei();												//to detect start pulse and initiate transmision
-while(!(Tx_complete));								//Wait here untill transmisson is complete 
-cli();}
+while(!(Tx_complete));}								//Wait here untill transmisson is complete 
 
 
 
@@ -34,14 +23,13 @@ Rx_complete = 0;
 One_wire_mode = 2;									//Rx mode
    
 PCMSK0 |= 1 << PCINT4;		       					//set up IPC to detect start pulse
-sei();												//which intiates reception
-while (!(Rx_complete));
-cli();}
+while (!(Rx_complete));}
 
 
 
 /*************************************************************************************************************/
 ISR(PCINT0_vect){                                   //Detect low on PINC4
+
 if(PINB4_down){										//If start bit:
 
 PCMSK0 &= (~(1 << PCINT4));                         //clear IPC
@@ -54,7 +42,7 @@ PORTB |= (1 << PORTB4);                               //WPU
 
 wait_for_comms_tick;
 
-DDRB |= (1 << DDB4);                                //OH
+DDRB |= (1 << DDB4);                                 //OH
 wait_for_comms_tick;
  for(int m = 0; m <= 7; m++){                        //Eight data bits
     if (One_wire_Tx_char & (1 << (7-m)))
@@ -78,15 +66,30 @@ One_wire_Rx_char = One_wire_Rx_char << 1;			//Assemble bits as they are received
 if (PINB & (1 << PINB4))
 {One_wire_Rx_char |= 1;}}
 TCCR0B = 0;
-Rx_complete = 1;}}
+Rx_complete = 1;}}}
 
-
-/*if(PINC3_down){										//The interrupt is due to a low on PINC3					
-_delay_ms(100);										//Swich bounce delay
-while(PINC3_down);									//Wait for switch to be released
-_delay_ms(100);
-One_wire_Tx_char = 'G'; UART_Tx_1_wire();}*/
-
-}			//Intensity command: This cannot be issued when PINC3 is low.
 	
+	
+	
+	
+	
+/*************************************************************************************************************/
+ISR(PCINT1_vect){ 									//Interogates vertical switch presses
+if(PINC5_down){
+_delay_ms(25);
+if(PINC5_up)return;
+_delay_ms(225);										//Switch bounce delay
+if(PINC5_up)
+{setRunBL_bit; }
+else{
+LED_1_on; LED_2_on;
+_delay_ms(250);
+if(PINC5_up);
+if(PINC5_down)											//Wait for switch to be released
+{sei();One_wire_Tx_char = 'G'; UART_Tx_1_wire(); }}
+SW_reset;}}
+
+
+
+
 
