18af738cbd1c5f9904cfb868fb6561d97e5860b8
aclandmark <mark.osborne15@btinternet.com>
2021-10-27 09:18:25 +0100

    Update One_wire_transactions.c

--- User_projects/Floating_point_numbers/Resources_FPNs/One_wire_transactions.c
+++ User_projects/Floating_point_numbers/Resources_FPNs/One_wire_transactions.c
@@ -4,30 +4,7 @@ void sendChar(char);
 void sendString(const char*);
 
 
-/*************************************************************************************************************/
-void send_int_num(long num){
-One_wire_Tx_char = 'C'; 								//Command 'C' indicates the a long number will be sent
-UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){
-One_wire_Tx_char = num >> (8 * (3 - m)); 				//Split the number into 4 chars
-UART_Tx_1_wire();}}										//and send them individually
 
-
-
-
-/*************************************************************************************************************/
-void send_float_num(float FP_num){
-char * Char_ptr;
-Char_ptr = (char*)&FP_num;
-	
-One_wire_Tx_char = 'D'; 								//Command 'D' indicates the a floating point number will be sent
-UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){							//Split the number into 4 chars
-One_wire_Tx_char = *Char_ptr;							//and send them individually
-UART_Tx_1_wire();	
-Char_ptr += 1;}}
-
-	
 
 
 /*************************************************************************************************************/
@@ -37,9 +14,7 @@ Tx_complete = 0;
 One_wire_mode = 1;									//Tx mode
   
 PCMSK0 |= 1 << PCINT4;				       			//set up Interrupt on pin change (IPC)
-sei();												//to detect start pulse and initiate transmision
-while(!(Tx_complete));								//Wait here untill transmisson is complete 
-cli();}
+while(!(Tx_complete));}								//Wait here untill transmisson is complete 
 
 
 
@@ -50,14 +25,13 @@ Rx_complete = 0;
 One_wire_mode = 2;									//Rx mode
    
 PCMSK0 |= 1 << PCINT4;		       					//set up IPC to detect start pulse
-sei();												//which intiates reception
-while (!(Rx_complete));
-cli();}
+while (!(Rx_complete));}
 
 
 
 /*************************************************************************************************************/
 ISR(PCINT0_vect){                                   //Detect low on PINC4
+
 if(PINB4_down){										//If start bit:
 
 PCMSK0 &= (~(1 << PCINT4));                         //clear IPC
@@ -70,7 +44,7 @@ PORTB |= (1 << PORTB4);                               //WPU
 
 wait_for_comms_tick;
 
-DDRB |= (1 << DDB4);                                  //OH
+DDRB |= (1 << DDB4);                                 //OH
 wait_for_comms_tick;
  for(int m = 0; m <= 7; m++){                        //Eight data bits
     if (One_wire_Tx_char & (1 << (7-m)))
@@ -94,42 +68,30 @@ One_wire_Rx_char = One_wire_Rx_char << 1;			//Assemble bits as they are received
 if (PINB & (1 << PINB4))
 {One_wire_Rx_char |= 1;}}
 TCCR0B = 0;
-Rx_complete = 1;}}
+Rx_complete = 1;}}}
 
-
-/*if(PINC3_down){										//The interrupt is due to a low on PINC3					
-_delay_ms(100);										//Swich bounce delay
-while(PINC3_down);									//Wait for switch to be released
-_delay_ms(100);
-One_wire_Tx_char = 'G'; UART_Tx_1_wire();}*/
-
-}			//Intensity command: This cannot be issued when PINC3 is low.
 	
-
-/*ISR(PCINT1_vect){ 
+	
+	
+	
+	
+/*************************************************************************************************************/
+ISR(PCINT1_vect){ 									//Interogates vertical switch presses
 if(PINC5_down){
-_delay_ms(100);										//Switch bounce delay
-while(PINC5_down);									//Wait for switch to be released
-_delay_ms(100);
-One_wire_Tx_char = 'G'; UART_Tx_1_wire();}}*/
-
-ISR(PCINT1_vect){ 
-if(PINC5_down){
-_delay_ms(250);										//Switch bounce delay
-if(PINC5_up){setRunBL_bit; SW_reset;}
-LED_1_on; LED_2_on;
-
-
-_delay_ms(250);
-
+_delay_ms(25);
+if(PINC5_up)return;
+_delay_ms(225);										//Switch bounce delay
 if(PINC5_up)
-{Reset_ATtiny1606;//One_rwire_Tx_char = 'F'; UART_Tx_1_wire();
-SW_reset;}
+{setRunBL_bit; }
+else{
+LED_1_on; LED_2_on;
 _delay_ms(250);
+if(PINC5_up);
+if(PINC5_down)											//Wait for switch to be released
+{sei();One_wire_Tx_char = 'G'; UART_Tx_1_wire(); }}
+SW_reset;}}
 
 
-if(PINC5_down)								//Wait for switch to be released
-//_delay_ms(500);
-{One_wire_Tx_char = 'G'; UART_Tx_1_wire();
-SW_reset;}}}
+
+
 
