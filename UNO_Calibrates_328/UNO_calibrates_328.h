d3af9430d2f9970c2d5c8d8330ea7e45c547c1b6
aclandmark <mark.osborne15@btinternet.com>
2021-11-11 18:01:47 +0000

    Create UNO_calibrates_328.h

---
+++ UNO_Calibrates_328/UNO_calibrates_328.h
@@ -0,0 +1,55 @@
+
+
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+char isCharavailable (int);
+char receiveChar(void);
+char measure_cal_error(unsigned char, unsigned char, long *);
+void num_to_PC(long);
+void print_cal_result(unsigned char, long, char);
+
+#define wdr()  __asm__ __volatile__("wdr")
+
+
+
+/**********************************************************************************/
+ #define setup_328_HW \
+setup_watchdog;\
+ADMUX |= (1 << REFS0);\
+initialise_IO;\
+Serial.begin(76800);\
+while (!Serial);
+
+
+
+/**********************************************************************************/
+#define setup_watchdog \
+wdr();\
+MCUSR &= ~(1<<WDRF);\
+WDTCSR |= (1 <<WDCE) | (1<< WDE);\
+WDTCSR = 0;
+
+
+
+/**********************************************************************************/
+#define initialise_IO \
+MCUCR &= (~(1 << PUD));\
+DDRB = 0;\
+DDRC = 0;\
+DDRD = 0;\
+PORTB = 0xFF;\
+PORTC = 0xFF;\
+PORTD = 0xFF;
+
+
+
+/**********************************************************************************/
+#define  OSC_CAL \
+if ((eeprom_read_byte((uint8_t*)0x3FF) > 0x0F)\
+&&  (eeprom_read_byte((uint8_t*)0x3FF) < 0xF0) && (eeprom_read_byte((uint8_t*)0x3FF)\
+== eeprom_read_byte((uint8_t*)0x3FE))) {OSCCAL = eeprom_read_byte((uint8_t*)0x3FE);}
