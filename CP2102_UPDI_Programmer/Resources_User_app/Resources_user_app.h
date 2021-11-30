5342a1342b47a9b3445e073b5f1a413d8d222653
aclandmark <mark.osborne15@btinternet.com>
2021-11-30 15:43:08 +0000

    Update Resources_user_app.h

--- CP2102_UPDI_Programmer/Resources_User_app/Resources_user_app.h
+++ CP2102_UPDI_Programmer/Resources_User_app/Resources_user_app.h
@@ -4,27 +4,31 @@ volatile char One_wire_Rx_char, One_wire_Tx_char;
 volatile char Tx_complete, Rx_complete;
 char display_bkp[7];
 
+int Comms_clock;
+
+/**********************************************************************************/
+#define  comms_cal \
+if (((signed char)eeprom_read_byte((uint8_t*)0x3F6) > -50)\
+&&  ((signed char)eeprom_read_byte((uint8_t*)0x3F6) < 50) && ((signed char)eeprom_read_byte((uint8_t*)0x3F6)\
+== (signed char)eeprom_read_byte((uint8_t*)0x3F7)))\
+{Comms_clock = 200 + (signed char) eeprom_read_byte((uint8_t*)0x3F6);}
+
 
 #define set_up_pin_change_interrupt  		PCICR |= (1 << PCIE1); PCMSK1 |= (1 << PCINT13); 
-#define set_up_one_way_comms 				PCICR |= (1 << PCIE0); PORTB &= (~(1 << PORTB4));  
+#define set_up_one_wire_comms 				PCICR |= (1 << PCIE0); PORTB &= (~(1 << PORTB4));  
 #define Reset_ATtiny1606					One_wire_Tx_char = 'F'; UART_Tx_1_wire();
 
 
-#define Tx_clock_1     		200			//5K Baud rate	
-#define Rx_clock_1     		200		
-#define Start_clock_1    	TCNT0 = 0;  TCCR0B = (1 << CS01);
-#define Half_Rx_clock_1 	100
-			
+#define Start_clock		    	TCNT0 = 0;  OCR0A = 0; TCCR0B = (1 << CS01);
 
 
 #define wait_for_comms_tick \
-OCR0A +=  Rx_clock_1;\
+OCR0A +=  Comms_clock;\
 while (!(TIFR0 & (1 << OCF0A)));\
 TIFR0 = 0xFF;
 
-
 #define wait_for_half_comms_tick \
-OCR0A =  Half_Rx_clock_1;\
+OCR0A =  Comms_clock/2;\
 while (!(TIFR0 & (1 << OCF0A)));\
 TIFR0 = 0xFF;  
 
@@ -35,7 +39,7 @@ TIFR0 = 0xFF;
 #define PINC5_up	(PINC & 0x20)
 
 
-
+/*
 #define  set_up_activity_leds \
 DDRB |= (1 << DDB0) | (1 << DDB1);\
 LED_1_off;\
@@ -46,7 +50,7 @@ LED_2_off;
 
 #define LED_2_off	 PORTB &= (~(1 << PB0));
 #define LED_2_on	 PORTB |= (1 << PB0);
-
+*/
 
 
 
