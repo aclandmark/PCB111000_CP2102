61cad9a5959fbf58a373304eeeab722ae1a865b5
aclandmark <mark.osborne15@btinternet.com>
2021-11-30 15:42:21 +0000

    Update Project_subroutines.c

--- CP2102_UPDI_Programmer/Resources_User_app/Project_subroutines.c
+++ CP2102_UPDI_Programmer/Resources_User_app/Project_subroutines.c
@@ -11,7 +11,7 @@ display_bkp[segment - 'a'] = display_bkp[segment - 'a'] ^ (1 << digit_num);}
 
 
 /*************************************************************************************************************/
-unsigned int PRN_16bit_GEN(unsigned int start){                  //Google LFSR
+/*unsigned int PRN_16bit_GEN(unsigned int start){                  //Google LFSR
 unsigned int bit, lfsr;
 
 if(!(start)) lfsr = (eeprom_read_byte((uint8_t*)(0x1FB)) << 8) + eeprom_read_byte((uint8_t*)(0x1FA));
@@ -22,5 +22,18 @@ if(!(start)){
 eeprom_write_byte((uint8_t*)(0x1FB),(lfsr>>8));
 eeprom_write_byte((uint8_t*)(0x1FA),lfsr);}
 
-return lfsr;}
+return lfsr;}*/
  
+ 
+unsigned int PRN_16bit_GEN(unsigned int start){																//Google LFSR
+unsigned int bit, lfsr;
+
+if(!(start)) lfsr = (eeprom_read_byte((uint8_t*)(0x3F2)) << 8) + eeprom_read_byte((uint8_t*)(0x3F3));
+else lfsr = start;
+bit = (( lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
+lfsr = (lfsr >> 1) | (bit << 15);
+if(!(start)){
+eeprom_write_byte((uint8_t*)(0x3F2),(lfsr>>8));
+eeprom_write_byte((uint8_t*)(0x3F3),lfsr);}
+
+return lfsr;}
