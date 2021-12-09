1a0f4438969802455191ce854c533b158bcd9eab
aclandmark <mark.osborne15@btinternet.com>
2021-12-09 21:43:06 +0000

    Update Floating_point_numbers.ino

--- User_projects/Floating_point_numbers/Floating_point_numbers.ino
+++ User_projects/Floating_point_numbers/Floating_point_numbers.ino
@@ -1,67 +1,36 @@
 
+/*
+User enters floating point numbers at keyboard
+They are sent to the 7 segment display and PC
+and used for some simple arithmetic  
+*/
+
+
+
+
 #include "FPN_project.h"                              //Contains resources used by template and shared with programmer
 
 
-
 int main (void){ 
-  
-char keypress;
-long int_num;
+ 
 float FPN_1, FPN_2;
-char FP_string[12];
 char op;                                                //+, -, *, /, pow
-char test;
-
 
 setup_328_HW;                                           //see "Resources\ATMEGA_Programmer
-
-
 while(1){
-
-sei();                                                    //Required to control intensity
-  
+sei();                                                  //Reads switch and changes intensity
 User_prompt_template;
 
-if (User_response =='i')
+if (User_response =='f'){clear_display;
+sendString("Enter FPN from KBD \
+(terminate in cr)\r\n"); 
+FPN_1 =  Float_from_KBD();                              //Sends string to display and returns binary
 
-/*{sendString("Enter integer from KBD (terminate in cr)\r\n");
-
-while (int_num = Int_from_KBD()){
-sendString("AK to do arithmetic\r\n");
-waitforkeypress();
-Long_Num_to_UNO = Long_Num_from_UNO * 2;                //Simple arithmetic to check operation of binary/string conversions
-send_int_num(Long_Num_to_UNO);
-sendString("New number?\r\n");}}*/
-
-
-{
-  
-  test = (eeprom_read_byte((uint8_t*)0x3F0));
-
-  for(int m = 0; m <= 7; m++){
-    if (test & (1 << (7 - m)))sendChar('1'); else sendChar('0');
-    }
-
-
-
-  
-  while(1){
-int_num = 10;
-while(int_num < 99999999){
-send_int_num(int_num);
-_delay_ms(10);  
- int_num = int_num*10/9;}}}
-
-
-if (User_response =='f'){ clear_display;
- sendString("Enter FPN from KBD (terminate in cr)\r\n"); 
- FPN_1 =  Float_from_KBD();
- for(int m = 0; m<=100; m++)                                                  
-  {if (!m)sendString("\r\nEnter zero to exit \
+for(int m = 0; m<=100; m++)                                                  
+{if (!m){sendString("\r\nEnter zero to exit \
 or +,-,*,/ or ^ followed by new number.\r\n");
-
+sendFloat(FPN_1);sendString("\r\n");}                   //Send number to PC
 op = waitforkeypress();
-
 if(op == '0')break;
 FPN_2 = Float_from_KBD();
 
@@ -73,13 +42,33 @@ switch (op){                                            //Do some arithmetic
   case '^': FPN_1 = pow(FPN_1, FPN_2);break;
   default: break;}
 
-send_float_num(FPN_1);
-sei();                                                  //Required for intensity control
+check_for_overflow_underflow(FPN_1);                      //Reset if yes
+send_float_num(FPN_1);                                    //Display result
+sendFloat(FPN_1);sendString("\r\n");                      //Send result to PC
+
 }}}
-
-
-SW_reset;
 return 1;}
 
 
- 
+
+
+/****************************************************************************************/
+void check_for_overflow_underflow(float FPN){
+unsigned long *long_ptr;
+long_ptr = (unsigned long *) &FPN;
+if((*long_ptr == 0X0) || (*long_ptr == 0X80000000))
+{sendString("Zero\r\n");Reset_ATtiny1606;SW_reset;}
+if((*long_ptr == 0x7F800000) || (*long_ptr == 0xFF800000))
+{sendString("Infinity\r\n");Reset_ATtiny1606;SW_reset;}}
+
+
+
+/****************************************************************************************/
+void sendFloat(float FPN){
+ftoaL(FPN);
+if(sign == '-') sendChar(sign);
+if (print_buffer[0] == '.')sendChar('0');
+sendString(print_buffer);
+if(expt){sendChar('E');
+itoa (expt, expt_string, 10 );
+sendString(expt_string);}}
