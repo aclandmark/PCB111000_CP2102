259a53cb07421db5efe9e5977d5f7e65ffd39648
aclandmark <mark.osborne15@btinternet.com>
2021-12-18 20:52:44 +0000

    Update Proj_1A1_header.h

--- User_projects/Favorites/Proj_1A1/Proj_1A1_header.h
+++ User_projects/Favorites/Proj_1A1/Proj_1A1_header.h
@@ -4,6 +4,7 @@
 #include <avr/wdt.h>
 
 #include "Resources_1A1\Proj_1A1_header_file.h"
+#include "Resources_1A1\One_wire_header.h"
 #include "Resources_1A1\One_wire_transactions.c"
 #include "Resources_1A1\Basic_IO_and_Timer.c"
 #include "Resources_1A1\Proj_1A1_subroutines.c"
