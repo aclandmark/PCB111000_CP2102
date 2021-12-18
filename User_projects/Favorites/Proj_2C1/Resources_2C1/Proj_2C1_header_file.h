5fce3286765b64a5db0a09802cf4119f8969c229
aclandmark <mark.osborne15@btinternet.com>
2021-12-18 14:45:36 +0000

    Update Proj_2C1_header_file.h

--- User_projects/Favorites/Proj_2C1/Resources_2C1/Proj_2C1_header_file.h
+++ User_projects/Favorites/Proj_2C1/Resources_2C1/Proj_2C1_header_file.h
@@ -2,10 +2,7 @@
 
 void UART_Tx_1_wire(void);
 void UART_Rx_1_wire(void);
-//void One_wire_Tx_2_integers(unsigned int, unsigned int);
 void I2C_Tx_any_segment(char, char);
-
-
 
 char User_response;
 
@@ -14,12 +11,8 @@ volatile int One_wire_Rx_int;
 volatile char Tx_complete, Rx_complete;
 
 volatile char One_wire_mode;
-
-
 char display_bkp[7];
-
 int Comms_clock;
-
 
 
 /**********************************************************************************/
@@ -37,7 +30,6 @@ if (((signed char)eeprom_read_byte((uint8_t*)0x3F6) > -50)\
 {Comms_clock = 200 + (signed char) eeprom_read_byte((uint8_t*)0x3F6);}
 
 
-
 /************************************************************************************************************************************/
 #define setup_328_HW \
 \
@@ -52,9 +44,6 @@ USART_init(0,16);\
 setup_one_wire_comms;\
 set_up_activity_leds;\
 sei();
-
-
-
 
 
 /************************************************************************************************************************************/
@@ -89,13 +78,11 @@ User_response = receiveChar();\
 if(User_response == 'r') break;} sendString("\r\n");
 
 
-
-
-
 /************************************************************************************************************************************/
 #define setup_one_wire_comms \
 PCICR |= (1 << PCIE0); PCMSK0 |= (1 << PCINT4);\
 PORTB &= (~(1 << PORTB4));
+
 
 #define clear_display 						One_wire_Tx_char = 'c';  UART_Tx_1_wire();
 #define set_up_pin_change_interrupt  		PCICR |= (1 << PCIE1); PCMSK1 |= (1 << PCINT13); 
@@ -106,12 +93,8 @@ PORTB &= (~(1 << PORTB4));
 One_wire_Tx_char = 'F'; UART_Tx_1_wire();
 
 
-
-
-
 /************************************************************************************************************************************/
 #define Start_clock		    	TCNT0 = 0;  OCR0A = 0; TCCR0B = (1 << CS01);
-
 
 #define wait_for_comms_tick \
 OCR0A +=  Comms_clock;\
@@ -130,7 +113,7 @@ TIFR0 = 0xFF;
 #define PINC5_down	((PINC & 0x20)^0x20)
 #define PINC5_up	(PINC & 0x20)
 
-
+/************************************************************************************************************************************/
 #define  set_up_activity_leds \
 DDRB |= (1 << DDB0) | (1 << DDB1);\
 LED_1_off;\
