0dd0667e1ebf71ad6fcf9754b0e2a92200b1676b
aclandmark <mark.osborne15@btinternet.com>
2021-12-20 18:25:42 +0000

    Create Project_header.h

---
+++ AVR_freaks_first_posting/Firmware/UNO_Calibrates_328/Project_header.h
@@ -0,0 +1,11 @@
+
+
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+#include "Resources\UNO_Calibrates_328.h"
+#include "Resources\ASKII_subroutines.c"
