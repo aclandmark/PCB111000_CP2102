ab43b102f02c74106a4992c69ba9d8fdc2270604
aclandmark <mark.osborne15@btinternet.com>
2021-12-20 18:02:58 +0000

    Update Hex_verification.c

--- CP2102_bootloader/3_Hex_verification/Hex_verification/Hex_verification.c
+++ CP2102_bootloader/3_Hex_verification/Hex_verification/Hex_verification.c
@@ -54,14 +54,12 @@ return 1;}
 
 
 /***************************************************************************************************************************************************/
-
 void Verify_Flash (void){
-
 	int  line_counter = 0, print_line = 0;								//Controls printing of hex file
 	int line_no;														//Refers to the .hex file
 	signed int phys_address;											//Address in flash memory
 	signed int prog_counter_mem;										//Initialised with size of .hex file used for programming
-	char print_out_mode = 0;											//Print out flash contents as hex or askii characters
+	//char print_out_mode = 0;											//Print out flash contents as hex or askii characters
 	char skip_lines[4];													//Enter number to limit the print out
 
 	phys_address = 0;  read_ops=0;
@@ -78,62 +76,39 @@ void Verify_Flash (void){
 	print_line = askiX2_to_hex(skip_lines);
 	Hex_to_PC (print_line); sendString("   ");
 
-
-	if (print_line == 0); 												//hex file print out not required
-	else {sendString("1/2?\r\n");										//else -1- sends file as askii, -2- sends it as hex
-		print_out_mode =  waitforkeypress(); binUnwantedChars();
-	}
-
-
-
 	while(1){if(!(prog_counter_mem))break;								//print out loop starts here, exit when finished
-		
-		while(1) {															//Start reading the flash memory searching for the next hex command
-
+		while(1){															//Start reading the flash memory searching for the next hex command
 			Hex_cmd = readCMD(phys_address);
-
 			phys_address++;
 			if (phys_address == FlashSZ)break;									//No more memory? Quit if yes
 			if (Hex_cmd != 0xFFFF) break;										//If the hex command is 0xFFFF remain in this loop otherwise exit.
 		LED_1_on;}
-
 		LED_1_off;
-		if (phys_address == FlashSZ)break;									//Exit when there is no more flash to read
-
-		if ((print_line == 0)  && (!(line_no%10)))						//Print out of hex file not required
-		{sendChar('*');Timer_T0_sub(T0_delay_20ms);}																	//Print a -*- every tenth line of the file
+		if (print_line == 0){
+		if(!(line_no%10))sendChar('*');	
+		if(!(line_no & 0b00010000))toggle_led_1;	
+		Timer_T0_sub(T0_delay_5ms);}
+				
 		if(print_line && (!(line_no%print_line)))							//Print out required: Print all lines or just a selection
 		{newline(); Hex_to_PC ((phys_address-1)*2);
 			sendString("   "); line_counter++;
-			if(print_out_mode == '1'){send_as_askii;} 							//Start with the address of the first command in the line
-		else Hex_to_PC (Hex_cmd);}											//Print first command in askii or hex
+			 Hex_to_PC (Hex_cmd); 								//Print first command in askii or hex
+		if(!(line_no & 0b00100000))toggle_led_1;}
+		
 		read_ops++;															//Value to be sent to PC for comparison with the hex filer size
 		prog_counter_mem--;													//"prog_counter_mem" decrements to zero when the end of the file is reached
-
-
 		for(int m=0; m<7; m++){    										//Read the next seven locations in the flash memory
-
 			Hex_cmd = readCMD(phys_address);
-
-
 			phys_address++;
 			if(Hex_cmd == 0xFFFF)break;											//Read 0xFFFF: return to start of print out loop
 			prog_counter_mem--;
-
-			if(phys_address & 0x0040) {LED_1_on;	} else {LED_1_off;}
-
 			if(print_line && (!(line_no%print_line)))
 			{Timer_T0_sub(T0_delay_5ms);										//5ms delay prevents PC from getting overwhelmed with characters
-				if(print_out_mode == '1'){send_as_askii;}
-			else Hex_to_PC (Hex_cmd);}
+			 Hex_to_PC (Hex_cmd);}
 			read_ops++;
-
 		if(phys_address==FlashSZ)break;}
 		if ((print_line != 0) && (!(line_no%print_line)) && (!(line_counter%8)))sendString("\r\n");
-
 		line_no++;
 	if (phys_address == FlashSZ)break;}
-
 	LED_1_off;
-
 newline(); }
