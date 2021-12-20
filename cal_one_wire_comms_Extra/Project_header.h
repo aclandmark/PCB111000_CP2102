9d33319405947a74f2f5d8899cd59a756fffe6a0
aclandmark <mark.osborne15@btinternet.com>
2021-12-20 20:38:46 +0000

    Delete Project_header.h

--- cal_one_wire_comms_Extra/Project_header.h
+++
@@ -1,7 +0,0 @@
-
-
-
-#include <avr/wdt.h>
-#include "cal_one_wire_Resources/cal_one_wire_Extra.h"
-#include "cal_one_wire_Resources/ASKII_subroutines.c"
-#include "cal_one_wire_Resources/One_wire_transactions.c"
