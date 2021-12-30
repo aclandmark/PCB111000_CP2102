e207b86f920ae06ca8718f71ea330b70f3dad2ad
aclandmark <mark.osborne15@btinternet.com>
2021-12-30 17:39:51 +0000

    Update Project_header.h

--- AVR_freaks_first_posting/Firmware/Recal_328_using_crystal/Project_header.h
+++ AVR_freaks_first_posting/Firmware/Recal_328_using_crystal/Project_header.h
@@ -7,5 +7,9 @@
 #include <avr/wdt.h>
 #include <avr/eeprom.h>
 
+#define fast 2
+#define slow 3
+char cal_speed = fast; 
+
 #include "Resources\Recal_328.h"
 #include "Resources\ASKII_subroutines.c"
