0b2a31cce0743da2229b94e5eac96ef2402ab154
aclandmark <mark.osborne15@btinternet.com>
2022-01-10 15:50:19 +0000

    Update A_first_project.ino

--- AVR_freaks_second_posting/Projects/A_first_project/A_first_project.ino
+++ AVR_freaks_second_posting/Projects/A_first_project/A_first_project.ino
@@ -109,8 +109,8 @@
 
 
 
-******************Example 6*********************************************/
-int main (void) {
+******************Example 6*********************************************
+  int main (void) {
   unsigned int PORT_1, PORT_2, m = 0, n = 0;
 
   setup_328_HW;
@@ -126,31 +126,36 @@ int main (void) {
     if (m == 0)n = 0;
   }
   return 1;
+  }
+
+
+
+
+******************Example 7*********************************************/
+int main (void) {
+  unsigned int PORT_1 = 1;
+  char m = 1;
+  char overflow = 0;
+
+  setup_328_HW;
+  while (1) {
+    One_wire_Tx_2_integers(PORT_1, ~PORT_1);
+    _delay_ms(60);
+    if (m <= 5) {
+      PORT_1 |= (PORT_1 << 1);
+      m += 1;
+    }
+    else PORT_1 = PORT_1 << 1;
+    if (overflow)PORT_1 |= 1;
+    if (PORT_1 & 0x8000) overflow = 1;
+    else overflow = 0;
+  }
 }
 
 
 
 
-/******************Example 7*********************************************
-  int main (void){
-  unsigned int PORT_1=1;
-  char m=1;
-  char overflow=0;
-
-  setup_328_HW;
-  while(1){
-  One_wire_Tx_2_integers(PORT_1, ~PORT_1);
-  _delay_ms(60);
-  if (m<=5){PORT_1 |= (PORT_1 << 1);m += 1;}
-  else PORT_1 = PORT_1 << 1;
-  if(overflow)PORT_1 |= 1;
-  if (PORT_1 & 0x8000) overflow=1;
-  else overflow = 0;}}
-
-
-
-
-*****************Example 8**********************************************
+/*****************Example 8**********************************************
   int main (void) {
   unsigned int random_num;
   long PORT_1 = 1, PORT_2 = 1;
