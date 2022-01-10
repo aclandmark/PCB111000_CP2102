a41649b78cff31a7b5cc0a9cc9c989d3037412c6
aclandmark <mark.osborne15@btinternet.com>
2022-01-10 15:44:17 +0000

    Update A_first_project.ino

--- AVR_freaks_second_posting/Projects/A_first_project/A_first_project.ino
+++ AVR_freaks_second_posting/Projects/A_first_project/A_first_project.ino
@@ -4,8 +4,8 @@
 
 
 
-/*****************Example 1**********************************************/
-int main (void) {
+/*****************Example 1**********************************************
+  int main (void) {
   unsigned int PORT_1;
 
   setup_328_HW;
@@ -18,29 +18,31 @@ int main (void) {
   }
   SW_reset;
   return 1;
-}
+  }
 
 
 
 
-/**************Example 2*************************************************
-  int main (void){
+**************Example 2*************************************************/
+int main (void) {
   unsigned int PORT_1, m = 0, n = 0;
 
   setup_328_HW;
   sei();
   PORT_1 = 1;
-  while(1){
-  One_wire_Tx_2_integers(PORT_1 << m, PORT_1 << m);
-  _delay_ms(30);
-  if (!(n)) m += 1; if (m == 16)n = 1; if (n == 1)m -= 1;
-  if (m == 0)n = 0;}
-  return 1;}
+  while (1) {
+    One_wire_Tx_2_integers(PORT_1 << m, PORT_1 << m);
+    _delay_ms(30);
+    if (!(n)) m += 1; if (m == 16)n = 1; if (n == 1)m -= 1;
+    if (m == 0)n = 0;
+  }
+  return 1;
+}
 
 
 
 
-***************Example 3************************************************
+/***************Example 3************************************************
   int main (void){
   unsigned int PORT_1, PORT_2;
 
