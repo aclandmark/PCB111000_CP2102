c040dc03e220a8734bff43f84e22b769acdcfb27
aclandmark <mark.osborne15@btinternet.com>
2022-01-10 15:32:31 +0000

    Update A_first_project.ino

--- AVR_freaks_second_posting/Projects/A_first_project/A_first_project.ino
+++ AVR_freaks_second_posting/Projects/A_first_project/A_first_project.ino
@@ -4,8 +4,8 @@
 
 
 
-/*****************Example 1**********************************************/
-int main (void) {
+/*****************Example 1**********************************************
+  int main (void) {
   unsigned int PORT_1;
 
   setup_328_HW;
@@ -18,13 +18,13 @@ int main (void) {
   }
   SW_reset;
   return 1;
-}
+  }
 
 
 
 
-/**************Example 2*************************************************
-  int main (void) {
+**************Example 2*************************************************/
+int main (void) {
   unsigned int PORT_1, m = 0, n = 0;
 
   setup_328_HW;
@@ -37,26 +37,26 @@ int main (void) {
     if (m == 0)n = 0;
   }
   return 1;
-  }
+}
 
 
 
 
-  ***************Example 3************************************************
+/***************Example 3************************************************
   int main (void) {
   unsigned int PORT_1, PORT_2;
 
   setup_328_HW;
   sei();
   while (1) {
-    PORT_1 = 1;
-    PORT_2 = 0x8000;
-    for (int m = 0; m <= 15; m++) {
-      One_wire_Tx_2_integers(PORT_1, PORT_2);
-      _delay_ms(30);
-      PORT_1 = PORT_1 << 1;
-      PORT_2 = PORT_2 >> 1;
-    }
+  PORT_1 = 1;
+  PORT_2 = 0x8000;
+  for (int m = 0; m <= 15; m++) {
+    One_wire_Tx_2_integers(PORT_1, PORT_2);
+    _delay_ms(30);
+    PORT_1 = PORT_1 << 1;
+    PORT_2 = PORT_2 >> 1;
+  }
   }
   return 1;
   }
@@ -71,14 +71,14 @@ int main (void) {
   setup_328_HW;
   sei();
   while (1) {
-    PORT_1 = 1;
-    PORT_2 = 0x80000000;
-    for (int m = 0; m <= 31; m++) {
-      One_wire_Tx_2_integers(PORT_1, PORT_2);
-      _delay_ms(30);
-      PORT_1 = PORT_1 << 1;
-      PORT_2 = PORT_2 >> 1;
-    }
+  PORT_1 = 1;
+  PORT_2 = 0x80000000;
+  for (int m = 0; m <= 31; m++) {
+    One_wire_Tx_2_integers(PORT_1, PORT_2);
+    _delay_ms(30);
+    PORT_1 = PORT_1 << 1;
+    PORT_2 = PORT_2 >> 1;
+  }
   }
   return 1;
   }
@@ -86,22 +86,22 @@ int main (void) {
 
 
 
- *****************Example 5**********************************************
+*****************Example 5**********************************************
   int main (void) {
   unsigned int PORT_1, PORT_2;
 
   setup_328_HW;
   sei();
   while (1) {
-    PORT_1 = 1;
-    PORT_2 = 0x8000;
-    for (int m = 0; m <= 7; m++) {
-      One_wire_Tx_2_integers(PORT_1 | PORT_2,
-                             (PORT_1 << 8)  | (PORT_2 >> 8));
-      _delay_ms(30);
-      PORT_1 = PORT_1 << 1;
-      PORT_2 = PORT_2 >> 1;
-    }
+  PORT_1 = 1;
+  PORT_2 = 0x8000;
+  for (int m = 0; m <= 7; m++) {
+    One_wire_Tx_2_integers(PORT_1 | PORT_2,
+                           (PORT_1 << 8)  | (PORT_2 >> 8));
+    _delay_ms(30);
+    PORT_1 = PORT_1 << 1;
+    PORT_2 = PORT_2 >> 1;
+  }
   }
   return 1;
   }
@@ -118,12 +118,12 @@ int main (void) {
   PORT_1 = 1;
   PORT_2 = 0x8000;
   while (1) {
-    One_wire_Tx_2_integers(PORT_1 << m, PORT_2 >> m);
-    _delay_ms(30);
-    if (!(n)) m += 1;
-    if (m == 16)n = 1;
-    if (n == 1)m -= 1;
-    if (m == 0)n = 0;
+  One_wire_Tx_2_integers(PORT_1 << m, PORT_2 >> m);
+  _delay_ms(30);
+  if (!(n)) m += 1;
+  if (m == 16)n = 1;
+  if (n == 1)m -= 1;
+  if (m == 0)n = 0;
   }
   return 1;
   }
@@ -139,38 +139,38 @@ int main (void) {
 
   setup_328_HW;
   while (1) {
-    One_wire_Tx_2_integers(PORT_1, ~PORT_1);
-    _delay_ms(60);
-    if (m <= 5) {
-      PORT_1 |= (PORT_1 << 1);
-      m += 1;
-    }
-    else PORT_1 = PORT_1 << 1;
-    if (overflow)PORT_1 |= 1;
-    if (PORT_1 & 0x8000) overflow = 1;
-    else overflow = 0;
+  One_wire_Tx_2_integers(PORT_1, ~PORT_1);
+  _delay_ms(60);
+  if (m <= 5) {
+    PORT_1 |= (PORT_1 << 1);
+    m += 1;
+  }
+  else PORT_1 = PORT_1 << 1;
+  if (overflow)PORT_1 |= 1;
+  if (PORT_1 & 0x8000) overflow = 1;
+  else overflow = 0;
   }
   }
 
 
 
 
-  *****************Example 8**********************************************
+*****************Example 8**********************************************
   int main (void) {
   unsigned int random_num;
   long PORT_1 = 1, PORT_2 = 1;
 
   setup_328_HW;
   while (1) {
-    random_num = (PRN_16bit_GEN(0)) % 3;
-    for (int m = 0; m < random_num; m++)
-    { if (!(PORT_1 = ((PORT_1 * 2) % 0x10000)))
-        PORT_1 = 1;
-    }
-    if (!(PORT_2 = ((PORT_2 * 2) % 0x10000)))
-      PORT_2 = 1;
-    One_wire_Tx_2_integers(PORT_1, PORT_2);
-    Timer_T2_10mS_delay_x_m(4);
+  random_num = (PRN_16bit_GEN(0)) % 3;
+  for (int m = 0; m < random_num; m++)
+  { if (!(PORT_1 = ((PORT_1 * 2) % 0x10000)))
+      PORT_1 = 1;
+  }
+  if (!(PORT_2 = ((PORT_2 * 2) % 0x10000)))
+    PORT_2 = 1;
+  One_wire_Tx_2_integers(PORT_1, PORT_2);
+  Timer_T2_10mS_delay_x_m(4);
   }
   }
 
