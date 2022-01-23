35d228bbecd73d8dbb06b81437400f245d2ea9d1
aclandmark <mark.osborne15@btinternet.com>
2022-01-23 16:48:16 +0000

    Update 1_first_project.ino

--- AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
+++ AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
@@ -5,7 +5,7 @@
 
 
 /*****************Example 1**********************************************/
-  int main (void)
+/*int main (void)
   { unsigned int PORT_1;
 
   setup_328_HW;
@@ -13,12 +13,12 @@
   PORT_1 = 1;
   for (int m = 0; m <= 15; m++)
   { One_wire_Tx_2_integers(PORT_1, PORT_1);
-    _delay_ms(30);
-    PORT_1 = PORT_1 << 1;
+  _delay_ms(30);
+  PORT_1 = PORT_1 << 1;
   }
   SW_reset;
   return 1;
-  }
+  }*/
 
 
 
@@ -155,8 +155,8 @@
 
 
 *****************Example 8**********************************************
-int main (void)
-{ unsigned int random_num;
+  int main (void)
+  { unsigned int random_num;
   long PORT_1 = 1, PORT_2 = 1;
 
   setup_328_HW;
@@ -171,6 +171,43 @@ int main (void)
     One_wire_Tx_2_integers(PORT_1, PORT_2);
     Timer_T2_10mS_delay_x_m(4);
   }
+  }
+
+
+
+*****************Example 9**********************************************/
+char display_bkp[7];
+
+int main (void)
+
+{ char letter = 0;
+  char digit_num = 0;
+  char seg_counter = 0;
+  char direction = 0;
+
+  setup_328_HW;
+  One_wire_Tx_char = 'c';  UART_Tx_1_wire();
+
+  while (1)
+  { while (seg_counter < 56) {
+      letter = (PRN_16bit_GEN (0) % 7) + 'a';
+      digit_num = (PRN_16bit_GEN (0) % 8);
+      if ((!(direction)) && (display_bkp[letter - 'a'] & (1 << digit_num))) continue;
+      if ((direction) && (!(display_bkp[letter - 'a'] & (1 << digit_num)))) continue;
+      One_wire_comms_any_segment(letter, digit_num);
+      backup_the_display(letter, digit_num);
+      seg_counter += 1;
+    }
+
+    direction ^= 1;
+    seg_counter = 0;
+    _delay_ms(500);
+  }
+  SW_reset;
+  return 1;
 }
 
-*****************************************************************/
+void backup_the_display(char segment, char digit_num)
+{ display_bkp[segment - 'a'] =
+    display_bkp[segment - 'a'] ^ (1 << digit_num);
+}
