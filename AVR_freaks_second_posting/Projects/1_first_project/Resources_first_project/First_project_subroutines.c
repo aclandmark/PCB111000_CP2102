48eea7cc0339b962df08b3c1a7bb94bea9c7d3bc
aclandmark <mark.osborne15@btinternet.com>
2022-02-08 17:50:38 +0000

    Update First_project_subroutines.c

--- AVR_freaks_second_posting/Projects/1_first_project/Resources_first_project/First_project_subroutines.c
+++ AVR_freaks_second_posting/Projects/1_first_project/Resources_first_project/First_project_subroutines.c
@@ -1,5 +1,5 @@
 
-
+void print_eep_address (unsigned int);
 
 
 
@@ -14,9 +14,44 @@ One_wire_Tx_char = NUM_2 >> 8;  UART_Tx_1_wire();
 reinstate_pin_change_interrupt_on_PC5;}
 
 
-unsigned int PRN_16bit_GEN(unsigned int start){							//Pseuo random numbrer generation: Google  LFSR for details
-unsigned int bit, lfsr;
+unsigned int PRN_16bit_GEN(unsigned int start, unsigned char *PRN_counter){							//Pseuo random numbrer generation: Google  LFSR for details
+unsigned int bit, lfsr, eep_address;
+unsigned char eep_offset;
 
+eep_offset = eeprom_read_byte((uint8_t*)(0x3ED));
+
+if(!(*PRN_counter)) 
+{lfsr = (eeprom_read_byte((uint8_t*)(0x3F2 - eep_offset)) << 8) + 
+eeprom_read_byte((uint8_t*)(0x3F3 - eep_offset));}
+
+else lfsr = start;
+bit = (( lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
+lfsr = (lfsr >> 1) | (bit << 15);
+
+*PRN_counter += 32;
+
+if(!(*PRN_counter))
+{eeprom_write_byte((uint8_t*)(0x3F3),(lfsr>>8));
+eeprom_write_byte((uint8_t*)(0x3F2),lfsr);
+
+print_eep_address(0x3F3);
+Char_to_PC(' ');
+print_eep_address(0x3F2);
+String_to_PC("\r\n");
+Toggle_LED_1;}
+
+return lfsr;}
+
+void print_eep_address (unsigned int eep_address)
+{char num_string[10];
+eep_address -= eeprom_read_byte((uint8_t*)(0x3ED));
+itoa (eep_address, num_string, 16 );
+{int m = 0; while (num_string[m])Char_to_PC(num_string[m++]);}}
+
+
+
+
+/*
 if(!(start)) lfsr = (eeprom_read_byte((uint8_t*)(0x3F3)) << 8) 
 + eeprom_read_byte((uint8_t*)(0x3F2));
 else lfsr = start;
@@ -27,7 +62,7 @@ eeprom_write_byte((uint8_t*)(0x3F3),(lfsr>>8));
 eeprom_write_byte((uint8_t*)(0x3F2),lfsr);}
 
 return lfsr;}
-
+*/
 
 /*void I2C_Tx_any_segment_clear_all(void){
 char segment = 'a'; char digit_num = 0;
