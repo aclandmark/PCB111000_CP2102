c514aea125bc74406dfbf13f4e06ec8ce1feb3e5
aclandmark <mark.osborne15@btinternet.com>
2022-02-08 17:51:47 +0000

    Update Proj_2C1_subroutines.c

--- AVR_freaks_first_posting/Firmware/CP2102_bootloader_New/1_Random_pattern_generator/Resources_2C1/Proj_2C1_subroutines.c
+++ AVR_freaks_first_posting/Firmware/CP2102_bootloader_New/1_Random_pattern_generator/Resources_2C1/Proj_2C1_subroutines.c
@@ -15,16 +15,17 @@ display_bkp[segment - 'a'] = display_bkp[segment - 'a'] ^ (1 << digit_num);}
 
 
 /*************************************************************************************************************/
-unsigned int PRN_16bit_GEN(unsigned int start){																//Google LFSR
-unsigned int bit, lfsr;
+unsigned int PRN_16bit_GEN(unsigned int start){		//Google LFSR  (Note: zero start is not used in the default application)
+unsigned int bit, lfsr;								//Removing unwanted lines does not free up and additional page and has not been done
 
-if(!(start)) lfsr = (eeprom_read_byte((uint8_t*)(0x3F2)) << 8) + eeprom_read_byte((uint8_t*)(0x3F3));
-else lfsr = start;
+if(!(start)) lfsr = (eeprom_read_byte((uint8_t*)(0x3F2)) << 8) + eeprom_read_byte((uint8_t*)(0x3F3));	//unwanted line
+else 																									//unwanted line
+lfsr = start;
 bit = (( lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
 lfsr = (lfsr >> 1) | (bit << 15);
-if(!(start)){
-eeprom_write_byte((uint8_t*)(0x3F2),(lfsr>>8));
-eeprom_write_byte((uint8_t*)(0x3F3),lfsr);}
+if(!(start)){																							//unwanted line
+eeprom_write_byte((uint8_t*)(0x3F2),(lfsr>>8));															//unwanted line
+eeprom_write_byte((uint8_t*)(0x3F3),lfsr);}																//unwanted line
 
 return lfsr;}
  
