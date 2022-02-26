2c3100f44bc1562e0eb6e14e3c4288c380df56be
aclandmark <mark.osborne15@btinternet.com>
2022-02-26 11:05:30 +0000

    Update 1_first_project.ino

--- AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
+++ AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
@@ -2,21 +2,20 @@
 #include "First_project_header.h"
 
 
-/*****************Example 1**********************************************/
-int main (void)
-  { unsigned int PORT_1;
+int main (void)                          //Example 1
+{ unsigned int PORT_1;
 
   setup_328_HW;
   sei();
   PORT_1 = 1;
   for (int m = 0; m <= 15; m++)
   { One_wire_Tx_2_integers(PORT_1, PORT_1);
-  _delay_ms(30);
-  PORT_1 = PORT_1 << 1;
+    _delay_ms(30);
+    PORT_1 = PORT_1 << 1;
   }
   SW_reset;
   return 1;
-  }
+}
 
 
 
@@ -25,10 +24,10 @@ int main (void)
 
 /***********************************************************************
 
-Use this area for saving the examples when they have been got working and finished with
-Use the space above for the active program (the one eing worked on)
+  Use this area for saving the examples when they have been got working and finished with
+  Use the space above for the active program (the one eing worked on)
 
- 
+
 
 
 
@@ -173,10 +172,10 @@ Use the space above for the active program (the one eing worked on)
   setup_328_HW;
   PRN_counter = 0;
   random_num = PRN_16bit_GEN (0, &PRN_counter);
-  
-  
+
+
   while (1)
-{for (int m = 0; m < random_num%3; m++)
+  {for (int m = 0; m < random_num%3; m++)
     { if (!(PORT_1 = ((PORT_1 * 2) % 0x10000)))
         PORT_1 = 1;
     }
@@ -184,16 +183,16 @@ Use the space above for the active program (the one eing worked on)
       PORT_2 = 1;
    One_wire_Tx_2_integers(PORT_1, PORT_2);
     Timer_T2_10mS_delay_x_m(4);
-random_num = PRN_16bit_GEN (random_num, &PRN_counter);}}
+  random_num = PRN_16bit_GEN (random_num, &PRN_counter);}}
 
 
 
 *****************Example 9**********************************************
-char display_bkp[7];
+  char display_bkp[7];
 
-int main (void)
+  int main (void)
 
-{ char letter = 0;
+  { char letter = 0;
   char digit_num = 0;
   char seg_counter = 0;
   char direction = 0;
@@ -224,28 +223,27 @@ int main (void)
   }
   SW_reset;
   return 1;
-}
+  }
 
-void backup_the_display(char segment, char digit_num)
-{ display_bkp[segment - 'a'] =
+  void backup_the_display(char segment, char digit_num)
+  { display_bkp[segment - 'a'] =
     display_bkp[segment - 'a'] ^ (1 << digit_num);
-}
+  }
 
 
 
 *****************Example 10**********************************************
-int main (void)  
-{unsigned int PRN;
-unsigned char PRN_counter; 
+  int main (void)
+  {unsigned int PRN;
+  unsigned char PRN_counter;
 
-setup_328_HW;
-PRN_counter = 0;
-PRN = PRN_16bit_GEN (0, &PRN_counter);
-sei();
-while(1) 
-{PRN = PRN_16bit_GEN (PRN, &PRN_counter);
-One_wire_Tx_2_integers(PRN, (PRN<<((PRN%2) + 1))); 
-Timer_T2_10mS_delay_x_m(10);}}  
+  setup_328_HW;
+  PRN_counter = 0;
+  PRN = PRN_16bit_GEN (0, &PRN_counter);
+  sei();
+  while(1)
+  {PRN = PRN_16bit_GEN (PRN, &PRN_counter);
+  One_wire_Tx_2_integers(PRN, (PRN<<((PRN%2) + 1)));
+  Timer_T2_10mS_delay_x_m(10);}}
 
 **************************************************************************/
- 
