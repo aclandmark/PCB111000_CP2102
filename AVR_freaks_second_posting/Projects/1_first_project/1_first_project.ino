b023aaff6059197b8955828f9bab5a183fd9cc97
aclandmark <mark.osborne15@btinternet.com>
2022-02-26 11:37:56 +0000

    Update 1_first_project.ino

--- AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
+++ AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
@@ -1,22 +1,25 @@
 
 #include "First_project_header.h"
 
-int main (void)                          //Example 6
-{ unsigned int PORT_1, PORT_2, m = 0, n = 0;
+int main (void)                          //Example 7
+{ unsigned int PORT_1 = 1;
+  char m = 1;
+  char overflow = 0;
 
   setup_328_HW;
-  sei();
-  PORT_1 = 1;
-  PORT_2 = 0x8000;
   while (1)
-  { One_wire_Tx_2_integers(PORT_1 << m, PORT_2 >> m);
-    _delay_ms(30);
-    if (!(n)) m += 1;
-    if (m == 16)n = 1;
-    if (n == 1)m -= 1;
-    if (m == 0)n = 0;
+  { One_wire_Tx_2_integers(PORT_1, ~PORT_1);
+    _delay_ms(60);
+    if (m <= 5)
+    {
+      PORT_1 |= (PORT_1 << 1);
+      m += 1;
+    }
+    else PORT_1 = PORT_1 << 1;
+    if (overflow)PORT_1 |= 1;
+    if (PORT_1 & 0x8000) overflow = 1;
+    else overflow = 0;
   }
-  return 1;
 }
 
 
@@ -134,32 +137,29 @@ int main (void)                          //Example 6
 
 
 ******************Example 6*********************************************
+  int main (void)                          //Example 6
+  { unsigned int PORT_1, PORT_2, m = 0, n = 0;
 
+  setup_328_HW;
+  sei();
+  PORT_1 = 1;
+  PORT_2 = 0x8000;
+  while (1)
+  { One_wire_Tx_2_integers(PORT_1 << m, PORT_2 >> m);
+    _delay_ms(30);
+    if (!(n)) m += 1;
+    if (m == 16)n = 1;
+    if (n == 1)m -= 1;
+    if (m == 0)n = 0;
+  }
+  return 1;
+  }
 
 
 
 
 ******************Example 7********************************************
-  int main (void)
-  { unsigned int PORT_1 = 1;
-  char m = 1;
-  char overflow = 0;
 
-  setup_328_HW;
-  while (1)
-  { One_wire_Tx_2_integers(PORT_1, ~PORT_1);
-    _delay_ms(60);
-    if (m <= 5)
-    {
-      PORT_1 |= (PORT_1 << 1);
-      m += 1;
-    }
-    else PORT_1 = PORT_1 << 1;
-    if (overflow)PORT_1 |= 1;
-    if (PORT_1 & 0x8000) overflow = 1;
-    else overflow = 0;
-  }
-  }
 
 
 *****************Example 8**********************************************
