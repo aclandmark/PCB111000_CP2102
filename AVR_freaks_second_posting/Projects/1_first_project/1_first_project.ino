c4754f6aae7b6d6b05fcb378ecf5755a14abde7d
aclandmark <mark.osborne15@btinternet.com>
2022-02-26 11:31:01 +0000

    Update 1_first_project.ino

--- AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
+++ AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
@@ -1,25 +1,23 @@
 
 #include "First_project_header.h"
 
-int main (void)                          //Example 5
-{ unsigned int PORT_1, PORT_2;
+int main (void)                          //Example 6
+{ unsigned int PORT_1, PORT_2, m = 0, n = 0;
 
   setup_328_HW;
   sei();
+  PORT_1 = 1;
+  PORT_2 = 0x8000;
   while (1)
-  { PORT_1 = 1;
-    PORT_2 = 0x8000;
-    for (int m = 0; m <= 7; m++)
-    { One_wire_Tx_2_integers(PORT_1 | PORT_2,
-                             (PORT_1 << 8)  | (PORT_2 >> 8));
-      _delay_ms(30);
-      PORT_1 = PORT_1 << 1;
-      PORT_2 = PORT_2 >> 1;
-    }
+  { One_wire_Tx_2_integers(PORT_1 << m, PORT_2 >> m);
+    _delay_ms(30);
+    if (!(n)) m += 1;
+    if (m == 16)n = 1;
+    if (n == 1)m -= 1;
+    if (m == 0)n = 0;
   }
   return 1;
 }
-
 
 
 
@@ -111,6 +109,24 @@ int main (void)                          //Example 5
 
 
 *****************Example 5**********************************************
+  int main (void)                          //Example 5
+  { unsigned int PORT_1, PORT_2;
+
+  setup_328_HW;
+  sei();
+  while (1)
+  { PORT_1 = 1;
+    PORT_2 = 0x8000;
+    for (int m = 0; m <= 7; m++)
+    { One_wire_Tx_2_integers(PORT_1 | PORT_2,
+                             (PORT_1 << 8)  | (PORT_2 >> 8));
+      _delay_ms(30);
+      PORT_1 = PORT_1 << 1;
+      PORT_2 = PORT_2 >> 1;
+    }
+  }
+  return 1;
+  }
 
 
 
@@ -118,23 +134,7 @@ int main (void)                          //Example 5
 
 
 ******************Example 6*********************************************
-  int main (void)
-  { unsigned int PORT_1, PORT_2, m = 0, n = 0;
 
-  setup_328_HW;
-  sei();
-  PORT_1 = 1;
-  PORT_2 = 0x8000;
-  while (1)
-  { One_wire_Tx_2_integers(PORT_1 << m, PORT_2 >> m);
-    _delay_ms(30);
-    if (!(n)) m += 1;
-    if (m == 16)n = 1;
-    if (n == 1)m -= 1;
-    if (m == 0)n = 0;
-  }
-  return 1;
-  }
 
 
 
