7f8f90acd61fe89bd82eab35af6b256690e3d13b
aclandmark <mark.osborne15@btinternet.com>
2022-03-23 16:07:46 +0000

    Update 5B_Segment_driver.ino

--- AVR_freaks_second_posting/Projects/5_The_8_digit_display/5B_Segment_driver/5B_Segment_driver.ino
+++ AVR_freaks_second_posting/Projects/5_The_8_digit_display/5B_Segment_driver/5B_Segment_driver.ino
@@ -18,7 +18,8 @@ setup_328_HW;
 
 Serial.begin(115200);
     while (!Serial);
-if (eeprom_read_byte ((uint8_t*)0x3FC) == 0xFF)eeprom_write_byte((uint8_t*)0x3F5, 0xFF);      //POR detected
+if (eeprom_read_byte ((uint8_t*)0x3FC) == 0xFF)
+eeprom_write_byte((uint8_t*)0x3F5, 0xFF);                               //POR detected
 
 if(!(eeprom_read_byte ((uint8_t*)0x3F5)))
 Serial.write(message_2);    
@@ -26,24 +27,24 @@ else{eeprom_write_byte((uint8_t*)0x3F5, 0xFF);
 Serial.write(message_1);}
 sei();
 
-while(1){                                                                             //Keep inputting data until x is received
+while(1){                                                               //Keep inputting data until x is received
 
 while(!(Serial.available())); keypress = Serial.read();            
   
-switch(keypress){                                                             //Test keypress
+switch(keypress){                                                       //Test keypress
 case '0': case '1': case '2': 
 case '3': case '4': case '5': 
-case '6': case '7': digit_num = keypress - '0'; break;                          //If numeric save it as "digit_num".
+case '6': case '7': digit_num = keypress - '0'; break;                  //If numeric save it as "digit_num".
 
-case 'a': case 'b': case 'c':                                                     //If a letter illuminate the display 
+case 'a': case 'b': case 'c':                                           //If a letter illuminate the display 
 case 'd': case 'e': case 'f': 
 case 'g': One_wire_comms_any_segment(keypress, digit_num); break;
 
 case 'x': {eeprom_write_byte((uint8_t*)0x3F5, 0);
-clear_display;SW_reset;} break;                                                         //if any other key (AOK) execute a SW_reset.
+clear_display;SW_reset;} break;                                          //if any other key (AOK) execute a SW_reset.
 
 defaut: break;
 
-}                                                                                     //Bottom of switch block return to top of "do-loop"
+}                                                                        //Bottom of switch block return to top of "do-loop"
 }
-}                                                                                //Repeat program
+}                                                                         //Repeat program
