67e0e8768c69830e0cf40b8ac41f14ba097ef429
aclandmark <mark.osborne15@btinternet.com>
2022-03-24 16:21:38 +0000

    Update Scientific_numbers.ino

--- AVR_freaks_second_posting/Projects/4_Arduino_PC_comms_library/Scientific_numbers/Scientific_numbers.ino
+++ AVR_freaks_second_posting/Projects/4_Arduino_PC_comms_library/Scientific_numbers/Scientific_numbers.ino
@@ -1,12 +1,12 @@
 
+/*
+The Arduino library accepts numbers containing a decimal point and also sends them to the PC
+Here we use this facility and extend it to include scientific numbers i.e 1.25E8
+*/
+
+
 
 #include "Scientific_numbers_header.h"
-
-//NOTE ARDUINO automatically configures the UART for 115200 Baud
-//This translates to 57600 for PCB111000_PCB2102
-//The header file includes a macro to initiate the UART
-//This allows users to determine their own baud rate
-
 
 /************************************************************************************************************/
 
@@ -41,7 +41,7 @@ Serial.read();}
 
 
 /*****************************************************************************************/
-void Sc_Num_to_PC(float num, char pre_dp, char post_dp, char next_char)    //SC_num_to_C
+void Sc_Num_to_PC(float num, char pre_dp, char post_dp, char next_char)
 {int A = 1;
 char keypresses[12];
 char sign = '+';
