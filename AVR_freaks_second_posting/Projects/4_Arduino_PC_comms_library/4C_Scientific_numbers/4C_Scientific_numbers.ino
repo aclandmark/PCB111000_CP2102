60e1c6c331ff92cd86c6ed70df2a94caf2ee8f8b
aclandmark <mark.osborne15@btinternet.com>
2022-04-06 15:53:41 +0100

    Create 4C_Scientific_numbers.ino

---
+++ AVR_freaks_second_posting/Projects/4_Arduino_PC_comms_library/4C_Scientific_numbers/4C_Scientific_numbers.ino
@@ -0,0 +1,87 @@
+
+/*
+The Arduino library accepts numbers containing a decimal point and also sends them to the PC
+Here we use this facility and extend it to include scientific numbers i.e 1.25E8
+*/
+
+
+
+#include "Scientific_numbers_header.h"
+
+/************************************************************************************************************/
+
+
+
+
+
+int main (void)  
+  { 
+    char num_string[12], User_response;
+    float  num;
+ 
+ setup_328_HW;
+    Serial.begin(115200);
+    while (!Serial);
+   User_prompt;
+ 
+   Serial.write("Scientific number\t");
+num = Sc_Num_from_PC(num_string, '\r');
+
+while(1){
+num = pow (num,1.5);
+Sc_Num_to_PC(num, 2, 4, '\r');
+
+while(!(Serial.available()));
+Serial.read();}
+ SW_reset;
+  return 1;
+  }
+
+
+
+
+/*****************************************************************************************/
+void Sc_Num_to_PC(float num, char pre_dp, char post_dp, char next_char)
+{int A = 1;
+char keypresses[12];
+char sign = '+';
+int Exp = 0;
+
+Check_num_for_to_big_or_small(num);
+
+if (num < 0){sign = '-'; num = num * (-1);}
+
+while(--pre_dp){A = A*10;}
+while (num >= A){num = num/10.0; Exp += 1;}
+while (num <= A){num = num*10.0; Exp -= 1;}
+
+if(sign == '-')num = num * (-1);
+
+Serial.print(num, post_dp);
+if(Exp) {Serial.write ('E'); Serial.print(Exp);}
+Serial.write(next_char);}
+
+
+
+
+/*****************************************************************************************/
+void Check_num_for_to_big_or_small(float num)
+{unsigned long * long_ptr;
+long_ptr = (unsigned long *)&num;
+if (*long_ptr == 0x7F800000){Serial.write("+ve Num too large\r\n");SW_reset;}
+if (*long_ptr == 0xFF800000){Serial.write("-ve Num too large\r\n");SW_reset;}
+if (*long_ptr == 0X0){Serial.write("+ve Num too small\r\n");SW_reset;}
+if (*long_ptr == 0X80000000){Serial.write("-ve Num too small\r\n");SW_reset;}}
+
+
+
+/******************************************************************************************/
+float Sc_Num_from_PC(char * num_as_string,char next_char)
+{char strln;
+
+Serial.flush();   
+strln = Serial.readBytesUntil('\r',num_as_string, 20);
+num_as_string[strln] = 0;
+Serial.write(num_as_string);
+Serial.write(next_char);
+return atof(num_as_string);}
