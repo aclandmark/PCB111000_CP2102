f088a1b167a801f1cdff03e1f4de55709af72c5d
aclandmark <mark.osborne15@btinternet.com>
2022-04-11 20:21:28 +0100

    Create Proj_3A_subroutines.c

---
+++ AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3A_UART_timer_interrupts/Resources_3A/Proj_3A_subroutines.c
@@ -0,0 +1,26 @@
+
+void Num_to_PC (long number)
+{ int i = 0;
+  char s[12];
+ 
+  do
+  { s[i++] = number % 10 + '0';
+  }
+  while ((number = number / 10) > 0);
+  s[i] = '\0';
+  for (int m = i; m > 0; m--)Char_to_PC(s[m - 1]);
+  Char_to_PC(' ');
+}
+
+
+
+void One_wire_Tx_2_integers(unsigned int NUM_1, unsigned int NUM_2){	//Transaction: Send type and two 16 bit words to the ATtiny1608
+pause_pin_change_interrupt_on_PC5;										//Reset control not allowed during transaction
+One_wire_Tx_char = 'a';  UART_Tx_1_wire();								//Transaction type is 'a'
+One_wire_Tx_char = NUM_1;  UART_Tx_1_wire(); 							//Send lower 8 bits
+One_wire_Tx_char = NUM_2;  UART_Tx_1_wire(); 
+
+One_wire_Tx_char = NUM_1 >> 8;  UART_Tx_1_wire();						//Send upper 8 bits 
+One_wire_Tx_char = NUM_2 >> 8;  UART_Tx_1_wire(); 
+reinstate_pin_change_interrupt_on_PC5;}
+
