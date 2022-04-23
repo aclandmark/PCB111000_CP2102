913f99b4c35b5a974cdaec9e191e17de5ee18b41
aclandmark <mark.osborne15@btinternet.com>
2022-04-23 20:54:24 +0100

    Update 4_Char_to_binary.ino

--- AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/4_Char_to_binary/4_Char_to_binary.ino
+++ AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/4_Char_to_binary/4_Char_to_binary.ino
@@ -12,36 +12,43 @@ int main (void){
 
 op=0;
 mode = 'u';                          //mode: signed or unsigned
-setup_HW;
-I2C_Tx_display_char(digit,mode);
-setup_and_enable_PCI;
+setup_328_HW;
 sei();
+One_wire_char_as_binary(digit,mode);
+
+set_up_PCI_on_user_switches;
+enable_PCI_on_user_switches;
 
 
 while(1){
 switch(op){
-case 0: break;                        //Static display
-case 1: I2C_Tx_display_char(++digit,mode);break;      //Increment "digit" before calling "I2C_Tx....."
-case 2: I2C_Tx_display_char(--digit, mode);break;}      //Decrement "digit" before calling "I2C_Tx....."
+case 0: break;                                                //Static display
+case 1: One_wire_char_as_binary(++digit,mode);break;              //Increment "digit" before calling "I2C_Tx....."
+case 2: One_wire_char_as_binary(--digit, mode);break;}            //Decrement "digit" before calling "I2C_Tx....."
 
-Timer_T0_10mS_delay_x_m(15);}}
+Timer_T2_10mS_delay_x_m(15);}}
 
-
-
-/**************************************************************************************************/
-ISR(PCINT2_vect) {                      //sw1 and sw3 interrupt
-if(switch_2_down)return;                  //Ignore if sw2 is still down
-if((switch_1_up) && (switch_3_up)){op = 0; return;}   //Both switches up
-
-if(switch_1_down) {op = 1; return;}           //Digits increment
-if(switch_3_down) {op = 2; return;}}            //Digits decrement
 
 
 /**************************************************************************************************/
-ISR(PCINT0_vect){                     //sw2 interrupt service routine
-if(switch_2_up)return;                    //Ignore sw2 key release
-if((switch_1_down) || (switch_3_down))return;       //Ignore if sw1 or 3 are still down
+/*ISR(PCINT2_vect) {                                              //sw1 and sw3 interrupt
+if(switch_2_down)return;                                        //Ignore if sw2 is still down
+if((switch_1_up) && (switch_3_up)){op = 0; return;}             //Both switches up
+
+if(switch_1_down) {op = 1; return;}                            //Digits increment
+if(switch_3_down) {op = 2; return;}}                            //Digits decrement
+*/
+
+/**************************************************************************************************/
+/*ISR(PCINT0_vect){                                                 //sw2 interrupt service routine
+if(switch_2_up)return;                                           //Ignore sw2 key release
+if((switch_1_down) || (switch_3_down))return;                    //Ignore if sw1 or 3 are still down
 if (mode == 's')  
-{mode = 'u'; I2C_Tx_display_char(digit,mode);return;}   //toggle display from signed to unsigned
+{mode = 'u'; One_wire_char_as_binary(digit,mode);return;}             //toggle display from signed to unsigned
 if (mode == 'u')
-{mode = 's'; I2C_Tx_display_char(digit,mode);return;}}    //and visa-versa
+{mode = 's'; One_wire_char_as_binary(digit,mode);return;}}            //and visa-versa
+*/
+
+ISR(PCINT2_vect) {}
+
+void One_wire_char_as_binary(char num, char mode){}
