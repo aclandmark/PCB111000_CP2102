7a453ac4bfbbd7249916572df46d09ab0b273063
aclandmark <mark.osborne15@btinternet.com>
2022-04-28 17:39:18 +0100

    Update clock_timer_header.h

--- AVR_freaks_second_posting/Projects/7_Timers/1_Basic_clock/Resources_Basic_clock/clock_timer_header.h
+++ AVR_freaks_second_posting/Projects/7_Timers/1_Basic_clock/Resources_Basic_clock/clock_timer_header.h
@@ -13,24 +13,14 @@
 
 /***********Define clock display functions and modes***************/
 #define reset_clock_1   {digits[0] = 0; for (int m = 1; m < 8; m++)digits[m] = '0'; }
-#define reset_clock_2   {for (int m = 0;  m < 8; m++)\
-{if ((m==2) || (m== 5))digits[m] = ' '; else digits[m] = '0'; }\
-I2C_Tx_8_byte_array_plus_mode(7, digits);I2C_Tx_3(8, '2');}
 
-#define AT_clock_mode 7
-#define AT_exit_stop_watch 10, '1'
-#define AT_mode_1 4
 
-#define display_clock 8, '1'
-#define hide_clock 8, '2'
-#define pause_clock 8, '3'
-#define increment_seconds 8, '5'
-#define decrement_minutes 8, '6'
-#define decrement_seconds 8, '7'
-#define increment_minutes 8, '8'
+#define Display_time \
+{One_wire_Tx_char = 'A'; UART_Tx_1_wire();\
+for(int p = 0; p <= 7; p++){\
+switch (p){\
+  case 2:\
+  case 4:\
+  case 6: One_wire_Tx_char = digits[p] | 0x80; UART_Tx_1_wire(); break;\
+  default: One_wire_Tx_char = digits[p]; UART_Tx_1_wire(); break;}}}
 
-#define display_current_time 9, '1'
-#define display_stored_times 9, '4'
-#define store_time 9, '2'
-#define one100ms_mode 9, '1'
-#define ten_ms_mode 9, '2'
