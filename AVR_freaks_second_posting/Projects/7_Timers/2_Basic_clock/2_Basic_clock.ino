0ace209349c0339cb776714f8eb240e4da360f2d
aclandmark <mark.osborne15@btinternet.com>
2022-04-29 18:10:06 +0100

    Update 2_Basic_clock.ino

--- AVR_freaks_second_posting/Projects/7_Timers/2_Basic_clock/2_Basic_clock.ino
+++ AVR_freaks_second_posting/Projects/7_Timers/2_Basic_clock/2_Basic_clock.ino
@@ -11,63 +11,77 @@ Counts 106 in the last 100mS
  
 #include "Basic_clock_header.h"
 
- volatile char clock_tick;
+volatile char clock_tick;
 volatile char tick_counter; 
 
-void set_time(void);
-void timer_utoa(char);
-void Format_time_for_display(void);//Display_time(void);
+char digits[8], charH, charL ;
+char Hours, Minutes, Seconds, deci_Secs;
+long deci_sec_counter;
 
-char digits[8], charH, charL, Hours, Minutes, Seconds;
-unsigned int sec_counter;
 
 
 int main (void){
 char User_response;
 setup_328_HW;
+initialise_T2();
 sei();
-
+clear_display;
 
 String_to_PC("Press 'R' to enter time or 'r' to start at time zero  ");
 User_prompt;
 if(User_response == 'R')set_time();
-else {reset_clock_1; msecsH = 0; msecsL = 0;  }
+else {reset_clock_1; deci_SecsH = '0'; deci_SecsL = '0'; deci_sec_counter = 0; }
 Display_time;
 
 String_to_PC("AK to start\r\n");
 waitforkeypress();
-binUnwantedChars();
 
-while(1){Timer_T2_10mS_delay_x_m(100);sec_counter++; Format_time_for_display();}}
-
+start_clock();
+while(1){while(clock_tick <= 1);clock_tick = 0; deci_sec_counter += 2;
+if(deci_sec_counter == 864000)deci_sec_counter = 0;
+Format_time_for_display();Display_time;}}
 
 
 
 /**********************************************************************************************************************/
 void Format_time_for_display(void){
-Hours = sec_counter/3600;
-Minutes = (sec_counter%3600)/60;
-Seconds = (sec_counter%3600)%60;
+Hours =     deci_sec_counter/36000;
+Minutes =   (deci_sec_counter%36000)/600;
+Seconds =   ((deci_sec_counter%36000)%600)/10;
+deci_Secs = ((deci_sec_counter%36000)%600)%10;
+
 timer_utoa(Hours); HoursH = charH; HoursL = charL; 
 timer_utoa(Minutes); MinsH = charH; MinsL = charL; 
 timer_utoa(Seconds); SecsH = charH; SecsL = charL; 
+timer_utoa(deci_Secs); deci_SecsH = charH; deci_SecsL = charL; }
+
+
+/*******************************************************************************************************************************/
+void set_time(void){
+
+for(int m = 0; m <= 7; m++)digits[m] = 0;
+String_to_PC("Enter start time Hours, Minutes and Seconds\
+\r\n(24 hour clock with no spaces). Terminate with cr\r\n");
+
+while(isCharavailable(50) == 0){String_to_PC("T?  ");}
+
+digits[7] = Char_from_PC();Display_time;
+for (int m = 0; m<=4; m++){while(isCharavailable(5) == 0);
+if(m == 4)
+{digits[2] = Char_from_PC();deci_SecsH = '0'; deci_SecsL = '0'; }
+else 
+digits[6 - m] = Char_from_PC(); 
 Display_time;}
 
+waitforkeypress();
+deci_sec_counter = 10*(long)((((long)((HoursH -'0') * 10) + HoursL-'0') * 3600) +
+((((MinsH-'0') * 10) + MinsL-'0') * 60) +(SecsH-'0') * 10 + SecsL - '0');
+
+clear_display;
+_delay_ms(50);}
 
 
-/**********************************************************************************************************************/
-void set_time(void){
-String_to_PC("\r\nEnter start time Hours, Minutes and Seconds (12 hour clock, no spaces)\r\n");
-while(isCharavailable(50) == 0){String_to_PC("T?  ");}    
-    HoursH = Char_from_PC();
-    while(isCharavailable(5) == 0); HoursL = Char_from_PC();
-    while(isCharavailable(5) == 0); MinsH = Char_from_PC();
-    while(isCharavailable(5) == 0); MinsL = Char_from_PC();
-    while(isCharavailable(5) == 0); SecsH = Char_from_PC();
-    while(isCharavailable(5) == 0); SecsL= Char_from_PC(); 
-    msecsH = 0; msecsL = 0; 
-    sec_counter = ((((HoursH -'0') * 10) + HoursL-'0') * 3600) + ((((MinsH-'0') * 10) + MinsL-'0') * 60) +(SecsH-'0') * 10 + SecsL - '0'; }
-    
+
 
 
 /**********************************************************************************************************************/
@@ -79,24 +93,6 @@ charL = charL + '0';
 charH = charH + '0';}
 else
 {charH = '0'; charL = n + '0';}}
-
-
-
-
-
-
-
-/*********************************************************************/
-void binUnwantedChars (void){char bin_char;
-while(1){if (isCharavailable(5)==1)bin_char = Char_from_PC();else break;}}
-
-
-
-
-
-
-
-
 
 
 
@@ -121,25 +117,7 @@ ISR (TIMER2_COMPA_vect){ char string[5];
   OCR2A += 102;
   clock_tick += 1;
   tick_counter += 1;
-  if(tick_counter == 9){tick_counter = -1; OCR2A += 4;}
+  if(tick_counter == 9){tick_counter = -1; OCR2A += 4;}}
   
-  
- /*Binary_to_Askii (OCR2A, string) ;
-  String_to_PC("\r\n");
-  String_to_PC(string);*/
-  
-  }
 
-   
-int Binary_to_Askii (long number, char * array_ptr)
-{ int i = 0;
-  do {
-    array_ptr[i++] = number % 10 + '0';
-  }
-  while ((number = number / 10) > 0);
-  array_ptr[i] = '\0';
-  return i;
-}
-
-
-/****************************************************************************************************************/
+/************************************************************************************************************/
