28d9cd9dd13408ed09355986bc568601359c5236
aclandmark <mark.osborne15@btinternet.com>
2022-05-01 15:01:42 +0100

    Create clock_timer_header.h

---
+++ AVR_freaks_second_posting/Projects/7_Timers/1_String_only_clock/Resources_Basic_clock/clock_timer_header.h
@@ -0,0 +1,26 @@
+
+/************Define clock memory locations********************/
+#define HoursH digits[7]
+#define HoursL digits[6]
+#define MinsH digits[5]
+#define MinsL digits[4]
+#define SecsH digits[3]
+#define SecsL digits[2]
+#define msecsH digits[1]
+#define msecsL digits[0]
+
+
+
+/***********Define clock display functions and modes***************/
+#define reset_clock_1   {digits[0] = 0; for (int m = 1; m < 8; m++)digits[m] = '0'; }
+
+
+#define Display_time \
+{One_wire_Tx_char = 'A'; UART_Tx_1_wire();\
+for(int p = 0; p <= 7; p++){\
+switch (p){\
+  case 2:\
+  case 4:\
+  case 6: One_wire_Tx_char = digits[p] | 0x80; UART_Tx_1_wire(); break;\
+  default: One_wire_Tx_char = digits[p]; UART_Tx_1_wire(); break;}}}
+
