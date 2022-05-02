5dc04df25ebb1a404c86fe87510cebea8133bc9d
aclandmark <mark.osborne15@btinternet.com>
2022-05-02 16:00:12 +0100

    Update 4_stand_alone_clock.ino

--- AVR_freaks_second_posting/Projects/7_Timers/4_stand_alone_clock/4_stand_alone_clock.ino
+++ AVR_freaks_second_posting/Projects/7_Timers/4_stand_alone_clock/4_stand_alone_clock.ino
@@ -46,11 +46,13 @@ while(1){while(clock_tick <= 1);clock_tick = 0; Inc_OS_time;
 
 void time_from_IO(void);
 volatile int digit_num = 7;
-char start_time[8];
+char digits[8];
 volatile char Data_Entry_complete=0;
 char keypress = 0;
+volatile char tick_counter; 
+volatile char clock_tick;
 
-  
+ 
 int main (void){
 char display_mode;
 
@@ -60,18 +62,18 @@ sei();
 clear_display;                       
 User_instructions;
 time_from_IO();
-I2C_Tx_OS_timer(AT_clock_mode, start_time);           //Send Start clock command (AT clock mode is 7)
+//I2C_Tx_OS_timer(AT_clock_mode, digits);           //Send Start clock command (AT clock mode is 7)
 
 
 
 /********************Clock Control loop**************************************************************************/
 
-display_mode = 0;
+/*display_mode = 0;
 while(1){                           //Operation continuously cycles around this loop 
-Timer_T0_10mS_delay_x_m(10);
+Timer_T1_sub(T1_delay_100ms);
 switch (display_mode){                      //Beware of switch bounce
 
-case 0: if(switch_2_down){Timer_T0_10mS_delay_x_m(50); display_mode = 'A';}
+case 0: if(switch_2_down){Timer_T1_sub(T1_delay_500ms); display_mode = 'A';}
     if(switch_1_down){display_mode = 'B';I2C_Tx_Clock_command(hide_clock);while(switch_1_down);}
     if(switch_3_down){display_mode = 'C';I2C_Tx_Clock_command(pause_clock);while(switch_3_down);}break;
 
@@ -80,13 +82,15 @@ case 'B':if(switch_1_down){display_mode = 0;I2C_Tx_Clock_command(display_clock);
 case 'C':if(switch_3_down){display_mode = 0; I2C_Tx_Clock_command(display_clock);while(switch_3_down);}break;
 
 
-case 1: if (switch_1_down){I2C_Tx_Clock_command(increment_seconds);Timer_T0_10mS_delay_x_m(20);}
-    if (switch_3_down){I2C_Tx_Clock_command(increment_minutes);Timer_T0_10mS_delay_x_m(20);}
+case 1: if (switch_1_down){I2C_Tx_Clock_command(increment_seconds);Timer_T1_sub(T1_delay_200ms);}
+    if (switch_3_down){I2C_Tx_Clock_command(increment_minutes);Timer_T1_sub(T1_delay_200ms);}
     if(switch_2_down){ while (switch_2_down);display_mode = 0; }break;
     
-case 2: if (switch_1_down){I2C_Tx_Clock_command(decrement_seconds);Timer_T0_10mS_delay_x_m(20);}
-    if (switch_3_down){I2C_Tx_Clock_command(decrement_minutes);Timer_T0_10mS_delay_x_m(20);}
-    if(switch_2_down){while (switch_2_down);display_mode = 0; }break;}}}
+case 2: if (switch_1_down){I2C_Tx_Clock_command(decrement_seconds);Timer_T1_sub(T1_delay_200ms);}
+    if (switch_3_down){I2C_Tx_Clock_command(decrement_minutes);Timer_T1_sub(T1_delay_200ms);}
+    if(switch_2_down){while (switch_2_down);display_mode = 0; }break;}}
+    */
+    }
 
 
 
@@ -94,89 +98,90 @@ case 2: if (switch_1_down){I2C_Tx_Clock_command(decrement_seconds);Timer_T0_10mS
 
 void time_from_IO(void){
 char temp=0;
-setup_and_enable_PCI;
+set_up_PCI;
+enable_PCI;
 disable_pci_on_sw2;
-for(int m = 0; m<= 7; m++)start_time[m] = 0;
-start_time[7] = '_';I2C_Tx_8_byte_array(start_time);
+for(int m = 0; m<= 7; m++)digits[m] = 0;
+digits[7] = '_';Display_time(digits);
 sei();
 while(!(Data_Entry_complete));              //Line A. wait here for pci interrupts used to enter data
 Data_Entry_complete = 0;        
 for(int m = 0; m<=3; m++)               //Reverse "start_time" array
-{temp = start_time[m]; 
-start_time[m] = start_time[7-m];
-start_time[7-m] = temp;}}
+{temp = digits[m]; 
+digits[m] = digits[7-m];
+digits[7-m] = temp;}}
 
 
 /***********************************************************************************************************************/
 
 ISR(PCINT2_vect) {                    //input number: store keypresses in array -start_time
 if((switch_1_up) && (switch_3_up))return;
-Timer_T0_10mS_delay_x_m(5);               //Place delay here to trap bounce on sw release
-clear_PCI_on_sw1_and_sw3;
+Timer_T1_sub(T1_delay_50ms);               //Place delay here to trap bounce on sw release
+clear_PCI;
 
 if (switch_1_down){                   //Switch_1: used to scroll through the permissible                    
 switch(digit_num){                    //characters for each digit on the display
 case 7: 
-switch (start_time[7]){                 //Enter Digit 7 hours 0,1 or 2
-case '0': case '1': start_time[7]++; break;
-case '2': start_time[7] = '0';break;
-default:start_time[7]='0';break; }break;
+switch (digits[7]){                 //Enter Digit 7 hours 0,1 or 2
+case '0': case '1': digits[7]++; break;
+case '2': digits[7] = '0';break;
+default:digits[7]='0';break; }break;
 
 case 6:
-if (start_time[7]== '2'){               //Enter Digit 6 hours 0,1,2 or 3
-switch (start_time[6]){
-case '0': case '1': case '2': start_time[6]++; break;
-case '3': start_time[6] = '0';break;
-default:start_time[6]='0';break;}} 
+if (digits[7]== '2'){               //Enter Digit 6 hours 0,1,2 or 3
+switch (digits[6]){
+case '0': case '1': case '2': digits[6]++; break;
+case '3': digits[6] = '0';break;
+default:digits[6]='0';break;}} 
 
 else                          //Enter Digit 6 hours 0 to 9
-switch (start_time[6]){
-case '_': case '9': start_time[6] = '0'; break;
-default:start_time[6]++;break;}
+switch (digits[6]){
+case '_': case '9': digits[6] = '0'; break;
+default:digits[6]++;break;}
 break;
 
 case 4:                         //Enter Digit 4 minutes 0 to 5
-switch (start_time[4]){
+switch (digits[4]){
 case '0': case '1': case '2':
-case '3': case '4': start_time[4]++; break;
-case '5': start_time[4] = '0'; break;
-default:start_time[4]='0';break;} break;
+case '3': case '4': digits[4]++; break;
+case '5': digits[4] = '0'; break;
+default:digits[4]='0';break;} break;
 
 
 case 3:                         //Enter Digit 3 minutes 0 to 9
-switch (start_time[3]){
-case '_': case '9': start_time[3] = '0'; break;
-default:start_time[3]++;break;} break;
+switch (digits[3]){
+case '_': case '9': digits[3] = '0'; break;
+default:digits[3]++;break;} break;
 
 
 case 1:                         //Enter Digit 1 seconds 0 to 5
-switch (start_time[1]){
+switch (digits[1]){
 case '0': case '1': case '2':
-case '3': case '4': start_time[1]++; break;
-case '5': start_time[1] = '0'; break;
-default:start_time[1]='0';break;} break;
+case '3': case '4': digits[1]++; break;
+case '5': digits[1] = '0'; break;
+default:digits[1]='0';break;} break;
 
 case 0:                         //Enter Digit 0 secondes 0 to 9
-switch (start_time[0]){
-case '_': case '9': start_time[0] = '0'; break;
-default:start_time[0]++;break;} break;}
+switch (digits[0]){
+case '_': case '9': digits[0] = '0'; break;
+default:digits[0]++;break;} break;}
 
-I2C_Tx_8_byte_array(start_time);}           //Keep display up-to date 
+Display_time(digits);}           //Keep display up-to date 
       
 
 
 if (switch_3_down){                   //Switch_3: Used to select a character and move onto the next digit
 switch(digit_num){
 case 7: case 4: case 1:
-digit_num--;start_time[digit_num] = '_';break;
-case 6: case 3: digit_num -=2; start_time[digit_num] = '_'; break;
+digit_num--;digits[digit_num] = '_';break;
+case 6: case 3: digit_num -=2; digits[digit_num] = '_'; break;
 case 0: digit_num = 7; Data_Entry_complete = 1;
 disable_pci_on_sw1_and_sw3;
-start_time[2] = ' ';
-start_time[5] = ' ';
-while(switch_3_down);Timer_T0_10mS_delay_x_m(10); 
+digits[2] = ' ';
+digits[5] = ' ';
+while(switch_3_down);Timer_T1_sub(T1_delay_100ms); 
 break;}
-I2C_Tx_8_byte_array(start_time);}}            //return to Line A when sw1 is released
+Display_time(digits);}}            //return to Line A when sw1 is released
 
 
 
