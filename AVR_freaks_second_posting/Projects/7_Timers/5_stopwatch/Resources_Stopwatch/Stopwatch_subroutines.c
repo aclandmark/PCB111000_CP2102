633aa435ba4671ba390017cfdef3c21661534b74
aclandmark <mark.osborne15@btinternet.com>
2022-05-06 15:45:00 +0100

    Update Stopwatch_subroutines.c

--- AVR_freaks_second_posting/Projects/7_Timers/5_stopwatch/Resources_Stopwatch/Stopwatch_subroutines.c
+++ AVR_freaks_second_posting/Projects/7_Timers/5_stopwatch/Resources_Stopwatch/Stopwatch_subroutines.c
@@ -7,14 +7,14 @@ void initialise_T2(void){
 ASSR = (1 << AS2); 
 TCNT2 = 0;
 TCCR2A = 0;
-TCCR2B |= (1 << CS20) | (1 << CS21);
+TCCR2B |= (1 << CS21);
 OCR2B = 0;}
 
 
 void start_clock(void){
 tick_counter = 0;
 TCNT2 = 0;
-OCR2A = 102; 
+OCR2A = 41; 
 TIMSK2 |= (1 << OCIE2A);}
 
 
@@ -22,7 +22,7 @@ ISR (TIMER2_COMPA_vect){ char string[5];
   OCR2A += 102;
   clock_tick += 1;
   tick_counter += 1;
-  if(tick_counter == 9){tick_counter = -1; OCR2A += 4;}}
+  if(tick_counter == 99){tick_counter = -1; OCR2A -= 4;}}
   
   
   
