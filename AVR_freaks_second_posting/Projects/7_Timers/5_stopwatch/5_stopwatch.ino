cdc3d09312f24372ff1f15324657b45a4a02acd5
aclandmark <mark.osborne15@btinternet.com>
2022-05-07 13:27:24 +0100

    Update 5_stopwatch.ino

--- AVR_freaks_second_posting/Projects/7_Timers/5_stopwatch/5_stopwatch.ino
+++ AVR_freaks_second_posting/Projects/7_Timers/5_stopwatch/5_stopwatch.ino
@@ -4,6 +4,16 @@
 
 
 
+#define deci_secs_from_mini_OS \
+{deci_sec_counter = 0;\
+One_wire_Tx_char = 'M'; UART_Tx_1_wire();\
+for(int m = 0; m <= 3; m++){\
+UART_Rx_1_wire(); deci_secs_byte[m] = One_wire_Rx_char;}\
+\
+for(int m = 0; m <= 3; m++){\
+deci_sec_counter = deci_sec_counter << 8;\
+deci_sec_counter |= deci_secs_byte[m];}}
+
  
 #include "Stopwatch_header.h"
 
@@ -24,7 +34,6 @@ initialise_T2();
 sei();
 
 clear_display;                       
-//time_from_IO();
 
 while(switch_2_up);
 
@@ -41,89 +50,47 @@ centi_sec_couter_to_display;
 
 
 
-/***********************************************************************************************************************/
-/*void time_from_IO(void){
-set_up_PCI;
-enable_PCI;
-disable_pci_on_sw2;
-for(int m = 0; m<= 7; m++)digits[m] = 0;
-while(!(Data_Entry_complete));                          //Line A. wait here for pci interrupts used to enter data
-Data_Entry_complete = 0;
-disable_pci_on_sw1_and_sw3;}*/
-
-
 
 /***********************************************************************************************************************/
-/*ISR(PCINT2_vect) {                                    //input number: store keypresses in array -start_time
-if((switch_1_up) && (switch_3_up))return;
+ISR(PCINT2_vect) {  }                                  //input number: store keypresses in array -start_time
 
-if (switch_1_down){                                 //Switch_1: used to scroll through the permissible                    
-switch(digit_num){                                  //characters for each digit on the display
-case 7: 
-switch (digits[7]){                                 //Enter Digit 7 hours 0,1 or 2
-case '0': case '1': digits[7]++; break;
-case '2': digits[7] = '0';break;
-default:digits[7]='0';break; }break;
-
-case 6:
-if (digits[7]== '2'){                               //Enter Digit 6 hours 0,1,2 or 3
-switch (digits[6]){
-case '0': case '1': case '2': digits[6]++; break;
-case '3': digits[6] = '0';break;
-default:digits[6]='0';break;}} 
-
-else                                                //Enter Digit 6 hours 0 to 9
-{switch (digits[6]){
-case '9': digits[6] = '0'; break;
-default:digits[6]++;break;}}
-break;
-
-case 5:                                             //Enter Digit 4 minutes 0 to 5
-switch (digits[5]){
-case '0': case '1': case '2':
-case '3': case '4': digits[5]++; break;
-case '5': digits[5] = '0'; break;
-default:digits[5]='0';break;} break;
-
-
-case 4:                                             //Enter Digit 3 minutes 0 to 9
-switch (digits[4]){
-case '9': digits[4] = '0'; break;
-default:digits[4]++;break;} break;
-
-
-case 3:                                             //Enter Digit 1 seconds 0 to 5
-switch (digits[3]){
-case '0': case '1': case '2':
-case '3': case '4': digits[3]++; break;
-case '5': digits[3] = '0'; break;
-default:digits[3]='0';break;} break;
-
-case 2:                                         //Enter Digit 0 secondes 0 to 9
-switch (digits[2]){
-case '9': digits[2] = '0'; break;
-default:digits[2]++;break;} break;}}
-
-
-if (switch_3_down){                                             //Switch_3: Used to select a character and move onto the next digit
-
-digit_num -=1;digits[digit_num] = '0'; 
-if (digit_num == 1){
-digits[1] = '0';
-digits[0] = '0';
-Data_Entry_complete = 1;
-disable_pci_on_sw1_and_sw3;
-while(switch_3_down);}}
-
-
-disable_pci_on_sw1_and_sw3;
-sei();
-Display_time(digits); 
-Timer_T1_sub(T1_delay_10ms);
-enable_PCI;
-disable_pci_on_sw2;
-clear_PCI;}*/
   
+
+
+void initialise_T2(void){
+ASSR = (1 << AS2); 
+TCNT2 = 0;
+TCCR2A = 0;
+TCCR2B |= (1 << CS21);
+OCR2B = 0;}
+
+
+void start_clock(void){
+tick_counter = 0;
+TCNT2 = 0;
+OCR2A = 41; 
+TIMSK2 |= (1 << OCIE2A);}
+
+
+ISR (TIMER2_COMPA_vect){ 
+centi_sec_counter += 1;
+  OCR2A += 41;
+  clock_tick += 1;
+  tick_counter += 1;
+  if(tick_counter == 99){tick_counter = -1; OCR2A -= 4;}}
+  
+  
+  
+  
+  
+/****************************************************************************************************************/
+void deci_Seconds_to_display(long num){
+One_wire_Tx_char = 'L';                           //Command 'C' indicates the a long number will be sent
+UART_Tx_1_wire();
+for(int m = 0; m <= 3; m++){
+One_wire_Tx_char = num >> (8 * (3 - m));        //Split the number into 4 chars
+UART_Tx_1_wire();}}                             //and send them individually
+
 
 
 
