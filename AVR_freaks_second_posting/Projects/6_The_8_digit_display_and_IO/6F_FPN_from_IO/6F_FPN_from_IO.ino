d06f16d5ea04fb9ae105b6f30efe40c8541bdec4
aclandmark <mark.osborne15@btinternet.com>
2022-05-11 17:08:08 +0100

    Update 6F_FPN_from_IO.ino

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6F_FPN_from_IO/6F_FPN_from_IO.ino
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6F_FPN_from_IO/6F_FPN_from_IO.ino
@@ -22,11 +22,12 @@ SW3 PORTD7 PCINT23
  
 #include "FPN_from_IO_header.h"
 
+#define shift_digits_left {for (int n = 0; n < 7; n++){display_buffer[7-n] = display_buffer[6-n];}}
 
 
 volatile int digit_num = 7;
-char digits[8];
-volatile char Data_Entry_complete=0;
+char display_buffer[8];
+//volatile char Data_Entry_complete=0;
 
 
 
@@ -37,9 +38,9 @@ long deci_sec_counter = 0;
 setup_328_HW;
 initialise_T2();
 sei();
-
+Data_Entry_complete=0;
 clear_display;                       
-time_from_IO();
+/*time_from_IO();
 
 while(switch_2_up);
 
@@ -63,7 +64,11 @@ deci_Seconds_to_display(deci_sec_counter);
 _delay_ms(25); }
 while(switch_2_up);
 TIMSK2 |= (1 << OCIE2A);}
-}} 
+}*/
+
+} 
+
+
 
 
 
@@ -75,95 +80,105 @@ float f_number;
 float * Flt_ptr_local;
 char * Char_ptr_local;
 
-for(int m = 0; m <= 7; m++){data_buff[m] = 0;}              //Clear display
+for(int m = 0; m <= 7; m++){display_buffer[m] = 0;}                //Clear display
 
-
-num_type = FPN;                               //Floatng point
+//num_type = FPN;                                               //Floatng point
 
 Flt_ptr_local = &f_number;
 Char_ptr_local = (char*)&f_number;
 
-setup_and_enable_PCI;
-disable_pci_on_sw3;
-clear_display();
+set_up_PCI;
+enable_PCI_on_sw1_and_sw2;
 
-do{                                     //Repeat untill FPN string entry is complete
+clear_display;
+
+do{                                                             //Repeat untill FPN string entry is complete
 while((!(Data_Entry_complete)) && (!(digit_entry)));            //Wait for input from IO 
 
-enable_pci_on_sw3;
+enable_PCI_on_sw3;
 
 digit_entry = 0;
 }while(!(Data_Entry_complete));
 Data_Entry_complete = 0;
 
-cr_keypress = 1;                              //Entry of FP string complete 
-float_string_to_display();                          //Acquire FP number from display driver
+cr_keypress = 1;                                              //Entry of FP string complete 
+Send_float_num_string;                                      //Acquire FP number from display driver
 cr_keypress = 0;
 
-TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT);                        //Activate TWI and wait for contact from display pcb 
-while (!(TWCR & (1 << TWINT)));
+One_wire_Tx_char = 'E'; UART_Tx_1_wire();
+for(int m = 0; m <= 3; m++){
+UART_Rx_1_wire(); *Char_ptr_local = One_wire_Rx_char;
+Char_ptr_local += 1;}
 
-*Char_ptr_local =  byte(receive_byte_with_Ack());  Char_ptr_local += 1; //Build up the number as each byte is received
-*Char_ptr_local =  byte(receive_byte_with_Ack());  Char_ptr_local += 1;  
-*Char_ptr_local =  byte(receive_byte_with_Ack());  Char_ptr_local += 1;      
-*Char_ptr_local =  byte(receive_byte_with_Nack());    
 f_number = *Flt_ptr_local;
-TWCR = (1 << TWINT);
-disable_pci_on_sw1_and_sw2;
-disable_pci_on_sw3;
+
+disable_PCI_on_sw1_and_sw2;
+disable_PCI_on_sw3;
 
 return f_number;}
 
 
 /********************************************************************************************************/
-ISR(PCINT0_vect){                              //Press SW3 for entry of complete FPN 
+ISR(PCINT2_vect){
 char disp_bkp[8];
-if (switch_2_down){clear_display();return;}               //No backspace but dispaly and be re-initialised
 
-if(switch_3_up)return;                            //Ignore switch release
+if((switch_1_up) && (switch_2_up) && (switch_3_up))return;
+
+if(switch_3_down){                                        //End of data entry
+if (switch_2_down){clear_display;return;}               //No backspace but dispaly can be re-initialised
 
 for(int m = 0; m<=7; m++)
-{disp_bkp[m]=data_buff[m]; data_buff[m]= 0;}
-float_string_to_display();
-Timer_T0_10mS_delay_x_m(50);                        //Flash display
+{disp_bkp[m]=display_buffer[m]; display_buffer[m]= 0;}
+Send_float_num_string;
+Timer_T2_10mS_delay_x_m(50);                                //Flash display
 for(int m = 0; m<=7; m++)
-{data_buff[m]=disp_bkp[m];}
+{display_buffer[m]=disp_bkp[m];}
 digit_entry = 1;
 Data_Entry_complete=1;
-float_string_to_display();
+Send_float_num_string;
 while(switch_3_down);                           //Pause update of display
-_delay_ms(250);}                              //Delay for switch bounce
+_delay_ms(250);
+return;}
 
+
+while(switch_1_down)
+{scroll_display_zero();                           //Hold SW1 down to scroll throuh the availble chars (0-9, E anf -)
+Timer_T2_10mS_delay_x_m(20);}
+
+while(switch_3_down);enable_PCI_on_sw3;                   //Wait for SW3 to be released (may have been pressed to select dp)
+if(switch_2_down)shift_display_left();                    //Press SW2 to accept the next char and shift the display
+Timer_T2_10mS_delay_x_m(20);
+clear_PCI;}                                              //was clear_PCI_on_sw1_and_sw2;}
 
 
 
 
 /********************************************************************************************************/
-ISR(PCINT2_vect){
+/*ISR(PCINT2_vect){                                         //PCI on SW1 and SW2
 if((switch_1_up) && (switch_2_up))return;
 while(switch_1_down)
 {scroll_display_zero();                           //Hold SW1 down to scroll throuh the availble chars (0-9, E anf -)
 Timer_T0_10mS_delay_x_m(20);}
-while(switch_3_down);enable_pci_on_sw3;                   //Wait for SW3 to be released (may have been pressed to select dp)
+while(switch_3_down);enable_PCI_on_sw3;                   //Wait for SW3 to be released (may have been pressed to select dp)
 if(switch_2_down)shift_display_left();                    //Press SW2 to accept the next char and shift the display
 Timer_T0_10mS_delay_x_m(20);
-clear_PCI_on_sw1_and_sw2;}
+clear_PCI;}  */                                                //was clear_PCI_on_sw1_and_sw2;}
 
 
   
 /********************************************************************************************************/
 void shift_display_left(void){
 
-if(data_buff[0] & 0x80)dp_control = 1;                    //No more decimal points allowed
-if (data_buff[0] == 'e')
+if(display_buffer[0] & 0x80)dp_control = 1;                    //No more decimal points allowed
+if (display_buffer[0] == 'e')
 {exp_control = 1; neg_sign = 0;}                      //Negative exponent allowed
 else neg_sign = 1;                              //FP number can have one negative sign 
 
 scroll_control = (exp_control << 2) | (dp_control << 1) | neg_sign;   
 
 shift_digits_left; 
-data_buff[0] = '0'; 
-float_string_to_display();
+display_buffer[0] = '0'; 
+Send_float_num_string;
 digit_entry = 1;}
 
 
@@ -173,63 +188,63 @@ digit_entry = 1;}
 void scroll_display_zero(void){
 unsigned char data_zero;
 
-if(num_type == int_num){
-switch (data_buff[0]){
-case '9': if(!(data_buff[1])) 
-data_buff[0] = '-'; 
-else data_buff[0] = '0'; break;
-case '-': data_buff[0] = '0'; break;
-default: data_buff[0] += 1;break;}
+/*if(num_type == int_num){
+switch (display_buffer[0]){
+case '9': if(!(display_buffer[1])) 
+display_buffer[0] = '-'; 
+else display_buffer[0] = '0'; break;
+case '-': display_buffer[0] = '0'; break;
+default: display_buffer[0] += 1;break;}
 
 int_string_to_display();
-return;}
+return;}*/
 
 
-disable_pci_on_sw3;
+disable_PCI_on_sw3;
 
-data_zero = data_buff[0];
+data_zero = display_buffer[0];
 
-if (switch_3_up){data_buff[0] &= (~(0x80));             //Decimal point dissabled
+if (switch_3_up){display_buffer[0] &= (~(0x80));             //Decimal point dissabled
 
-switch (data_buff[0]){
+switch (display_buffer[0]){
 case '9':
 switch(scroll_control){ 
-case 0: data_buff[0] = '-'; break;                    //Initial state scroll 0-9 -ve 0...... dp is enabled
-case 1: data_buff[0] = '0'; break;                    //First char entered -ve sign dissabled  dp enabled
-case 3: data_buff[0] = 'e'; break;                    //Decimal point entered.  Dissable DP. Enable E.
-case 6: data_buff[0] = '-'; break;                    //E entered. Enable -ve sign
-case 7: data_buff[0] = '0'; break;}break;               //Dissable -ve sign
+case 0: display_buffer[0] = '-'; break;                    //Initial state scroll 0-9 -ve 0...... dp is enabled
+case 1: display_buffer[0] = '0'; break;                    //First char entered -ve sign dissabled  dp enabled
+case 3: display_buffer[0] = 'e'; break;                    //Decimal point entered.  Dissable DP. Enable E.
+case 6: display_buffer[0] = '-'; break;                    //E entered. Enable -ve sign
+case 7: display_buffer[0] = '0'; break;}break;               //Dissable -ve sign
 
 case '-':
 switch(scroll_control){
-case 0: data_buff[0] = '0'; break;                    //Initial state
-case 6: data_buff[0] = '0'; break;}break;
+case 0: display_buffer[0] = '0'; break;                    //Initial state
+case 6: display_buffer[0] = '0'; break;}break;
 
 case 'e':
 switch(scroll_control){
-case 0: data_buff[0] = '0'; break;                    //Initial state
-case 1: data_buff[0] = '0'; break;    
-case 3: data_buff[0] = '0'; break;}break;
+case 0: display_buffer[0] = '0'; break;                    //Initial state
+case 1: display_buffer[0] = '0'; break;    
+case 3: display_buffer[0] = '0'; break;}break;
 
-default: data_buff[0] += 1; break;}}
+default: display_buffer[0] += 1; break;}}
 
-if((switch_3_down) && (!(dp_control))){data_buff[0] |= 0x80;      //Decimal point enabled 
+if((switch_3_down) && (!(dp_control))){display_buffer[0] |= 0x80;      //Decimal point enabled 
 
-switch (data_buff[0] & (~(0x80))){
+switch (display_buffer[0] & (~(0x80))){
 case '9': 
-data_buff[0] = ('0' | 0x80);break;
-default:  data_buff[0] += 1;break;}}
+display_buffer[0] = ('0' | 0x80);break;
+default:  display_buffer[0] += 1;break;}}
 
-float_string_to_display();}
+Send_float_num_string;}
 
 
 
 
 /****************************************************************************************************/
-void clear_display(void){
+/*void clear_display(void){
 for(int m = 0; m<=7; m++)
-{data_buff[m]= 0;}
-data_buff[0] = '0';
+{display_buffer[m]= 0;}
+display_buffer[0] = '0';
 dp_control = 0;       
 neg_sign = 0;           
 exp_control = 0;
@@ -237,17 +252,17 @@ cr_keypress = 0;
 Data_Entry_complete = 0;                          //Set to 1 when FP number entry is complete
 digit_entry = 0;                              //Set to 1 when digit is selected
 scroll_control = 0;
-float_string_to_display();}
+float_string_to_display();}*/
 
 
 
   /****************************************************************************************************/
-long Int_number_from_IO(void){
+/*long Int_number_from_IO(void){
 
 char keypress = 0;
 long I_number;
 
-for(int n = 0; n<=8; n++) data_buff[n] = 0;                //Clear display
+for(int n = 0; n<=8; n++) display_buffer[n] = 0;                //Clear display
 
 num_type = int_num;                               //Integer
 
@@ -256,7 +271,7 @@ Data_Entry_complete = 0;                          //Set to 1 when FP number entr
 digit_entry = 0;                              //Set to 1 when digit is selected
 
 cr_keypress = 0;                              //Set to 1 when atoi() conversion is required  
-data_buff[0] = '0';
+display_buffer[0] = '0';
 int_string_to_display();                          //Initialise display
 
 do{                                     //Repeat untill FPN string entry is complete
@@ -279,9 +294,9 @@ I_number = (I_number << 8) + byte(receive_byte_with_Ack());
 I_number = (I_number << 8) + byte(receive_byte_with_Nack());    
 
 TWCR = (1 << TWINT);
-disable_pci_on_sw1_and_sw2;
-disable_pci_on_sw3;
-return I_number;}
+disable_PCI_on_sw1_and_sw2;
+disable_PCI_on_sw3;
+return I_number;}*/
 
 
 
