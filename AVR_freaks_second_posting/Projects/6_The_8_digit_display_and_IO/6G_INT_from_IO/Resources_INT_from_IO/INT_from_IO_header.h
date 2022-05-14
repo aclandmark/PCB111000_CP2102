847a93f1e042e830bbea955dae8eb64bf49bef97
aclandmark <mark.osborne15@btinternet.com>
2022-05-14 12:04:55 +0100

    Update INT_from_IO_header.h

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/Resources_INT_from_IO/INT_from_IO_header.h
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/Resources_INT_from_IO/INT_from_IO_header.h
@@ -1,32 +1,4 @@
 
-/************Define clock memory locations********************/
-/*#define HoursH digits[7]
-#define HoursL digits[6]
-#define MinsH digits[5]
-#define MinsL digits[4]
-#define SecsH digits[3]
-#define SecsL digits[2]
-#define deci_SecsH digits[1]
-#define deci_SecsL digits[0]*/
-
-
-
-/***********Define clock display functions and modes***************/
-/*#define reset_clock_1   {digits[0] = 0; for (int m = 1; m < 8; m++)digits[m] = '0'; }
-
-
-#define Display_time \
-{One_wire_Tx_char = 'J'; UART_Tx_1_wire();\
-for(int p = 0; p <= 7; p++){\
-switch (p){\
-  case 2:\
-  case 4:\
-  case 6: One_wire_Tx_char = digits[p] | 0x80; UART_Tx_1_wire(); break;\
-  default: One_wire_Tx_char = digits[p]; UART_Tx_1_wire(); break;}}}
-
-
-#define Inc_OS_time \
-{One_wire_Tx_char = 'K'; UART_Tx_1_wire();}*/
 
 
 /*********************************************************************/
@@ -62,6 +34,20 @@ for(int m = 0; m <= 3; m++){\
 UART_Rx_1_wire(); *Char_ptr = One_wire_Rx_char;\
 Char_ptr += 1;}\
 f_number = *Flt_ptr;
+
+
+
+
+
+#define Int_from_mini_OS \
+One_wire_Tx_char = 'E'; UART_Tx_1_wire();\
+for(int m = 0; m <= 3; m++){\
+UART_Rx_1_wire(); num_byte[m] = One_wire_Rx_char;}\
+\
+for(int m = 0; m <= 3; m++){\
+Long_Num_from_mini_OS = Long_Num_from_mini_OS << 8;\
+Long_Num_from_mini_OS |= num_byte[m];}
+
 
 
 
