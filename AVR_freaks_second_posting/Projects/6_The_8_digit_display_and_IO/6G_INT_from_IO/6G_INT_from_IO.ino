44bdc9593e6709e9ac9287572458f2f2e1f7afce
aclandmark <mark.osborne15@btinternet.com>
2022-05-14 20:20:23 +0100

    Update 6G_INT_from_IO.ino

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/6G_INT_from_IO.ino
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/6G_INT_from_IO.ino
@@ -1,6 +1,15 @@
 
 /*
- 
+Takes an integer number from the IO, converts it to floating point
+and then to the form A * 2^N where A is between 1 and 2 and N is an integer.
+
+Press 
+  SW1 to scroll through the digits
+  SW2 to shift the display left
+  SW3 to enter the number
+
+Then press SW1 and SW3 to dsisplay A and N
+
 Switch assignments:
     SW1 PORTD2 PCINT18 
     SW2 PORTD5 PCINT21
@@ -26,16 +35,14 @@ Long_num = Int_number_from_IO();
 
 FPN_1_num = (float)Long_num;
 
-
 if(FPN_1_num < 0){
 _delay_ms(250);
 FPN_1_num *= -1.0; 
 float_num_to_display(FPN_1_num);}
 
-
-if (FPN_1_num >= 1.0)                                   //Multiply or divide number by 2 untill it
-{twos_exp = 0; while (FPN_1_num >= 2.0)                 //is between 1 and 2 and adjust its twos_exp 
-{FPN_1_num = FPN_1_num/2.0; twos_exp += 1;}}                  //so that its value remains unchanged 
+if (FPN_1_num >= 1.0) 
+{twos_exp = 0; while (FPN_1_num >= 2.0) 
+{FPN_1_num = FPN_1_num/2.0; twos_exp += 1;}} 
 
 while(1){
 while(switch_1_up);
@@ -45,10 +52,7 @@ int_num_to_display(twos_exp);
 while(switch_1_up);
 float_num_to_display(pow(2, twos_exp) * FPN_1_num);
 while(switch_3_up);
-if(switch_2_down){SW_reset;}}
-
-
-} 
+if(switch_2_down){SW_reset;}}} 
 
 
 
@@ -60,37 +64,33 @@ char disp_bkp[8];
 
 if((switch_1_up) && (switch_2_up) && (switch_3_up))return;
 
-if(switch_3_down){                                        //End of data entry
+if(switch_3_down){ 
 if (switch_2_down){sei();clear_display;cli();
-
 while ((switch_3_down) || (switch_2_down));
-
 initialise_display;
 return; }
-
-
 
 for(int m = 0; m<=7; m++)
 {disp_bkp[m]=display_buffer[m]; display_buffer[m]= 0;}
 pause_PCI_and_Send_int_num_string;
-Timer_T2_10mS_delay_x_m(25);                                //Flash display
+Timer_T2_10mS_delay_x_m(25);     
 for(int m = 0; m<=7; m++)
 {display_buffer[m]=disp_bkp[m];}
 digit_entry = 1;
 Data_Entry_complete=1;
 pause_PCI_and_Send_int_num_string;
-while(switch_3_down);                                     //Pause update of display
+while(switch_3_down); 
 return;}
 
 
 while(switch_1_down)
-{scroll_display_zero();                                   //Hold SW1 down to scroll throuh the availble chars (0-9, E anf -)
+{scroll_display_zero(); 
 Timer_T2_10mS_delay_x_m(10);}
 
-while(switch_3_down);enable_PCI_on_sw3;                   //Wait for SW3 to be released (may have been pressed to select dp)
-if(switch_2_down)shift_display_left();                    //Press SW2 to accept the next char and shift the display
+while(switch_3_down);enable_PCI_on_sw3;  
+if(switch_2_down)shift_display_left();  
 Timer_T2_10mS_delay_x_m(10);
-clear_PCI;}                                              //was clear_PCI_on_sw1_and_sw2;}
+clear_PCI;}       
 
 
 
@@ -105,23 +105,19 @@ long Long_Num_from_mini_OS;
 
 set_up_PCI;
 enable_PCI_on_sw1_and_sw2;
-
 initialise_display;
 
-do{                                                           //Repeat untill FPN string entry is complete
+do{      
 while
-((!(Data_Entry_complete)) && (!(digit_entry)));            //Wait for input from IO 
-
+((!(Data_Entry_complete)) && (!(digit_entry)));            
 enable_PCI_on_sw3;
 
 digit_entry = 0;
 }while(!(Data_Entry_complete));
 Data_Entry_complete = 0;
 
-cr_keypress = 1;                              //Entry of FP string complete 
-
+cr_keypress = 1;  
 pause_PCI_and_Send_int_num_string;
-
 cr_keypress = 0;
 sei();
 Int_from_mini_OS;
