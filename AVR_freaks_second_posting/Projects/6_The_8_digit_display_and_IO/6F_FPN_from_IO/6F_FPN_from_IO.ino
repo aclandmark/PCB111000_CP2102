3229aa458d7b3683e4d800942ff0e9e9e777413d
aclandmark <mark.osborne15@btinternet.com>
2022-05-14 20:20:19 +0100

    Update 6F_FPN_from_IO.ino

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6F_FPN_from_IO/6F_FPN_from_IO.ino
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6F_FPN_from_IO/6F_FPN_from_IO.ino
@@ -1,11 +1,22 @@
 
+
 /*
- 
+Takes a scientific number from the IO and converts it to the form A * 2^N 
+where A is between 1 and 2 and N is an integer.
+
+Press 
+  SW1 to scroll through the digits
+  SW2 to shift the display left
+  SW3 to enter the number
+
+Then press SW1 and SW3 to dsisplay A and N
+
 Switch assignments:
     SW1 PORTD2 PCINT18 
     SW2 PORTD5 PCINT21
     SW3 PORTD7 PCINT23
 */
+
 
  
 #include "FPN_from_IO_header.h"
@@ -29,9 +40,9 @@ FPN_1_num *= -1.0;
 float_num_to_display(FPN_1_num);}
 
 
-if (FPN_1_num >= 1.0)                                   //Multiply or divide number by 2 untill it
-{twos_exp = 0; while (FPN_1_num >= 2.0)                 //is between 1 and 2 and adjust its twos_exp 
-{FPN_1_num = FPN_1_num/2.0; twos_exp += 1;}}                  //so that its value remains unchanged 
+if (FPN_1_num >= 1.0)                                           //Multiply or divide number by 2 untill it
+{twos_exp = 0; while (FPN_1_num >= 2.0)                         //is between 1 and 2 and adjust its twos_exp 
+{FPN_1_num = FPN_1_num/2.0; twos_exp += 1;}}                    //so that its value remains unchanged 
 
 if (FPN_1_num < 1.0)
 {twos_exp = 0; while (FPN_1_num < 1.0)
@@ -67,18 +78,16 @@ enable_PCI_on_sw1_and_sw2;
 
 initialise_display;
 
-do{                                                         //Repeat untill FPN string entry is complete
+do{                                                               //Repeat untill FPN string entry is complete
 while
-((!(Data_Entry_complete)) && (!(digit_entry)));            //Wait for input from IO 
-
+((!(Data_Entry_complete)) && (!(digit_entry)));                   //Wait here for input from IO 
 enable_PCI_on_sw3;
-
 digit_entry = 0;
 }while(!(Data_Entry_complete));
 Data_Entry_complete = 0;
 
-cr_keypress = 1;                                          //Entry of FP string complete 
-pause_PCI_and_Send_float_num_string;                     //Acquire FP number from display driver
+cr_keypress = 1;                                                  //Entry of FP string complete 
+pause_PCI_and_Send_float_num_string;                              //Acquire FP number from display driver
 cr_keypress = 0;
 
 f_number_from_mini_OS;
@@ -95,11 +104,9 @@ char disp_bkp[8];
 
 if((switch_1_up) && (switch_2_up) && (switch_3_up))return;
 
-if(switch_3_down){                                        //End of data entry
+if(switch_3_down){                                              //End of data entry
 if (switch_2_down){sei();clear_display;cli();
-
 while ((switch_3_down) || (switch_2_down));
-
 initialise_display;
 return; }
 
@@ -108,30 +115,27 @@ return; }
 for(int m = 0; m<=7; m++)
 {disp_bkp[m]=display_buffer[m]; display_buffer[m]= 0;}
 pause_PCI_and_Send_float_num_string;
-Timer_T2_10mS_delay_x_m(25);                                //Flash display
+Timer_T2_10mS_delay_x_m(25);                                      //Flash display
 for(int m = 0; m<=7; m++)
 {display_buffer[m]=disp_bkp[m];}
 digit_entry = 1;
 Data_Entry_complete=1;
 pause_PCI_and_Send_float_num_string;
-while(switch_3_down);                                     //Pause update of display
+while(switch_3_down);                                           //Pause update of display
 return;}
 
 
 while(switch_1_down)
-{scroll_display_zero();                                   //Hold SW1 down to scroll throuh the availble chars (0-9, E anf -)
+{scroll_display_zero();                                         //Hold SW1 down to scroll throuh the availble chars (0-9, E and -)
 Timer_T2_10mS_delay_x_m(10);}
 
-while(switch_3_down);enable_PCI_on_sw3;                   //Wait for SW3 to be released (may have been pressed to select dp)
-if(switch_2_down)shift_display_left();                    //Press SW2 to accept the next char and shift the display
+while(switch_3_down);enable_PCI_on_sw3;                         //Wait for SW3 to be released (may have been pressed to select dp)
+if(switch_2_down)shift_display_left();                          //Press SW2 to accept the next char and shift the display
 Timer_T2_10mS_delay_x_m(10);
-clear_PCI;}                                              //was clear_PCI_on_sw1_and_sw2;}
+clear_PCI;} 
 
 
 
-
-
-/********************************************************************************************************/
 
 
 /************************************************************************************************************/
