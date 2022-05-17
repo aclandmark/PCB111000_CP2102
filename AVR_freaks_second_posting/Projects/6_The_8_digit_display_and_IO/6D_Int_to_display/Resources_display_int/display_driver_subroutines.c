8d3d2cb92fb72922a35feb36267ff0b56f47f97d
aclandmark <mark.osborne15@btinternet.com>
2022-05-17 19:48:50 +0100

    Update display_driver_subroutines.c

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_Int_to_display/Resources_display_int/display_driver_subroutines.c
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_Int_to_display/Resources_display_int/display_driver_subroutines.c
@@ -29,27 +29,9 @@ Long_Num_from_mini_OS |= num_byte[m];}
 
 
 
-
-void int_num_to_display(long);
-void float_num_to_display(float);
-
 void Check_num_for_to_big_or_small(float);
 
 
-
-
-
-/******************************************************************************************************************************************/
-void int_num_to_display(long Int_num){
-pause_pin_change_interrupt_on_PC5;
-
-One_wire_Tx_char = 'C';                                 //Command 'C' indicates the a long number will be sent
-UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){                            //Split the number into 4 chars
-One_wire_Tx_char = Int_num >> ((3-m) * 8);              //and send them individually
-UART_Tx_1_wire(); 
-}
-reinstate_pin_change_interrupt_on_PC5;}
 
 
 
@@ -68,6 +50,21 @@ One_wire_Tx_char = *Char_ptr;							//and send them individually
 UART_Tx_1_wire();	
 Char_ptr += 1;}
 reinstate_pin_change_interrupt_on_PC5;}
+
+
+
+/******************************************************************************************************************************************/
+void int_num_to_display(long Int_num){
+pause_pin_change_interrupt_on_PC5;
+
+One_wire_Tx_char = 'C';                                 //Command 'C' indicates the a long number will be sent
+UART_Tx_1_wire();
+for(int m = 0; m <= 3; m++){                            //Split the number into 4 chars
+One_wire_Tx_char = Int_num >> ((3-m) * 8);              //and send them individually
+UART_Tx_1_wire(); 
+}
+reinstate_pin_change_interrupt_on_PC5;}
+
 
 
 
