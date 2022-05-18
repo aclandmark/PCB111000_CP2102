813aff2ae458de243beed6b0f3d4844ce81b6833
aclandmark <mark.osborne15@btinternet.com>
2022-05-18 17:32:59 +0100

    Update numerical_entry_header.h

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6C_Numerical_entry/numerical_entry_header.h
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6C_Numerical_entry/numerical_entry_header.h
@@ -28,7 +28,9 @@ comms_cal;\
 set_up_pin_change_interrupt_on_PC5;\
 setup_one_wire_comms;\
 set_up_activity_leds;\
-sei();
+sei();\
+Serial.begin(115200);\
+while (!Serial);
 
 //The reset control switch is connected to PC5
 
@@ -82,3 +84,7 @@ if((User_response == 'R') || (User_response == 'r'))break;} Serial.write("\r\n")
 #include "Resources_numerical_enty\One_wire_transactions.c"
 #include "Resources_numerical_enty\Arduino_IO_and_Timer.c"
 #include "Resources_numerical_enty\numerical_entry_subroutines.c"
+
+
+
+/************************************************************************************************************************************/
