6d8362ddaa84fb117c539be017effafb5b0d7707
aclandmark <mark.osborne15@btinternet.com>
2022-05-18 11:17:51 +0100

    Update display_int_header.h

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_Int_to_display/display_int_header.h
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_Int_to_display/display_int_header.h
@@ -29,7 +29,9 @@ set_up_pin_change_interrupt_on_PC5;\
 setup_one_wire_comms;\
 \
 set_up_activity_leds;\
-sei();
+sei();\
+Serial.begin(115200);\
+while (!Serial);
 
 //The reset control switch is connected to PC5
 
