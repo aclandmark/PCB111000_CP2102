4325c6a1a33e30fb89f6c40b68ed4b9e72fb55ba
aclandmark <mark.osborne15@btinternet.com>
2022-05-19 21:10:30 +0100

    Update Division_subroutine_header.h

--- AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/3_Division_subroutine/Division_subroutine_header.h
+++ AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/3_Division_subroutine/Division_subroutine_header.h
@@ -32,7 +32,9 @@ comms_cal;\
 set_up_pin_change_interrupt_on_PC5;\
 \
 setup_one_wire_comms;\
-set_up_activity_leds;
+set_up_activity_leds;\
+sei();\
+setup_PC_comms(0,16);
 
 
 //The reset control switch is connected to PC5
