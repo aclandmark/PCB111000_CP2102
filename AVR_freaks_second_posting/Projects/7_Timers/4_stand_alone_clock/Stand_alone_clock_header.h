b80ec26cd68aba5521e91f7cadeba97b8eb9e955
aclandmark <mark.osborne15@btinternet.com>
2022-05-20 15:28:19 +0100

    Update Stand_alone_clock_header.h

--- AVR_freaks_second_posting/Projects/7_Timers/4_stand_alone_clock/Stand_alone_clock_header.h
+++ AVR_freaks_second_posting/Projects/7_Timers/4_stand_alone_clock/Stand_alone_clock_header.h
@@ -32,7 +32,9 @@ comms_cal;\
 set_up_pin_change_interrupt_on_PC5;\
 setup_one_wire_comms;\
 \
-set_up_activity_leds;
+set_up_activity_leds;\
+setup_PC_comms(0,16);\
+sei();
 
 
 //The reset control switch is connected to PC5  
