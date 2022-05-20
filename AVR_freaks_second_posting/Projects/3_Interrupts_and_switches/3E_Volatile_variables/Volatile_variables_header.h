4cecaaa949f6abe96e87e7cbcdbcd821f363c70c
aclandmark <mark.osborne15@btinternet.com>
2022-05-20 16:33:07 +0100

    Update Volatile_variables_header.h

--- AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3E_Volatile_variables/Volatile_variables_header.h
+++ AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3E_Volatile_variables/Volatile_variables_header.h
@@ -32,7 +32,8 @@ set_up_pin_change_interrupt_on_PC5;\
 \
 setup_one_wire_comms;\
 set_up_activity_leds;\
-sei();
+sei();\
+setup_PC_comms(0,16);
 
 //The reset control switch is connected to PC5  USART_init(0,16);
 
