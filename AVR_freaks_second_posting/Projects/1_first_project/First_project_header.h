6d1c8b43ef5de099fbfbabb9fc07b97a93b531a3
aclandmark <mark.osborne15@btinternet.com>
2022-05-21 12:41:43 +0100

    Update First_project_header.h

--- AVR_freaks_second_posting/Projects/1_first_project/First_project_header.h
+++ AVR_freaks_second_posting/Projects/1_first_project/First_project_header.h
@@ -27,7 +27,10 @@ comms_cal;\
 set_up_pin_change_interrupt_on_PC5;\
 \
 setup_one_wire_comms;\
-set_up_activity_leds;
+set_up_activity_leds;\
+sei();\
+setup_PC_comms(0,16);\
+_delay_ms(10);
 
 
 //The reset control switch is connected to PC5
