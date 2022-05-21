7762d4ad99415ccd5c39ee8ac6c043d45f9777c3
aclandmark <mark.osborne15@btinternet.com>
2022-05-21 11:57:30 +0100

    Update Pin_Change_Interrupt_header.h

--- AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3B_Pin_Change_Interrupt/Pin_Change_Interrupt_header.h
+++ AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3B_Pin_Change_Interrupt/Pin_Change_Interrupt_header.h
@@ -31,7 +31,8 @@ set_up_pin_change_interrupt_on_PC5;\
 setup_one_wire_comms;\
 set_up_activity_leds;\
 sei();\
-setup_PC_comms(0,16);
+setup_PC_comms(0,16);\
+_delay_ms(10);
 
 
 //The reset control switch is connected to PC5
