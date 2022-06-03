b9291e03d6f6d4f75083a1a6dd04a171b9193674
aclandmark <mark.osborne15@btinternet.com>
2022-06-03 20:20:07 +0100

    Update 8A_FPN_to_display_with_WDT.ino

--- AVR_freaks_second_posting/Projects/8_Resets/8A_FPN_to_display_with_WDT/8A_FPN_to_display_with_WDT.ino
+++ AVR_freaks_second_posting/Projects/8_Resets/8A_FPN_to_display_with_WDT/8A_FPN_to_display_with_WDT.ino
@@ -26,13 +26,19 @@ setup_328_HW_Arduino;
 
 Serial.write("\r\nEnter scientific number \
 & terminate with Return key.\r\n");
-
+wdt_enable(WDTO_120MS);
 
 Num_1 = Float_KBD_to_display(digits);                     //Data from keyboard
 while (1){
- 
+
 Sc_Num_to_PC(Num_1,1,5 ,'\r');                            //Send number to PC
-waitforkeypress();
+//waitforkeypress();
+while(1){
+if (isCharavailable_with_WDT(8)){
+Serial.read();
+break;}}
+
+
 Num_1 = pow(Num_1, 1.2);                                  //Do some arithmetic
 float_num_to_display(Num_1);                              //Sends result to the display
 }                                                         //Generates reset if result of arithmetic is too large or small
