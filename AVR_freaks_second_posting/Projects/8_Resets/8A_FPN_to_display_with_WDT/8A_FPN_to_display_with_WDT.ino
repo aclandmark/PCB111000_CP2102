3db462bc6e5ec3db01a1977e0a94ff0f4b57c089
aclandmark <mark.osborne15@btinternet.com>
2022-06-07 21:48:13 +0100

    Update 8A_FPN_to_display_with_WDT.ino

--- AVR_freaks_second_posting/Projects/8_Resets/8A_FPN_to_display_with_WDT/8A_FPN_to_display_with_WDT.ino
+++ AVR_freaks_second_posting/Projects/8_Resets/8A_FPN_to_display_with_WDT/8A_FPN_to_display_with_WDT.ino
@@ -10,7 +10,7 @@ Obviouly Arduino do not supply library functions to drive the display and theref
 bit of DIY programming is required.
 */
 
-
+volatile char next_duty_cycle = 1;
 
 #include "display_FPN_header_with_WDT.h"
 float Num_1;
@@ -22,6 +22,8 @@ float pi = 3.14159;
 int print_spaces;
 float amplitude;
 float duty_cycle;
+int num_time_slots = 25;         //50
+int num_harmonics = 30;           //60
 
 setup_328_HW_Arduino;
 set_up_PCI;
@@ -71,17 +73,35 @@ for (int n = 0; n < print_spaces; n++){Serial.write(' ');}
 Serial.write('|');_delay_ms(10);newline;}*/
 
 /************Pulse wave*********************/
-/*duty_cycle = 0.5;
+/*duty_cycle = 0.1 * 1.5;
 while(1){
-for(int m = 1; m <= 200; m++){amplitude = sin(duty_cycle ) * (cos(pi * (float)m/100.0));
-for(int p = 3; p < 80; p+=2) {amplitude += sin(duty_cycle * (float)p) * cos(pi*(float)p * (float)m/100.0)/(float) p;}
+for(int m = -25; m < 25; m++){amplitude = sin(duty_cycle ) * (cos(pi * (float)m/50.0));
+for(int p = 3; p < 40; p+=2) {amplitude += sin(duty_cycle * (float)p) * cos(pi*(float)p * (float)m/50.0)/(float) p;}
 print_spaces = 200 + (int)(100.0 * amplitude);
 for (int n = 0; n < print_spaces; n++){Serial.write(' ');}
-Serial.write('|');_delay_ms(10);newline;}}*/
+Serial.write('|');//_delay_ms(10);
+newline;}
+if(Serial.available()){duty_cycle = (float)(Serial.read()- '0')/10.0 * 1.5;}}
+*/
+
+
+duty_cycle = 0.1 * 1.5;
+while(1){
+for(int m = -num_time_slots/2; m < num_time_slots/2; m++)
+//for(int m = 1; m <= num_time_slots; m++)
+{amplitude = sin(duty_cycle ) * (cos(pi * (float)m/(float)num_time_slots));
+for(int p = 3; p < num_harmonics; p+=2) 
+{amplitude += sin(duty_cycle * (float)p) * cos(pi*(float)p * (float)m/(float)num_time_slots)/(float) p;}
+print_spaces = 200 + (int)(100.0 * amplitude);
+for (int n = 0; n < print_spaces; n++){Serial.write(' ');}
+Serial.write('|');_delay_ms(10);
+newline;}
+if(Serial.available()){duty_cycle = (float)(Serial.read()- '0')/10.0 * 1.5;}
+}
 
 
 
-for(int m = 1; m <= 400; m++){amplitude = sin(pi * (float)m/100.0); 
+/*for(int m = 1; m <= 400; m++){amplitude = sin(pi * (float)m/100.0); 
 for(int p = 2; p < 80; p++) {if (p%2) amplitude += sin(pi*(float)p * (float)m/100.0)/(float) p; 
 else  amplitude -= sin(pi*(float)p * (float)m/100.0)/(float) p;}
 
@@ -93,7 +113,7 @@ print_spaces = 200 + (int)(100.0 * amplitude);
 for (int n = 0; n < print_spaces; n++){Serial.write(' ');}
 Serial.write('|');
 //_delay_ms(10);
-newline;}
+newline;}*/
 
 
 
@@ -110,16 +130,10 @@ if(switch_1_down){
 Sc_Num_to_PC(Num_1,1,5 ,'\r');
 Num_1 = pow(Num_1, 1.2);                                  //Do some arithmetic
 float_num_to_display(Num_1);                              //Sends result to the display
-
 } 
-
 if(switch_2_down){while(1);}
 while(switch_3_up)wdr();
-
-
-
 enable_PCI_on_sw1_and_sw2;}                              //Generates reset if result of arithmetic is too large or small
-
 
 
 
