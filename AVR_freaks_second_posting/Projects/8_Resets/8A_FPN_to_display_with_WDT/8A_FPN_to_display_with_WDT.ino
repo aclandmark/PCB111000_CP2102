ec3b5abb4538135a076e485724eb10141f51dfdf
aclandmark <mark.osborne15@btinternet.com>
2022-06-13 19:51:44 +0100

    Update 8A_FPN_to_display_with_WDT.ino

--- AVR_freaks_second_posting/Projects/8_Resets/8A_FPN_to_display_with_WDT/8A_FPN_to_display_with_WDT.ino
+++ AVR_freaks_second_posting/Projects/8_Resets/8A_FPN_to_display_with_WDT/8A_FPN_to_display_with_WDT.ino
@@ -15,10 +15,10 @@ char digits[12];
 
 
 #include "display_FPN_header_with_WDT.h"
-float Num_1;
+float Num_1, Num_2;
+
 
 int main (void){
-
 
 float pi = 3.14159;
 int print_spaces;
@@ -31,13 +31,13 @@ int num_harmonics;             //60 30
 setup_328_HW_Arduino_plus;
 set_up_PCI;
 
-wdt_enable(WDTO_120MS);
+wdt_enable(WDTO_120MS); //120
 
 if(WDT_out_status == 2){Timer_T1_sub_with_interrupt(T1_delay_250ms);
 for(int p = 0; p <10; p++)newline;}
 
 
-if(WDT_out_status == 1){Serial.write('A');
+if(WDT_out_status == 1){
 enable_PCI_on_sw1;
  eeprom_write_byte((uint8_t*)(0x0),'1');
  eeprom_write_byte((uint8_t*)(0x1),45); 
@@ -48,21 +48,22 @@ enable_PCI_on_sw1;
 Serial.write("\r\nEnter scientific number \
 & terminate with Return key.\r\n");
 
-Num_1 = Float_KBD_to_display(digits);                     //Data from keyboard
-waitforkeypress_with_WDT;}
+//Num_1 = Float_KBD_to_display(digits);                     //Data from keyboard
+eeprom_write_byte((uint8_t*)(0x5),Float_KBD_to_display(digits));
+
+
+while(1){if((switch_2_down) || (switch_3_down))break; else wdr();}}
 
 duty_cycle = (float)(eeprom_read_byte((uint8_t*)(0x0))- '0')/10.0 * 1.5;
 num_time_slots = (eeprom_read_byte((uint8_t*)(0x2)) << 8) + eeprom_read_byte((uint8_t*)(0x1));
 num_harmonics = (eeprom_read_byte((uint8_t*)(0x4)) << 8) + eeprom_read_byte((uint8_t*)(0x3));
 
-
 for(int n = 0; n <= 100; n++){if (n<40)Serial.write (' '); else Serial.write('_');}
 newline;
-while(1){
 
-for(int m = 1; m; m++)
-{
-amplitude = sin(duty_cycle ) * (cos(pi * (float)m/(float)num_time_slots));
+while(1)
+{for(int m = 1; m; m++)
+{amplitude = sin(duty_cycle ) * (cos(pi * (float)m/(float)num_time_slots));
 for(int p = 3; p < num_harmonics; p+=2) 
 {amplitude += sin(duty_cycle * (float)p) * cos(pi*(float)p * (float)m/(float)num_time_slots)/(float) p;}
 print_spaces = 50 + (int)(100.0 * amplitude);
@@ -70,11 +71,7 @@ for (int n = 0; n < print_spaces; n++){Serial.write(' ');}
 Serial.write('|');_delay_ms(20);
 newline;
 if(m == num_time_slots/2) m = -num_time_slots/2;
-wdr();}
-}
-
-
-
+wdr();}}
 
 while(1)wdr();}
                                                         //Generates reset if result of arithmetic is too large or smallSW_reset;}
@@ -85,7 +82,6 @@ ISR(PCINT2_vect){
   if (switch_1_up)return;
   sei();
   disable_PCI_on_sw1;
- Signal_WDTout_with_interrupt; 
  
 if((switch_2_up) && (switch_3_up)){
   if(Serial.available()){eeprom_write_byte((uint8_t*)(0x0),Serial.read());}}
@@ -97,9 +93,23 @@ if((switch_2_up) && (switch_3_up)){
 if((switch_3_down) && (switch_2_up)){ data = PCI_triggers_data_from_PC(digits);
  eeprom_write_byte((uint8_t*)(0x3),data);
  eeprom_write_byte((uint8_t*)(0x4), (data >> 8));}
- 
-  clear_display;
-  SW_reset;}
+
+if((switch_2_down) && (switch_3_down)){             //FAILS after a SW_reset
+ //Num_2 = pow(Num_1, 1.2);
+Num_2 = pow(eeprom_read_byte((uint8_t*)(0x5)), 1.2);
+
+
+float_num_to_display_local(Num_2);
+//Sc_Num_to_PC(Num_2, 3, 3, '\r');
+//Num_1 = Num_2; 
+
+eeprom_write_byte((uint8_t*)(0x5),Num_2);
+
+Timer_T1_sub_with_interrupt(T1_delay_250ms);
+return;}
+
+Signal_WDTout_with_interrupt; 
+SW_reset;}
   
 
 
@@ -108,9 +118,7 @@ int PCI_triggers_data_from_PC(char * num_as_string)
 {char strln;
 strln = Serial.readBytesUntil('\r',num_as_string, 20);
 num_as_string[strln] = 0;
-return atoi(num_as_string);
-//eeprom_write_byte((uint8_t*)(0x1),atoi(num_as_string));
-}
+return atoi(num_as_string);}
 
 
 void Timer_T1_sub_with_interrupt(char Counter_speed, unsigned int Start_point){ 
@@ -120,7 +128,26 @@ TCNT1L = Start_point & 0x00FF;
 TCCR1B = Counter_speed;}
 
 
-ISR(TIMER1_OVF_vect) {TIMSK1 &= (~(1 << TOIE1)); enable_PCI_on_sw1;}
+ISR(TIMER1_OVF_vect) {TIMSK1 &= (~(1 << TOIE1)); enable_PCI_on_sw1;Serial.write('A');}
+
+
+void float_num_to_display_local(float FP_num){
+char * Char_ptr;
+
+pause_pin_change_interrupt_on_PC5;
+Char_ptr = (char*)&FP_num;
+One_wire_Tx_char = 'D';                             //Command 'D' indicates the a floating point number will be sent
+UART_Tx_1_wire();
+for(int m = 0; m <= 3; m++){                        //Split the number into 4 chars
+One_wire_Tx_char = *Char_ptr;                       //and send them individually
+UART_Tx_1_wire(); 
+Char_ptr += 1;
+wdr();}
+reinstate_pin_change_interrupt_on_PC5;}
+
+
+
+
 
 
 
