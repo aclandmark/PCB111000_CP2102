76f574198b38cf210f3d550d16292c2e3081c612
aclandmark <mark.osborne15@btinternet.com>
2022-06-22 20:42:55 +0100

    Update Proj_2C1_header_file.h

--- AVR_freaks_first_posting/Firmware/CP2102_bootloader/1_Random_pattern_generator/Resources_2C1/Proj_2C1_header_file.h
+++ AVR_freaks_first_posting/Firmware/CP2102_bootloader/1_Random_pattern_generator/Resources_2C1/Proj_2C1_header_file.h
@@ -81,6 +81,8 @@ PORTD = 0xFF;
 /************************************************************************************************************************************/
 #define Start_clock		    	OCR0A = 0; TCNT0 = 0;  TCCR0B = (1 << CS01);
 
+#define boot_reset_ctl_reg			0x3FC
+#define 	set_Run_BL_bit			eeprom_write_byte((uint8_t*)boot_reset_ctl_reg, (eeprom_read_byte((uint8_t*)(0x3FC)) & (~4)));
 #define setRunBL_bit		eeprom_write_byte((uint8_t*)0x3FC, (eeprom_read_byte((uint8_t*)(0x3FC)) & (~2)));
 #define PINB4_down			((PINB & 0x10)^0x10)
 #define PINC5_down			((PINC & 0x20)^0x20)
