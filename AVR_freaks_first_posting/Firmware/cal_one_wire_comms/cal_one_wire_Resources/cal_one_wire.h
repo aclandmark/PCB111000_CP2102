81378f1753ddde13087d5f5375fcefadceeac4b4
aclandmark <mark.osborne15@btinternet.com>
2022-06-23 21:21:04 +0100

    Update cal_one_wire.h

--- AVR_freaks_first_posting/Firmware/cal_one_wire_comms/cal_one_wire_Resources/cal_one_wire.h
+++ AVR_freaks_first_posting/Firmware/cal_one_wire_comms/cal_one_wire_Resources/cal_one_wire.h
@@ -131,7 +131,6 @@ TIFR0 = 0xFF;
 #define boot_reset_ctl_reg			0x3FC
 #define 	set_Run_BL_bit			eeprom_write_byte((uint8_t*)boot_reset_ctl_reg, (eeprom_read_byte((uint8_t*)(0x3FC)) & (~4)));
 
-//#define setRunBL_bit        eeprom_write_byte((uint8_t*)0x3FC, (eeprom_read_byte((uint8_t*)(0x3FC)) & (~2)));
 #define PINB4_down  ((PINB & 0x10)^0x10)
 #define PINC5_down  ((PINC & 0x20)^0x20)
 #define PINC5_up  (PINC & 0x20)
