2d8d08d9b619f043427a9beaf4deeab6f30958ef
aclandmark <mark.osborne15@btinternet.com>
2022-06-23 17:27:06 +0100

    Update 1_first_project.ino

--- AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
+++ AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
@@ -16,31 +16,7 @@
 
 
 #include "First_project_header.h"
- 
-int main (void)                          //Example 10
-{ unsigned int PRN;
-  unsigned char PRN_counter;
 
-  setup_328_HW;
-  PRN_counter = 0;
-  PRN = PRN_16bit_GEN (0, &PRN_counter);
-  while (1)
-  { PRN = PRN_16bit_GEN (PRN, &PRN_counter);
-    One_wire_Tx_2_integers(PRN, (PRN << ((PRN % 2) + 1)));
-    Timer_T2_10mS_delay_x_m(10);
-  }
-}
-  
-
-
-
-/**************************************************************************************************
-
-  Use this area for saving the examples when they have been got working and finished with
-  Use the space above for the active program (the one eing worked on)
-
-
-***************Example 1: One bar sweep right to left*********************************************
 int main (void)                          //Example 1
   { unsigned int PORT_1;
 
@@ -54,6 +30,36 @@ int main (void)                          //Example 1
   SW_reset;
   return 1;
   }
+
+
+
+
+ /*
+int main (void)                          //Example 10
+{ unsigned int PRN;
+  unsigned char PRN_counter;
+
+  setup_328_HW;
+  PRN_counter = 0;
+  PRN = PRN_16bit_GEN (0, &PRN_counter);
+  while (1)
+  { PRN = PRN_16bit_GEN (PRN, &PRN_counter);
+    One_wire_Tx_2_integers(PRN, (PRN << ((PRN % 2) + 1)));
+    Timer_T2_10mS_delay_x_m(10);
+  }
+}*/
+  
+
+
+
+/**************************************************************************************************
+
+  Use this area for saving the examples when they have been got working and finished with
+  Use the space above for the active program (the one eing worked on)
+
+
+***************Example 1: One bar sweep right to left*********************************************
+
   
 
 
