2c183709457fc19946149ce43e3eb351af68a85e
aclandmark <mark.osborne15@btinternet.com>
2022-06-23 21:09:24 +0100

    Update Random_pattern_generator.c

--- AVR_freaks_first_posting/Firmware/CP2102_bootloader/1_Random_pattern_generator/Random_pattern_generator/Random_pattern_generator.c
+++ AVR_freaks_first_posting/Firmware/CP2102_bootloader/1_Random_pattern_generator/Random_pattern_generator/Random_pattern_generator.c
@@ -48,35 +48,3 @@ return 1;}
 
 
 
-
-
-
-/*int main (void){
-
-	char letter=0;
-	char digit_num=0;
-	char seg_counter = 0;
-	char direction = 0;
-
-
-setup_328_HW;																		//see "Resources\header file
-One_wire_Tx_char = 'c';  UART_Tx_1_wire();											//Clear display
-
-while(1){																			//Generate pattern
-while(seg_counter < 56){															//There are 56 segments in total
-letter = (PRN_16bit_GEN (0)%7) + 'a';
-digit_num = (PRN_16bit_GEN (0)%8);
-if ((!(direction)) && (display_bkp[letter - 'a'] & (1 << digit_num))) continue;		//Continue statements skip back to the top of the while-loop
-if ((direction) && (!(display_bkp[letter - 'a'] & (1 << digit_num)))) continue;		//This is to ensure segments are not turned-off before
-One_wire_comms_any_segment(letter, digit_num);												//they have all been turned on.
-backup_the_display(letter, digit_num);												//keep backup up to date
-seg_counter += 1;}
-
-direction ^= 1;																		//Toggle the direction_counter value
-seg_counter = 0;
-_delay_ms(500);}
-SW_reset;
-return 1;}*/
-
-
-
