b0d6128ef7ba049ab6f7de35c5f953a845816d4b
aclandmark <mark.osborne15@btinternet.com>
2022-06-25 20:58:22 +0100

    Update One_wire_header.h

--- AVR_freaks_second_posting/Projects/8_Resets/8B_FPN_to_display_with_WDT/Resources_display_fpn_with_WDT/One_wire_header.h
+++ AVR_freaks_second_posting/Projects/8_Resets/8B_FPN_to_display_with_WDT/Resources_display_fpn_with_WDT/One_wire_header.h
@@ -69,7 +69,8 @@ while (!(TIFR0 & (1 << OCF0A)));\
 TIFR0 = 0xFF;  
 
 
-#define setRunBL_bit				eeprom_write_byte((uint8_t*)0x3FC, (eeprom_read_byte((uint8_t*)(0x3FC)) & (~2)));
+#define 	reset_ctl_reg			0x3FC
+#define 	set_Run_BL_bit			eeprom_write_byte((uint8_t*)reset_ctl_reg, (eeprom_read_byte((uint8_t*)(reset_ctl_reg)) & (~4)));
 
 //if the hex_text_programmer reads bit 1 of EPPPROM loation 0x3FC as zero it will run the bootloader
 
