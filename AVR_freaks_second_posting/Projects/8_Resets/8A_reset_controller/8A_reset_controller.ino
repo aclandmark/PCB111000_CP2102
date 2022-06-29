42b661ee8bf8ffd77a85ff23ce3b7d815184ec4f
aclandmark <mark.osborne15@btinternet.com>
2022-06-29 17:06:09 +0100

    Update 8A_reset_controller.ino

--- AVR_freaks_second_posting/Projects/8_Resets/8A_reset_controller/8A_reset_controller.ino
+++ AVR_freaks_second_posting/Projects/8_Resets/8A_reset_controller/8A_reset_controller.ino
@@ -7,30 +7,34 @@
 
 
 #include "8A_reset_controller_header.h"
-
+volatile char test;
 
 int main (void){
 
-setup_328_HW_Arduino;
-determine_reset_source;
+char copy_of_reset_ctl_reg;
 
-One_25ms_WDT_with_interrupt;
+setup_328_HW_Arduino_IO;
+//determine_reset_source;
+
+//One_25ms_WDT_with_interrupt;
 
 set_up_PCI;
 enable_PCI_on_sw1;
-
+test = 0;
 switch (reset_status){
 
-case 1: User_prompt;
+case 1: User_prompt_Arduino;
 Serial.write("\r\nProgram running following POR\r\n"); break;
-case 2: Serial.write("\r\nProgram triggered by WDTout.\r\n"); break;
+case 2: Serial.write("\r\nProgram triggered by a SW_reset.\r\n"); break;
 case 3: Serial.write("\r\nProgram triggered by the p/r/t/D... user prompt.\r\n"); break;
-case 4: Serial.write("\r\nProgram triggered by a flagged WDTout.\r\n"); break;
-case 5: Serial.write("\r\nProgram recovery following system crash.\r\n"); break;}
+case 4: Serial.write("\r\nProgram triggered by a flagged WDTout.\r\n"); break;}
 
+Char_to_PC_as_binary((byte)copy_of_reset_ctl_reg);
 _delay_ms(10);
 
-while(1)wdr();
+while(!(test))wdr();
+while(1);
+return 1;
 }
 
 
@@ -45,10 +49,18 @@ Signal_flagged_WDTout; }
 
 if((switch_2_up) && (switch_3_down))while(1);
 
+if ((switch_2_down) && (switch_3_down)){Serial.write('G');cli(); while(1);}
+
 SW_reset;}
 
 
 
-ISR (WDT_vect){Signal_WDTout_with_interrupt;}
+ISR (WDT_vect){Signal_WDTout_with_interrupt;WDTout;}
+
+
+void Char_to_PC_as_binary(unsigned char num){
+  Serial.write("\t\t");
+for(int m = 0; m <= 7; m++)
+{if (num & (1 << (7 - m)))Serial.write('1'); else Serial.write('0');}}
 
 /*******************************************************************************************************************/
