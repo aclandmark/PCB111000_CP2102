a9754b49ec4dbcbeb68d5ec4d45fc3f0a69fd159
aclandmark <mark.osborne15@btinternet.com>
2022-06-30 16:02:48 +0100

    Update 8A_reset_controller.ino

--- AVR_freaks_second_posting/Projects/8_Resets/8A_reset_controller/8A_reset_controller.ino
+++ AVR_freaks_second_posting/Projects/8_Resets/8A_reset_controller/8A_reset_controller.ino
@@ -1,39 +1,37 @@
 
-#define Reset_control_switch_up    (PINC & 0x20)
-
-#define reset_ctl_reg                         0x3FC
-#define Signal_flagged_WDTout                 eeprom_write_byte((uint8_t*)reset_ctl_reg, ~0x10)
-#define Signal_WDTout_with_interrupt          eeprom_write_byte((uint8_t*)reset_ctl_reg, ~0x20)
 
 
 #include "8A_reset_controller_header.h"
-volatile char test;
 
 int main (void){
 
-char copy_of_reset_ctl_reg;
+
 
 setup_328_HW_Arduino_IO;
-//determine_reset_source;
 
-//One_25ms_WDT_with_interrupt;
 
-set_up_PCI;
-enable_PCI_on_sw1;
-test = 0;
 switch (reset_status){
 
 case 1: User_prompt_Arduino;
-Serial.write("\r\nProgram running following POR\r\n"); break;
-case 2: Serial.write("\r\nProgram triggered by a SW_reset.\r\n"); break;
-case 3: Serial.write("\r\nProgram triggered by the p/r/t/D... user prompt.\r\n"); break;
-case 4: Serial.write("\r\nProgram triggered by a flagged WDTout.\r\n"); break;}
+Serial.write("\r\nProgram running following POR\t"); break;
+case 2: Serial.write("\r\nProgram triggered by a SW_reset.\t"); break;
+case 3: Serial.write("\r\nProgram triggered by the p/r/t/D... user prompt."); break;
+case 4: Serial.write("\r\nProgram triggered by a flagged WDTout.\t"); break;
+case 5: Serial.write("\r\nProgram halted (WDTout with ISR)\r\nPress reset ctl switch to continue\t");
+Char_to_PC_as_binary((byte)copy_of_reset_ctl_reg);while(1)wdr();break;
+case 6: Serial.write("\r\nWDTout. Press reset ctl switch to continue.");
+Char_to_PC_as_binary((byte)copy_of_reset_ctl_reg);while(1)wdr();break;
+case 7: Serial.write("\r\nJumps to 0x0000.\r\n(WDT configured with interrupt but ISR missing.)");}
 
 Char_to_PC_as_binary((byte)copy_of_reset_ctl_reg);
 _delay_ms(10);
 
-while(!(test))wdr();
-while(1);
+set_up_PCI;
+enable_PCI_on_sw1;
+
+//cli();                          //This line prevents the fail safe mechanism from working
+while(1)wdr();                    //Waits here for the switches to be operated 
+
 return 1;
 }
 
@@ -43,24 +41,22 @@ if (switch_1_up)return;
   sei();
   disable_PCI_on_sw1;
 if((switch_2_up) && (switch_3_up));
+if((switch_2_down) && (switch_3_up)){Signal_flagged_WDTout;}
+if((switch_2_up) && (switch_3_down))while(1); 
+if((switch_2_down) && (switch_3_down)){cli();while(1);}
 
-if((switch_2_down) && (switch_3_up)){
-Signal_flagged_WDTout; }
-
-if((switch_2_up) && (switch_3_down))while(1);
-
-if ((switch_2_down) && (switch_3_down)){Serial.write('G');cli(); while(1);}
+while(switch_1_down)wdr();
 
 SW_reset;}
 
 
 
-ISR (WDT_vect){Signal_WDTout_with_interrupt;WDTout;}
+ISR (WDT_vect){Signal_WDTout_with_interrupt;WDTout;}      //Optional
 
 
 void Char_to_PC_as_binary(unsigned char num){
-  Serial.write("\t\t");
+Serial.write("\t\t");
 for(int m = 0; m <= 7; m++)
-{if (num & (1 << (7 - m)))Serial.write('1'); else Serial.write('0');}}
+{if (num & (1 << (7 - m)))Serial.write('1'); else Serial.write('0');}Serial.write("\t\t");}
 
 /*******************************************************************************************************************/
