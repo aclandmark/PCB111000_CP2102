a271505ea707d97de8914eafd78ce42a0d51cfd8
aclandmark <mark.osborne15@btinternet.com>
2022-07-04 19:55:47 +0100

    Update Basic_IO_and_Timer.c

--- AVR_freaks_second_posting/Projects/2_Receiver_Transmitter_Basic/Resources_Receiver_transmitter/Basic_IO_and_Timer.c
+++ AVR_freaks_second_posting/Projects/2_Receiver_Transmitter_Basic/Resources_Receiver_transmitter/Basic_IO_and_Timer.c
@@ -1,38 +1,27 @@
-
-/*
-Character IO is based on the following subroutines which are based on the book 
-C Programming for Microcontrollers by Jo Pardue
-
-Go to https://epdf.pub/queue/c-programming-for-microcontrollers.html for a download
-
-Similar functions are available from Arduino libries
-However these have been adopted for use with PCB_111000.
-
-They provide a good reason to explore the data sheet
-show how the AVR hardware can be controlled from within a C subroutine 
-and can be modified at will by the user. 
-
-*/
-
-
-
 #define T2_delay_10ms 	7,178
 
 
-void setup_PC_comms (unsigned char, unsigned char);
+void setup_PC_comms_Basic (unsigned char, unsigned char);
 void Timer_T2_10mS_delay_x_m(int);
 void Timer_T2_sub(char, unsigned char);
-char isCharavailable (int);
-char Char_from_PC(void);
-char waitforkeypress(void);
-void String_to_PC(const char*);
-void Char_to_PC(char);
+
+char isCharavailable_Basic (int);
+char isCharavailable_A (int);
+char waitforkeypress_Basic(void);
+char waitforkeypress_A(void);
+
+char Char_from_PC_Basic(void);
+void String_to_PC_Basic(const char*);
+void Char_to_PC_Basic(char);
+
+char decimal_digit (char);
+char wait_for_return_key_A(void);
 
 
 
 
 /**********************************************************************************************/
-void setup_PC_comms (unsigned char UBRROH_N, unsigned char UBRR0L_N ){
+void setup_PC_comms_Basic (unsigned char UBRROH_N, unsigned char UBRR0L_N ){
 UCSR0B = 0;
 UBRR0H = UBRROH_N; 
 UBRR0L = UBRR0L_N;  
@@ -44,7 +33,7 @@ UCSR0C =  (1 << UCSZ00)| (1 << UCSZ01);}
 
 /*********************************************************************/
 void Timer_T2_10mS_delay_x_m(int m)
-{for (int n = 0; n < m; n++){Timer_T2_sub(T2_delay_10ms);}}
+{for (int n = 0; n < m; n++){Timer_T2_sub(T2_delay_10ms);wdr();}}
 
 
 /**********************************************************************************************/
@@ -57,41 +46,72 @@ TIFR2 |= (1<<TOV2); TCCR2B = 0;}
 
 
 /**********************************************************************************************/
-char isCharavailable (int m){int n = 0;		
-while (!(UCSR0A & (1 << RXC0))){n++;
-if (n>8000) {m--;n = 0;}if (m == 0)return 0;}
+char isCharavailable_Basic (int m){int n = 0;
+while (!(UCSR0A & (1 << RXC0))){n++; wdr();	
+if (n>8000) {m--;n = 0;}if (m == 0)return 0;}	
 return 1;}
+	
+char isCharavailable_A (int m){int n = 0;
+while (!(Serial.available())){n++;	wdr();			
+if (n>8000) {m--;n = 0;}if (m == 0)return 0;}	
+return 1;}	
+
+
+char waitforkeypress_Basic (void){
+while (!(UCSR0A & (1 << RXC0)))wdr();	
+return UDR0;}	
+
+
+char waitforkeypress_A (void){
+while (!(Serial.available()))wdr();	
+return Serial.read(); }
+
+
 
 
 
 /**********************************************************************************************/
-char Char_from_PC(void)
+char Char_from_PC_Basic(void)
 {return UDR0;}
 
 
 
-/**********************************************************************************************/
-char waitforkeypress(void){
-while (!(UCSR0A & (1 << RXC0)));
-return UDR0;}
-
-
 
 /**********************************************************************************************/
-void String_to_PC(const char * s){
+void String_to_PC_Basic(const char s[]){
 int i = 0;
 while(i < 200){
 if(s[i] == '\0') break;
-Char_to_PC(s[i++]);}}
-
+Char_to_PC_Basic(s[i++]);}}
 
 
 
 /**********************************************************************************************/
-void Char_to_PC(char data){
+void Char_to_PC_Basic(char data){
 while (!(UCSR0A & (1 << UDRE0)));
 UDR0 = data;}
 
 
+
+
+/***************************************************************************************************************/
+char decimal_digit (char data){
+if (((data > '9') || (data < '0')) )return 0;
+else return 1;}
+
+
+
+
+/***************************************************************************************************************/
+char wait_for_return_key_A(void){                  							//Detects \r\n, \r or \n
+char keypress,temp;
+while(1){
+if (isCharavailable_A(8)){
+keypress = Serial.read();
+break;}}
+if((keypress == '\r') || (keypress == '\n')){
+if (isCharavailable_A(1)){temp = Serial.read();}
+keypress = '\r';}
+return keypress;}
 
 
