ed111cb0749e579f8e70fdfcb5479e2dc2209df5
aclandmark <mark.osborne15@btinternet.com>
2022-07-04 19:55:28 +0100

    Update 2_Receiver_Transmitter_Basic.ino

--- AVR_freaks_second_posting/Projects/2_Receiver_Transmitter_Basic/2_Receiver_Transmitter_Basic.ino
+++ AVR_freaks_second_posting/Projects/2_Receiver_Transmitter_Basic/2_Receiver_Transmitter_Basic.ino
@@ -14,8 +14,38 @@ the steps involved and introduce the Atmega328 data sheet.
 
 #include "Receiver_Transmitter_header.h"
  
+int main (void)                          //Example 9
+  { char keyboard_input;
+  char Num_string[12];
+  long Num, A = 55; long B = 7; long Div; long mod;
+  int no_decimal_places;
 
+    setup_328_HW_Basic_IO;
+  String_to_PC_Basic("?\r\n");
+  
+  for(int m = 0; m <=2; m++)
+  {Num_string_from_KBD_Basic(Num_string);
+  Num = Askii_to_binary(Num_string);
+  switch (m)
+  { case 0: A = Num; Char_to_PC_Basic('?');break;
+  case 1: B = Num; String_to_PC_Basic("?\r\n");break;
+  case 2: no_decimal_places = Num; break;}}
 
+  divide(A, B, &Div, &mod, no_decimal_places);
+  Num_to_PC_Basic(Div);
+  String_to_PC_Basic(". ");
+    while (no_decimal_places) {
+  no_decimal_places =    divide(mod*10, B, &Div, &mod, no_decimal_places);
+  Num_to_PC_Basic(Div);}
+  String_to_PC_Basic("\r\n");
+   SW_reset;
+  return 1; }
+
+  long divide(long A, long B, long *Div, long *mod, int no_decimal_places)
+  {*Div = A/B;
+  *mod = A%B;
+  no_decimal_places -= 1;
+  return no_decimal_places; } 
 
 
 /************************************************************************************************************
@@ -25,14 +55,14 @@ the steps involved and introduce the Atmega328 data sheet.
 
 
 
-********Example 1: Echoes keypresses*************************************************************************/
+********Example 1: Echoes keypresses*************************************************************************
   int main (void)                          //Example 1
-  { setup_328_HW;
-  Char_to_PC('?');
+  { setup_328_HW_Basic_IO;
+  Char_to_PC_Basic('?');
   newline;
   while (1)
   { Char_to_PC_Local
-    (waitforkeypress());
+    (waitforkeypress_Basic());
   }
   return 1;
   }
@@ -42,18 +72,19 @@ the steps involved and introduce the Atmega328 data sheet.
 
 
 
-/**********Example 2: Prints out ASKII characters**************************************************************
+**********Example 2: Prints out ASKII characters**************************************************************
   int main (void)                          //Example 2
   { char symbol;
-  setup_328_HW;
+  setup_328_HW_Basic_IO;
   newline;
   symbol = '!';
   while (symbol <= '~')
   { Char_to_PC_Local(symbol);
     _delay_ms(10);
     symbol++;
+    wdr();
   }
-  waitforkeypress();
+  waitforkeypress_Basic();
   SW_reset;
   return 1;
   }
@@ -63,17 +94,17 @@ the steps involved and introduce the Atmega328 data sheet.
 
 ********Example 3: Echo character string or prints file*******************************************************
 int main (void)                          //Example 3
-  { setup_328_HW;
-  while (!(isCharavailable(65)))
-    Char_to_PC('?');
+  { setup_328_HW_Basic_IO;
+  while (!(isCharavailable_Basic(65)))
+    Char_to_PC_Basic('?');
   newline;
-  Char_to_PC(Char_from_PC());
+  Char_to_PC_Basic(Char_from_PC_Basic());
   while (1)
-  { if (isCharavailable(10))
-      Char_to_PC(Char_from_PC());
+  { if (isCharavailable_Basic(10))
+      Char_to_PC_Basic(Char_from_PC_Basic());
     else break;
   }
-  String_to_PC("Done\r\n");
+  String_to_PC_Basic("Done\r\n");
   SW_reset;
   return 1;
   }
@@ -83,17 +114,17 @@ int main (void)                          //Example 3
 
 ********Example 4: Send strings to the PC********************************************************************
 int main (void)                          //Example 4
-  { setup_328_HW;
-  String_to_PC("\r\nDefining and using text strings\r\n");
+  { setup_328_HW_Basic_IO;
+  String_to_PC_Basic("\r\nDefining and using text strings\r\n");
   const char *message_1 = "Hello world\r\n";
   const char *message_2 = "Sending text to a PC\r\n";
   const char message_3[] = "Writing C programs and\r\n";
   const char message_4[] = "Uploading them to a device\r\n";
-  String_to_PC(message_1);
-  String_to_PC(message_2);
-  String_to_PC(message_3);
-  String_to_PC(message_4);
-  while (1);
+  String_to_PC_Basic(message_1);
+  String_to_PC_Basic(message_2);
+  String_to_PC_Basic(message_3);
+  String_to_PC_Basic(message_4);
+  while (1)wdr();
   return 1;
   }  
 
@@ -103,16 +134,16 @@ int main (void)                          //Example 4
 ************Example 5: Generate an ASKII table****************************************************************
 int main (void)                          //Example 5
   { char symbol = '!';
-  setup_328_HW;
+  setup_328_HW_Basic_IO;
   newline;
   while (symbol <= '~')
-  { Num_to_PC(symbol);
+  { Num_to_PC_Basic(symbol);
     Char_to_PC_Local(symbol++);
-    _delay_ms(50);
+    wdr();_delay_ms(50);
     if (!((symbol - '!') % 8))newline;
     else Char_to_PC_Local('\t');
   }
-  while (1);
+  while (1)wdr();
   return 1;
   }
   
@@ -125,17 +156,18 @@ int main (void)                          //Example 5
  int main (void)                          //Example 6
   { int i = 0, number = 12345;
   char s[12];
-  setup_328_HW;
+  setup_328_HW_Basic_IO;
   do {
     s[i++] = number % 10 + '0';
   }
   while ((number = number / 10) > 0);
   s[i] = '\0';
   for (int m = i; m > 0; m--)
-    Char_to_PC(s[m - 1]);
-  while (1);
+    Char_to_PC_Basic(s[m - 1]);
+  while (1)wdr();
   return 1;
   } 
+
 
 
 
@@ -143,18 +175,17 @@ int main (void)                          //Example 5
 int main (void)                          //Example 7
   { long num = 0;
   char keypress;
-  setup_328_HW;
-  num = 0; Char_to_PC('?');
-  while ((keypress = waitforkeypress()) != '\r')
+  setup_328_HW_Basic_IO;
+  num = 0; Char_to_PC_Basic('?');
+  while ((keypress = waitforkeypress_Basic()) != '\r')
   { num = num * 10 + keypress  - '0';
     Int_num_to_display(num);
   }
-  Num_to_PC(num * 2);
+  Num_to_PC_Basic(num * 2);
   Int_num_to_display(num * 2);
   SW_reset;
   return 1;
   }  
-
 
 
 
@@ -163,29 +194,30 @@ int main (void)                          //Example 8
 { long num = 0;
   char numLength;
   char Num_string[12];
-  setup_328_HW;
-  String_to_PC("\r\n?\r\n");
-  Num_string_from_KBD(Num_string);
+  setup_328_HW_Basic_IO;
+  String_to_PC_Basic("\r\n?\r\n");
+  Num_string_from_KBD_Basic(Num_string);
 
   { int m = 0; while (Num_string[m])
-      Char_to_PC(Num_string[m++]);
+      Char_to_PC_Basic(Num_string[m++]);
   }
 
   num = Askii_to_binary(Num_string);
 
   num = num * 2;
-  String_to_PC("\r\n");
+  String_to_PC_Basic("\r\n");
 
   numLength = Binary_to_Askii(num, Num_string);
 
   for (int m = numLength; m > 0; m--)
-    Char_to_PC(Num_string[m - 1]);
+    Char_to_PC_Basic(Num_string[m - 1]);
 
   Int_num_to_display(num);
 
   SW_reset;
   return 1;
 }
+
 
 
 *************Example 9: Arithmetic: More on pointers********************************************************
@@ -195,11 +227,11 @@ int main (void)                          //Example 9
   long Num, A = 55; long B = 7; long Div; long mod;
   int no_decimal_places;
 
-    setup_328_HW;
+    setup_328_HW_Basic_IO;
   String_to_PC("?\r\n");
   
   for(int m = 0; m <=2; m++)
-  {Num_string_from_KBD(Num_string);
+  {Num_string_from_KBD_Basic(Num_string);
   Num = Askii_to_binary(Num_string);
   switch (m)
   { case 0: A = Num; Char_to_PC('?');break;
@@ -207,11 +239,11 @@ int main (void)                          //Example 9
   case 2: no_decimal_places = Num; break;}}
 
   divide(A, B, &Div, &mod, no_decimal_places);
-  Num_to_PC(Div);
+  Num_to_PC_Basic(Div);
   String_to_PC(". ");
     while (no_decimal_places) {
   no_decimal_places =    divide(mod*10, B, &Div, &mod, no_decimal_places);
-  Num_to_PC(Div);}
+  Num_to_PC_Basic(Div);}
   String_to_PC("\r\n");
    SW_reset;
   return 1; }
@@ -224,9 +256,9 @@ int main (void)                          //Example 9
 
 
 ************This area is for project subroutines*************************************************************/
-void Num_string_from_KBD(char * array_ptr)
+void Num_string_from_KBD_Basic(char * array_ptr)
 { char keypress;
-  while ((keypress = waitforkeypress()) != '\r')
+  while ((keypress = waitforkeypress_Basic()) != '\r')
   { *array_ptr = keypress;
     array_ptr += 1;
   }
