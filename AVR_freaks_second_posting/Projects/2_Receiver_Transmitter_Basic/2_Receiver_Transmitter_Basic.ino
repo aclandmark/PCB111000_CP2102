addb26a7a9a6eda8e5d502c7b1e42d3873b95894
aclandmark <mark.osborne15@btinternet.com>
2022-07-04 20:12:04 +0100

    Update 2_Receiver_Transmitter_Basic.ino

--- AVR_freaks_second_posting/Projects/2_Receiver_Transmitter_Basic/2_Receiver_Transmitter_Basic.ino
+++ AVR_freaks_second_posting/Projects/2_Receiver_Transmitter_Basic/2_Receiver_Transmitter_Basic.ino
@@ -228,23 +228,23 @@ int main (void)                          //Example 9
   int no_decimal_places;
 
     setup_328_HW_Basic_IO;
-  String_to_PC("?\r\n");
+  String_to_PC_Basic("?\r\n");
   
   for(int m = 0; m <=2; m++)
   {Num_string_from_KBD_Basic(Num_string);
   Num = Askii_to_binary(Num_string);
   switch (m)
-  { case 0: A = Num; Char_to_PC('?');break;
-  case 1: B = Num; String_to_PC("?\r\n");break;
+  { case 0: A = Num; Char_to_PC_Basic('?');break;
+  case 1: B = Num; String_to_PC_Basic("?\r\n");break;
   case 2: no_decimal_places = Num; break;}}
 
   divide(A, B, &Div, &mod, no_decimal_places);
   Num_to_PC_Basic(Div);
-  String_to_PC(". ");
+  String_to_PC_Basic(". ");
     while (no_decimal_places) {
   no_decimal_places =    divide(mod*10, B, &Div, &mod, no_decimal_places);
   Num_to_PC_Basic(Div);}
-  String_to_PC("\r\n");
+  String_to_PC_Basic("\r\n");
    SW_reset;
   return 1; }
 
@@ -253,6 +253,7 @@ int main (void)                          //Example 9
   *mod = A%B;
   no_decimal_places -= 1;
   return no_decimal_places; } 
+
 
 
 ************This area is for project subroutines*************************************************************/
