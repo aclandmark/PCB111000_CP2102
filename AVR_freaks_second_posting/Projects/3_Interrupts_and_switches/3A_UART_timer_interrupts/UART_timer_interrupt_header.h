b742d69c16a8e9a91d77b9f134d12a75d0eb6eb7
aclandmark <mark.osborne15@btinternet.com>
2022-07-15 16:11:11 +0100

    Update UART_timer_interrupt_header.h

--- AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3A_UART_timer_interrupts/UART_timer_interrupt_header.h
+++ AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3A_UART_timer_interrupts/UART_timer_interrupt_header.h
@@ -155,7 +155,7 @@ if(reset_status == 6)\
 /************************************************************************************************************************************/
 #include "Resources_3A\One_wire_header.h"
 #include "Resources_3A\One_wire_transactions.c"
-#include "Resources_3A\Basic_IO_and_Timer.c"
+#include "Resources_3A\Basic_IO_and_Timer_with_UART_ISR.c"
 #include "Resources_3A\Proj_3A_subroutines.c"
 
 
