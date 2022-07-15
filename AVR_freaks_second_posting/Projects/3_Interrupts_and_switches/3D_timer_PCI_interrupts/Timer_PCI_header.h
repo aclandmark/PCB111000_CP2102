973fa965abee7df200996c44b5dca5725c15a8aa
aclandmark <mark.osborne15@btinternet.com>
2022-07-15 20:30:18 +0100

    Update Timer_PCI_header.h

--- AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3D_timer_PCI_interrupts/Timer_PCI_header.h
+++ AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3D_timer_PCI_interrupts/Timer_PCI_header.h
@@ -1,11 +1,15 @@
 
 
-
 #include <avr/wdt.h>
 
-
-void One_wire_Tx_2_integers(unsigned int, unsigned int);
 char User_response;
+char reset_status;
+
+#define newline String_to_PC_Basic("\r\n")
+
+
+//void One_wire_Tx_2_integers(unsigned int, unsigned int);
+//char User_response;
 
 
 /**********************************************************************************/
@@ -18,7 +22,7 @@ if ((eeprom_read_byte((uint8_t*)0x3FF) > 0x0F)\
 
 
 /************************************************************************************************************************************/
-#define setup_328_HW \
+#define setup_328_HW_Basic_IO \
 \
 setup_watchdog;\
 ADMUX |= (1 << REFS0);\
@@ -31,8 +35,12 @@ set_up_pin_change_interrupt_on_PC5;\
 setup_one_wire_comms;\
 set_up_activity_leds;\
 sei();\
-setup_PC_comms(0,16);\
-_delay_ms(10);
+setup_PC_comms_Basic(0,16);\
+_delay_ms(10);\
+determine_reset_source;\
+Two_50ms_WDT_with_interrupt;\
+failsafe;
+
 
 //The reset control switch is connected to PC5
 
@@ -46,7 +54,15 @@ MCUSR &= ~(1<<WDRF);\
 WDTCSR |= (1 <<WDCE) | (1<< WDE);\
 WDTCSR = 0;
 
-#define SW_reset {wdt_enable(WDTO_30MS);while(1);}
+
+#define SW_reset {Signal_SW_reset;wdt_enable(WDTO_30MS);while(1);}
+#define WDTout {wdt_enable(WDTO_30MS);while(1);}
+
+
+#define Two_50ms_WDT_with_interrupt \
+wdr();\
+WDTCSR |= (1 <<WDCE) | (1<< WDE);\
+WDTCSR = (1<< WDE) | (1 << WDIE) |  (1 << WDP2);
 
 
 /************************************************************************************************************************************/
@@ -60,6 +76,14 @@ PORTC = 0xFF;\
 PORTD = 0xFF;
 
 //All ports are initialised to weak pull up (WPU)
+
+
+/************************************************************************************************************************************/
+#define reset_ctl_reg                         0x3FC
+#define Signal_WDTout_with_interrupt          eeprom_write_byte((uint8_t*)reset_ctl_reg, ~0x20)
+#define Signal_SW_reset                       eeprom_write_byte((uint8_t*)reset_ctl_reg,(eeprom_read_byte((uint8_t*)reset_ctl_reg) & ~0x40))
+#define clear_reset_ctl_reg                   eeprom_write_byte((uint8_t*)reset_ctl_reg, ~0)
+
 
 
 /************************************************************************************************************************************/
@@ -88,8 +112,49 @@ if(User_response == 'r') break;} sendString("\r\n");
 #define switch_2_down             (PIND & 0x20)^0x20
 #define switch_3_down             (PIND & 0x80)^0x80
 
+
+
+/************************************************************************************************************************************/
+#define determine_reset_source \
+switch (eeprom_read_byte((uint8_t*)reset_ctl_reg))\
+{case ((byte)~1): reset_status = 1; break;\
+case ((byte)~0x42): reset_status = 2; break;\
+case ((byte)~8): reset_status = 3; break;\
+case ((byte) ~0x52):  reset_status = 4; break;\
+case ((byte) ~0x22):  reset_status = 5; break;\
+case ((byte) ~0x2):  reset_status = 6; break;\
+case ((byte) ~0):  reset_status = 7; break;}\
+clear_reset_ctl_reg;
+
+/*
+reset_status:
+1 POR                     bit 0 of reset control register
+2 SW_reset                bits 6 and 1 
+3 prtD                    bit 3
+4 Flaged WDTout           bit 6,4 and 1
+5 WDTout with ISR         bit 5 and 1
+6 WDTout                  bit 1
+7 As 5 but ISR missing    No bits
+
+Note : Set bit 2 to generate the prtD... prompt 
+*/
+
+/************************************************************************************************************************************/
+#define failsafe \
+if(reset_status == 7)\
+{String_to_PC_Basic("\r\nProgram restarted.");}\
+if(reset_status == 6)\
+{String_to_PC_Basic("\r\nWDTout\r\n");while(1)wdr();}
+
+
 /************************************************************************************************************************************/
 #include "Resources_3D\One_wire_header.h"
 #include "Resources_3D\One_wire_transactions.c"
 #include "Resources_3D\Basic_IO_and_Timer.c"
 #include "Resources_3D\Proj_3D_subroutines.c"
+
+
+
+
+
+/************************************************************************************************************************************/
