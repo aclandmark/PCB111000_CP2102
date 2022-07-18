5514fbff930718c17a1384908b88d44b61cd0b28
aclandmark <mark.osborne15@btinternet.com>
2022-07-18 15:03:51 +0100

    Update 1_Bit_ops_1.ino

--- AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/1_Bit_ops_1/1_Bit_ops_1.ino
+++ AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/1_Bit_ops_1/1_Bit_ops_1.ino
@@ -12,12 +12,12 @@ unsigned char lfsr;
 char BWop;                                  //bit wise operation and complement (i.e. swap ones and zeros)
 
 
-setup_328_HW;
+setup_328_HW_Basic_IO;
 
 
-String_to_PC("\r\nSelect OP:  |   ^   &   ~|  ~^  or  ~&");
-BWop = waitforkeypress();
-if (BWop == '~') {comp = 1; BWop = waitforkeypress();}else comp = 0;        //detect complement operator
+String_to_PC_Basic("\r\nSelect OP:  |   ^   &   ~|  ~^  or  ~&");
+BWop = waitforkeypress_Basic();
+if (BWop == '~') {comp = 1; BWop = waitforkeypress_Basic();}else comp = 0;        //detect complement operator
 if ((BWop != '|') && (BWop != '^') && (BWop != '&'))
 SW_reset;                                                              //reset if duff char was sent 
 Reset_ATtiny1606;
@@ -29,8 +29,9 @@ digits[0] = PRN_8bit_GEN(lfsr);
 digits[1] = PRN_8bit_GEN(digits[0]);
 digits[2] =  Op(digits[0] , digits[1], comp, BWop);
 lfsr = digits[1];
-One_wire_comms_3_bytes(digits);}  while (waitforkeypress() !='x');                
-wdt_enable(WDTO_30MS); while(1);}
+One_wire_comms_3_bytes(digits);}  while (waitforkeypress_Basic() !='x');                
+//wdt_enable(WDTO_30MS); while(1);
+SW_reset;}
 
 
 
