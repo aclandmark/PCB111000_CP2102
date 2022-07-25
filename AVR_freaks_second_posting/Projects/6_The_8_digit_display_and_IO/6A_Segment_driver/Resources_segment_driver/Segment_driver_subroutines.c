942b999397a67c9a06ebfa7c9e33627b4a40499c
aclandmark <mark.osborne15@btinternet.com>
2022-07-25 15:56:07 +0100

    Update Segment_driver_subroutines.c

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6A_Segment_driver/Resources_segment_driver/Segment_driver_subroutines.c
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6A_Segment_driver/Resources_segment_driver/Segment_driver_subroutines.c
@@ -7,9 +7,9 @@
 /************************************************************************************************************************************************/
 void One_wire_comms_any_segment(char letter, char digit_num){
 pause_pin_change_interrupt_on_PC5;										//Reset control not allowed during transaction
-One_wire_Tx_char = 'b';			UART_Tx_1_wire();
-One_wire_Tx_char = letter;		UART_Tx_1_wire(); 
-One_wire_Tx_char = digit_num;	UART_Tx_1_wire();
+One_wire_Tx_char = 'b';			UART_Tx_1_wire(); wdr();
+One_wire_Tx_char = letter;		UART_Tx_1_wire(); wdr();
+One_wire_Tx_char = digit_num;	UART_Tx_1_wire(); wdr();
 reinstate_pin_change_interrupt_on_PC5;}
 
 
