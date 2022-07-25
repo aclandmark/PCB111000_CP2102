d1b9d0afe3a4e0cbd29f9508327c8ec5685fea47
aclandmark <mark.osborne15@btinternet.com>
2022-07-25 18:14:12 +0100

    Update display_driver_subroutines_KBD.c

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6E_FPN_to_display/Resources_display_fpn/display_driver_subroutines_KBD.c
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6E_FPN_to_display/Resources_display_fpn/display_driver_subroutines_KBD.c
@@ -1,26 +1,26 @@
 
 
 
-#define Send_int_num_string \
+#define Int_num_string_to_display \
 One_wire_Tx_char = 'A'; UART_Tx_1_wire();\
-for(int m = 0; m <= 7; m++){One_wire_Tx_char = display_buffer[m]; UART_Tx_1_wire();}\
+for(int m = 0; m <= 7; m++){wdr();One_wire_Tx_char = display_buffer[m]; UART_Tx_1_wire();}\
 One_wire_Tx_char = cr_keypress;  UART_Tx_1_wire();
 
 
 
-#define Send_float_num_string \
+#define Float_num_string_to_display \
 One_wire_Tx_char = 'B'; UART_Tx_1_wire();\
-for(int m = 0; m <= 7; m++){One_wire_Tx_char = display_buffer[m]; UART_Tx_1_wire();}\
+for(int m = 0; m <= 7; m++){wdr();One_wire_Tx_char = display_buffer[m]; UART_Tx_1_wire();}\
 One_wire_Tx_char = cr_keypress;  UART_Tx_1_wire();
 
 
 
 #define Int_from_mini_OS \
 One_wire_Tx_char = 'E'; UART_Tx_1_wire();\
-for(int m = 0; m <= 3; m++){\
+for(int m = 0; m <= 3; m++){wdr();\
 UART_Rx_1_wire(); num_byte[m] = One_wire_Rx_char;}\
 \
-for(int m = 0; m <= 3; m++){\
+for(int m = 0; m <= 3; m++){wdr();\
 Long_Num_from_mini_OS = Long_Num_from_mini_OS << 8;\
 Long_Num_from_mini_OS |= num_byte[m];}
 
@@ -28,10 +28,10 @@ Long_Num_from_mini_OS |= num_byte[m];}
 
 #define f_number_from_mini_OS \
 One_wire_Tx_char = 'E'; UART_Tx_1_wire();\
-for(int m = 0; m <= 3; m++){\
-UART_Rx_1_wire(); *Char_ptr = One_wire_Rx_char;\
-Char_ptr += 1;}\
-f_number = *Flt_ptr;
+for(int m = 0; m <= 3; m++){wdr();\
+UART_Rx_1_wire(); *Char_ptr_local = One_wire_Rx_char;\
+Char_ptr_local += 1;}\
+Float_from_mini_OS = *Flt_ptr_local;
 
 
 
@@ -58,14 +58,13 @@ reinstate_pin_change_interrupt_on_PC5;}
 
 
 /******************************************************************************************************************************************/
-void int_num_to_display(long Int_num){
+void Int_num_to_display(long Int_num){
 pause_pin_change_interrupt_on_PC5;
-
 One_wire_Tx_char = 'C';                                 		//Command 'C' indicates the a long number will be sent
 UART_Tx_1_wire();
 for(int m = 0; m <= 3; m++){                            		//Split the number into 4 chars
 One_wire_Tx_char = Int_num >> ((3-m) * 8);              		//and send them individually
-UART_Tx_1_wire(); 
+UART_Tx_1_wire(); wdr();
 }
 reinstate_pin_change_interrupt_on_PC5;}
 
@@ -84,15 +83,15 @@ cr_keypress = 0;                                            	//Set to one when c
 for(int n = 0; n<=8; n++) display_buffer[n] = 0;           		//Clear the buffer used for the string
 
 while(1){
-keypress = waitforkeypress();
+keypress = waitforkeypress_A();
 if ((!(decimal_digit(keypress)))
 && (keypress != '-'))continue;                            		//Ignore keypress if it is not OK
 display_buffer[0] = keypress;
 break;}
  
-Send_int_num_string;
+Int_num_string_to_display;
 while(1){
-if ((keypress = wait_for_return_key())  =='\r')break;        	//Detect return key press (i.e \r or\r\n)
+if ((keypress = wait_for_return_key_A())  =='\r')break;        	//Detect return key press (i.e \r or\r\n)
 if ((decimal_digit(keypress)) || (keypress == '\b')\
  || (keypress == '-'))
 
@@ -104,19 +103,21 @@ else
 display_buffer[n] = display_buffer[n-1];
 display_buffer[0] = keypress;  }                             	//Add new keypress           
 
-Send_int_num_string;}}                                      	//Update display includes "cr_keypress"                                                 
+Int_num_string_to_display;}}                                      	//Update display includes "cr_keypress"                                                 
 cr_keypress = 1;                                            	//End of string; return key press detected
 
-Send_int_num_string;
+Int_num_string_to_display;
 cr_keypress = 0;
 
-One_wire_Tx_char = 'E'; UART_Tx_1_wire();						//Command ATtiny1606:  Waiting to receive number in binary form							
+Int_from_mini_OS;
+
+/*One_wire_Tx_char = 'E'; UART_Tx_1_wire();						//Command ATtiny1606:  Waiting to receive number in binary form							
 for(int m = 0; m <= 3; m++){
 UART_Rx_1_wire(); num_byte[m] = One_wire_Rx_char;}
 
 for(int m = 0; m <= 3; m++){									//Recover number as 4 bytes and re-assemble them
 Long_Num_from_mini_OS = Long_Num_from_mini_OS << 8;
-Long_Num_from_mini_OS |= num_byte[m];}
+Long_Num_from_mini_OS |= num_byte[m];}*/
 
 return Long_Num_from_mini_OS;}
 
@@ -127,7 +128,7 @@ return Long_Num_from_mini_OS;}
 float Float_KBD_to_display(char display_buffer[]){              //Acquires an integer string from the keyboard and returns the binary equivalent
 char keypress;
 char cr_keypress;
-float Float_Num_from_UNO = 0;
+float Float_from_mini_OS = 0;
 float * Flt_ptr_local;
 char * Char_ptr_local;
 char SREG_BKP;
@@ -135,14 +136,14 @@ char SREG_BKP;
 SREG_BKP = SREG;
 sei();
 
-Flt_ptr_local = &Float_Num_from_UNO;
-Char_ptr_local = (char*)&Float_Num_from_UNO;
+Flt_ptr_local = &Float_from_mini_OS;
+Char_ptr_local = (char*)&Float_from_mini_OS;
 
 cr_keypress = 0;                                                //Set to one when carriage return keypress terminates the string
 for(int n = 0; n<=7; n++) display_buffer[n] = 0;              //Clear the buffer used to the string
 
 while(1){
-keypress = waitforkeypress();
+keypress = waitforkeypress_A();
 if ((!(decimal_digit(keypress)))
 && (keypress != '-')
 && (keypress != '.'))continue;                                //Ignore keypress if it is not OK
@@ -152,9 +153,9 @@ break;}
 if (keypress == '.')display_buffer[0] = '0' | 0x80;
 
 
-Send_float_num_string;                                        //Update display with the first key press
+Float_num_string_to_display;                                        //Update display with the first key press
 while(1){
-if ((keypress = wait_for_return_key())  =='\r')break;           //Detect return key press (i.e \r or\r\n)
+if ((keypress = wait_for_return_key_A())  =='\r')break;           //Detect return key press (i.e \r or\r\n)
 
 if ((decimal_digit(keypress)) || (keypress == '.')
 || (keypress == '\b')|| (keypress == '-')
@@ -171,21 +172,32 @@ display_buffer[n] = display_buffer[n-1];
 display_buffer[0] = keypress;}                                  //Add new keypress           
 else display_buffer[0] |= 0x80;}                				//Intregrate decimal point and its parent digit
 
-Send_float_num_string;}}                                      	//Update display includes "cr_keypress"                                                 
+Float_num_string_to_display;}}                                      	//Update display includes "cr_keypress"                                                 
 cr_keypress = 1;                                              	//End of string; return key pressed
-Send_float_num_string;
+Float_num_string_to_display;
 cr_keypress = 0;
 
-One_wire_Tx_char = 'E'; UART_Tx_1_wire();
+f_number_from_mini_OS;
+
+/*One_wire_Tx_char = 'E'; UART_Tx_1_wire();
 for(int m = 0; m <= 3; m++){
 UART_Rx_1_wire(); *Char_ptr_local = One_wire_Rx_char;
 Char_ptr_local += 1;}
 
-Float_Num_from_UNO = *Flt_ptr_local;
+Float_from_mini_OS = *Flt_ptr_local;*/
 
 SREG = SREG_BKP;
-return Float_Num_from_UNO;}
+return Float_from_mini_OS;}
 
+
+/*
+#define f_number_from_mini_OS \
+One_wire_Tx_char = 'E'; UART_Tx_1_wire();\
+for(int m = 0; m <= 3; m++){wdr();\
+UART_Rx_1_wire(); *Char_ptr = One_wire_Rx_char;\
+Char_ptr_local += 1;}\
+Float_from_mini_OS = *Flt_ptr_local;
+*/
 
 
 
