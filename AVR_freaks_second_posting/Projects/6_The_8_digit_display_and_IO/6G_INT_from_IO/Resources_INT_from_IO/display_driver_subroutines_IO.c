02845feea5b33074f7b8f14f6aadf667af9e90e2
aclandmark <mark.osborne15@btinternet.com>
2022-07-26 12:55:53 +0100

    Update display_driver_subroutines_IO.c

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/Resources_INT_from_IO/display_driver_subroutines_IO.c
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/Resources_INT_from_IO/display_driver_subroutines_IO.c
@@ -58,10 +58,11 @@ pause_pin_change_interrupt_on_PC5;
 Char_ptr = (char*)&FP_num;
 One_wire_Tx_char = 'D';                                       //Command 'D' indicates the a floating point number will be sent
 UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){                                  //Split the number into 4 chars
+for(int m = 0; m <= 3; m++){ wdr();                                 //Split the number into 4 chars
 One_wire_Tx_char = *Char_ptr;                                 //and send them individually
 UART_Tx_1_wire(); 
 Char_ptr += 1;}
+
 reinstate_pin_change_interrupt_on_PC5;}
 
 
@@ -71,7 +72,7 @@ pause_pin_change_interrupt_on_PC5;
 
 One_wire_Tx_char = 'C';                                 //Command 'C' indicates the a long number will be sent
 UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){                            //Split the number into 4 chars
+for(int m = 0; m <= 3; m++){ wdr();                           //Split the number into 4 chars
 One_wire_Tx_char = Int_num >> ((3-m) * 8);              //and send them individually
 UART_Tx_1_wire(); 
 }
