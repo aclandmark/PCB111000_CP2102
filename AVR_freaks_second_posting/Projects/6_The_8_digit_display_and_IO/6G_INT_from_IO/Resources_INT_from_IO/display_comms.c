738edd8b9010b4228cb839425c20622aba85d7d6
aclandmark <mark.osborne15@btinternet.com>
2022-07-26 18:19:13 +0100

    Update display_comms.c

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/Resources_INT_from_IO/display_comms.c
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/Resources_INT_from_IO/display_comms.c
@@ -5,6 +5,7 @@ void float_num_to_display(float FP_num){
 char * Char_ptr;
 
 pause_pin_change_interrupt_on_PC5;
+Check_num_for_to_big_or_small(FP_num);
 Char_ptr = (char*)&FP_num;
 One_wire_Tx_char = 'D';                                       //Command 'D' indicates the a floating point number will be sent
 UART_Tx_1_wire();
@@ -17,14 +18,13 @@ reinstate_pin_change_interrupt_on_PC5;}
 
 
 /******************************************************************************************************************************************/
-void int_num_to_display(long Int_num){
+void Int_num_to_display(long Int_num){
 pause_pin_change_interrupt_on_PC5;
-
 One_wire_Tx_char = 'C';                                 //Command 'C' indicates the a long number will be sent
 UART_Tx_1_wire();
 for(int m = 0; m <= 3; m++){ wdr();                           //Split the number into 4 chars
 One_wire_Tx_char = Int_num >> ((3-m) * 8);              //and send them individually
-UART_Tx_1_wire(); 
+UART_Tx_1_wire(); wdr();
 }
 reinstate_pin_change_interrupt_on_PC5;}
 
