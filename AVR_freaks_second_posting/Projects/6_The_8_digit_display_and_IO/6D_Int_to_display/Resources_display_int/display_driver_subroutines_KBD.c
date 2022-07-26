779771787251059a92cc5c3eb9cd6cbd4f342337
aclandmark <mark.osborne15@btinternet.com>
2022-07-26 18:18:56 +0100

    Update display_driver_subroutines_KBD.c

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_Int_to_display/Resources_display_int/display_driver_subroutines_KBD.c
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_Int_to_display/Resources_display_int/display_driver_subroutines_KBD.c
@@ -48,7 +48,7 @@ Check_num_for_to_big_or_small(FP_num);
 Char_ptr = (char*)&FP_num;
 One_wire_Tx_char = 'D'; 										//Command 'D' indicates the a floating point number will be sent
 UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){									//Split the number into 4 chars
+for(int m = 0; m <= 3; m++){wdr();									//Split the number into 4 chars
 One_wire_Tx_char = *Char_ptr;									//and send them individually
 UART_Tx_1_wire();	
 Char_ptr += 1;
@@ -63,7 +63,7 @@ void Int_num_to_display(long Int_num){
 pause_pin_change_interrupt_on_PC5;
 One_wire_Tx_char = 'C';                                 		//Command 'C' indicates the a long number will be sent
 UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){                            		//Split the number into 4 chars
+for(int m = 0; m <= 3; m++){wdr();                            		//Split the number into 4 chars
 One_wire_Tx_char = Int_num >> ((3-m) * 8);              		//and send them individually
 UART_Tx_1_wire(); wdr();
 }
