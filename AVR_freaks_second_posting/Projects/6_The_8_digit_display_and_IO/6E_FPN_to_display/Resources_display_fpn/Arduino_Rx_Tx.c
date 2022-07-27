487e84f80389d6bc9ad88bb0138f9e671365e6da
aclandmark <mark.osborne15@btinternet.com>
2022-07-27 09:35:14 +0100

    Update Arduino_Rx_Tx.c

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6E_FPN_to_display/Resources_display_fpn/Arduino_Rx_Tx.c
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6E_FPN_to_display/Resources_display_fpn/Arduino_Rx_Tx.c
@@ -9,8 +9,10 @@ unsigned long Unsigned_Int_from_PC(char * num_as_string,char next_char)
 {char strln;
 int num;
 
+pause_WDT;
 Serial.flush();   
 strln = Serial.readBytesUntil('\r',num_as_string, 20);
+resume_WDT;
 num_as_string[strln] = 0;
 
 if(next_char){
@@ -26,8 +28,10 @@ return (unsigned int )num;}
 long Int_Num_from_PC(char * num_as_string,char next_char)
 {char strln;
 
+pause_WDT;
 Serial.flush();   
 strln = Serial.readBytesUntil('\r',num_as_string, 20);
+resume_WDT;
 num_as_string[strln] = 0;
 Serial.write(num_as_string);
 Serial.write(next_char);
@@ -88,10 +92,10 @@ Serial.write(next_char);}
 void Check_num_for_to_big_or_small(float num)
 {unsigned long * long_ptr;
 long_ptr = (unsigned long *)&num;
-if (*long_ptr == 0x7F800000){Serial.write("+ve Num too large\r\n"); SW_reset;}
-if (*long_ptr == 0xFF800000){Serial.write("-ve Num too large\r\n"); SW_reset;}
-if (*long_ptr == 0X0){Serial.write("+ve Num too small\r\n"); SW_reset;}
-if (*long_ptr == 0X80000000){Serial.write("-ve Num too small\r\n"); SW_reset;}}
+if (*long_ptr == 0x7F800000){Serial.write("+ve Num too large\r\n");SW_reset;}
+if (*long_ptr == 0xFF800000){Serial.write("-ve Num too large\r\n");SW_reset;}
+if (*long_ptr == 0X0){Serial.write("+ve Num too small\r\n");SW_reset;}
+if (*long_ptr == 0X80000000){Serial.write("-ve Num too small\r\n");SW_reset;}}
 
 
 
@@ -99,8 +103,10 @@ if (*long_ptr == 0X80000000){Serial.write("-ve Num too small\r\n"); SW_reset;}}
 float Sc_Num_from_PC(char * num_as_string,char next_char)
 {char strln;
 
+pause_WDT;
 Serial.flush();   
 strln = Serial.readBytesUntil('\r',num_as_string, 20);
+resume_WDT;
 num_as_string[strln] = 0;
 Serial.write(num_as_string);
 Serial.write(next_char);
