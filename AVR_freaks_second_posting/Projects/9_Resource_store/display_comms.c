618d6c34b294ea3f8c19cf7151c1a761616a21e6
aclandmark <mark.osborne15@btinternet.com>
2022-07-28 17:15:42 +0100

    Update display_comms.c

--- AVR_freaks_second_posting/Projects/9_Resource_store/display_comms.c
+++ AVR_freaks_second_posting/Projects/9_Resource_store/display_comms.c
@@ -8,6 +8,7 @@ void Check_num_for_to_big_or_small(float);
 
 
 
+
 /******************************************************************************************************************************************/
 void One_wire_Tx_2_integers(unsigned int NUM_1, unsigned int NUM_2){	//Transaction: Send type and two 16 bit words to the ATtiny1608
 pause_pin_change_interrupt_on_PC5;										//Reset control not allowed during transaction
@@ -22,20 +23,6 @@ reinstate_pin_change_interrupt_on_PC5;}
 
 
 
-void One_wire_Tx_2_integers_with_timer_control(unsigned int NUM_1, unsigned int NUM_2){			//Transaction: Send type and two 16 bit words to the ATtiny1608
-pause_pin_change_interrupt_on_PC5;																//Reset control not allowed during transaction
-pause_PCI;
-pause_timer;
-One_wire_Tx_char = 'a';  UART_Tx_1_wire();														//Transaction type is 'a'
-One_wire_Tx_char = NUM_1;  UART_Tx_1_wire(); 													//Send lower 8 bits
-One_wire_Tx_char = NUM_2;  UART_Tx_1_wire(); 
-wdr();
-One_wire_Tx_char = NUM_1 >> 8;  UART_Tx_1_wire();												//Send upper 8 bits 
-One_wire_Tx_char = NUM_2 >> 8;  UART_Tx_1_wire();
-reinstate_pin_change_interrupt_on_PC5;
-resume_timer;
-reinstate_PCI;
-}
 
 
 
@@ -88,12 +75,12 @@ reinstate_pin_change_interrupt_on_PC5;}
 
 
 /******************************************************************************************************************************************/
-void Check_num_for_to_big_or_small(float num)
+/*void Check_num_for_to_big_or_small(float num)
 {unsigned long * long_ptr;
 long_ptr = (unsigned long *)&num;
 if (*long_ptr == 0x7F800000){Serial.write("+ve Num too large\r\n");SW_reset;}
 if (*long_ptr == 0xFF800000){Serial.write("-ve Num too large\r\n");SW_reset;}
 if (*long_ptr == 0X0){Serial.write("+ve Num too small\r\n");SW_reset;}
-if (*long_ptr == 0X80000000){Serial.write("-ve Num too small\r\n");SW_reset;}}
+if (*long_ptr == 0X80000000){Serial.write("-ve Num too small\r\n");SW_reset;}}*/
 
 
