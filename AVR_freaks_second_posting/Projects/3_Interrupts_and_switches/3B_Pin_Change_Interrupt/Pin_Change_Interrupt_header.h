bb2aedf882163726fc135b3de3647380d103335f
aclandmark <mark.osborne15@btinternet.com>
2022-07-28 15:47:16 +0100

    Update Pin_Change_Interrupt_header.h

--- AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3B_Pin_Change_Interrupt/Pin_Change_Interrupt_header.h
+++ AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3B_Pin_Change_Interrupt/Pin_Change_Interrupt_header.h
@@ -141,7 +141,7 @@ if(reset_status == 6)\
 #include "Resources_3B\One_wire_header.h"
 #include "Resources_3B\One_wire_transactions.c"
 #include "Resources_3B\Basic_IO_and_Timer.c"
-#include "Resources_3B\Proj_3B_subroutines.c"
+#include "Resources_3B\display_comms.c"
 
 
 
