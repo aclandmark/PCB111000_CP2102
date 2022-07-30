2f5cd17e82eaa66ab3396481dc8acf5f5574a767
aclandmark <mark.osborne15@btinternet.com>
2022-07-30 15:53:04 +0100

    Update First_project_subroutines.c

--- AVR_freaks_second_posting/Projects/1_first_project/Resources_first_project/First_project_subroutines.c
+++ AVR_freaks_second_posting/Projects/1_first_project/Resources_first_project/First_project_subroutines.c
@@ -9,7 +9,7 @@ String_to_PC("\r\n");
 void print_eep_address (unsigned int);
 
 
-void One_wire_Tx_2_integers(unsigned int NUM_1, unsigned int NUM_2){	//Transaction: Send type and two 16 bit words to the ATtiny1608
+/*void One_wire_Tx_2_integers(unsigned int NUM_1, unsigned int NUM_2){	//Transaction: Send type and two 16 bit words to the ATtiny1608
 pause_pin_change_interrupt_on_PC5;										//Reset control not allowed during transaction
 One_wire_Tx_char = 'a';  UART_Tx_1_wire();								//Transaction type is 'a'
 One_wire_Tx_char = NUM_1;  UART_Tx_1_wire(); 							//Send lower 8 bits
@@ -18,7 +18,7 @@ wdr();
 One_wire_Tx_char = NUM_1 >> 8;  UART_Tx_1_wire();						//Send upper 8 bits 
 One_wire_Tx_char = NUM_2 >> 8;  UART_Tx_1_wire(); 
 reinstate_pin_change_interrupt_on_PC5;}
-
+*/
 
 
 /************************************************************************************************************************************************/
@@ -50,12 +50,12 @@ return lfsr;}
 
 
 /************************************************************************************************************************************************/
-void One_wire_comms_any_segment(char letter, char digit_num){
+/*void One_wire_comms_any_segment(char letter, char digit_num){
 pause_pin_change_interrupt_on_PC5;										//Reset control not allowed during transaction
 One_wire_Tx_char = 'b';			UART_Tx_1_wire(); wdr();
 One_wire_Tx_char = letter;		UART_Tx_1_wire(); wdr();
 One_wire_Tx_char = digit_num;	UART_Tx_1_wire(); wdr();
-reinstate_pin_change_interrupt_on_PC5;}
+reinstate_pin_change_interrupt_on_PC5;}*/
 
 
 
