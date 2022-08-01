ea54ab9f6550efc25374bb28e928bbc8e9e858e9
aclandmark <mark.osborne15@btinternet.com>
2022-08-01 15:26:29 +0100

    Update Segment_driver_header.h

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6A_Segment_driver/Segment_driver_header.h
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6A_Segment_driver/Segment_driver_header.h
@@ -158,8 +158,8 @@ if((User_response == 'R') || (User_response == 'r'))break;} Serial.write("\r\n")
 /************************************************************************************************************************************/
 #include "Resources_segment_driver\Chip2chip_comms\One_wire_header.h"
 #include "Resources_segment_driver\Chip2chip_comms\One_wire_transactions.c"
+#include "Resources_segment_driver\Chip2chip_comms\Display_driver.c"
 #include "Resources_segment_driver\PC_comms\Basic_Rx_Tx_and_Timer.c"
-#include "Resources_segment_driver\Segment_driver_subroutines.c"
 
 
 
