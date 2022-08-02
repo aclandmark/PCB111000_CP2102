ebdebc4253ae67a643e4777bea4fb76c6d9bae06
aclandmark <mark.osborne15@btinternet.com>
2022-08-02 17:16:39 +0100

    Update Stand_alone_clock_subroutines.c

--- AVR_freaks_second_posting/Projects/7_Timers/4_stand_alone_clock/Resources_Stand_alone_clock/Stand_alone_clock_subroutines.c
+++ AVR_freaks_second_posting/Projects/7_Timers/4_stand_alone_clock/Resources_Stand_alone_clock/Stand_alone_clock_subroutines.c
@@ -32,7 +32,7 @@ ISR (TIMER2_COMPA_vect){ char string[5];
 void deci_Seconds_to_display(long num){
 One_wire_Tx_char = 'L';                           //Command 'C' indicates the a long number will be sent
 UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){
+for(int m = 0; m <= 3; m++){wdr();
 One_wire_Tx_char = num >> (8 * (3 - m));        //Split the number into 4 chars
 UART_Tx_1_wire();}}                             //and send them individually
 
@@ -53,9 +53,9 @@ deci_sec_counter = (deci_sec_counter << 8) | {One_wire_Rx_char;}}}
 #define deci_secs_from_mini_OS \
 {deci_sec_counter = 0;\
 One_wire_Tx_char = 'M'; UART_Tx_1_wire();\
-for(int m = 0; m <= 3; m++){\
+for(int m = 0; m <= 3; m++){wdr();\
 UART_Rx_1_wire(); deci_secs_byte[m] = One_wire_Rx_char;}\
 \
-for(int m = 0; m <= 3; m++){\
+for(int m = 0; m <= 3; m++){wdr();\
 deci_sec_counter = deci_sec_counter << 8;\
 deci_sec_counter |= deci_secs_byte[m];}}
