6e2958068bbe1a10129816034e53522d752abeef
aclandmark <mark.osborne15@btinternet.com>
2022-08-04 15:09:37 +0100

    Update KBD_to_display.c

--- AVR_freaks_second_posting/Projects/9_Resource_store/PC_comms/KBD_to_display.c
+++ AVR_freaks_second_posting/Projects/9_Resource_store/PC_comms/KBD_to_display.c
@@ -1,13 +1,17 @@
 
-
+/*
+Obviously Arduino does not provide functions to drive the display directly using keyboard entry.
+The DIY versions provided here indicate the level of complexity required.
+Tests have shown that they are generally OK but there is probably a bug some where in there.
+*/
 
 
 /******************************************************************************************************************************************/
-long Int_KBD_to_display(char display_buffer[]){              //Acquires an integer string from the keyboard and returns the binary equivalent
+long Int_KBD_to_display(char display_buffer[]){              	//Acquires an integer string from the keyboard and returns the binary equivalent
 char keypress;
 long Long_Num_from_mini_OS;
 char cr_keypress;
-unsigned char num_byte[4];
+unsigned char num_byte[4];										//Mini-OS converts string to binary and returns the result as four 8 bit character numbers
 
 cr_keypress = 0;                                            	//Set to one when carriage return keypress terminates the string
 for(int n = 0; n<=8; n++) display_buffer[n] = 0;           		//Clear the buffer used for the string
@@ -19,7 +23,7 @@ if ((!(decimal_digit(keypress)))
 display_buffer[0] = keypress;
 break;}
  
-Int_num_string_to_display;
+Int_num_string_to_display;										//See "Chip2chip_comms\Display_driver_header.h"
 while(1){
 if ((keypress = wait_for_return_key_A())  =='\r')break;        	//Detect return key press (i.e \r or\r\n)
 if ((decimal_digit(keypress)) || (keypress == '\b')\
@@ -33,21 +37,14 @@ else
 display_buffer[n] = display_buffer[n-1];
 display_buffer[0] = keypress;  }                             	//Add new keypress           
 
-Int_num_string_to_display;}}                                      	//Update display includes "cr_keypress"                                                 
+Int_num_string_to_display;}}                                     //Update display includes "cr_keypress"                                                 
 cr_keypress = 1;                                            	//End of string; return key press detected
 
 Int_num_string_to_display;
 cr_keypress = 0;
 
-Int_from_mini_OS;
+Int_from_mini_OS;												//See "Chip2chip_comms\Display_driver_header.h"
 
-/*One_wire_Tx_char = 'E'; UART_Tx_1_wire();						//Command ATtiny1606:  Waiting to receive number in binary form							
-for(int m = 0; m <= 3; m++){
-UART_Rx_1_wire(); num_byte[m] = One_wire_Rx_char;}
-
-for(int m = 0; m <= 3; m++){									//Recover number as 4 bytes and re-assemble them
-Long_Num_from_mini_OS = Long_Num_from_mini_OS << 8;
-Long_Num_from_mini_OS |= num_byte[m];}*/
 
 return Long_Num_from_mini_OS;}
 
@@ -55,43 +52,39 @@ return Long_Num_from_mini_OS;}
 
 
 /********************************************************************************************************************************************/
-float Float_KBD_to_display(char display_buffer[]){              //Acquires an integer string from the keyboard and returns the binary equivalent
+float Float_KBD_to_display(char display_buffer[]){              //Operation is similar to that of Int_KBD_to_display()
 char keypress;
 char cr_keypress;
 float Float_from_mini_OS = 0;
-float * Flt_ptr;
+float * Flt_ptr;												//"Float_from_mini_OS" can be addresses as a FPN or four 8 bit character numbers										
 char * Char_ptr;
-char SREG_BKP;
 
-SREG_BKP = SREG;
-sei();
-
-Flt_ptr = &Float_from_mini_OS;
-Char_ptr = (char*)&Float_from_mini_OS;
+Flt_ptr = &Float_from_mini_OS;									//Flt_ptr addresses the entire 32 bit FPN
+Char_ptr = (char*)&Float_from_mini_OS;							//Char_ptr addresses the first 8 bits of the FPN 
 
 cr_keypress = 0;                                                //Set to one when carriage return keypress terminates the string
-for(int n = 0; n<=7; n++) display_buffer[n] = 0;              //Clear the buffer used to the string
+for(int n = 0; n<=7; n++) display_buffer[n] = 0;              	//Clear the buffer used to the string
 
-while(1){
+while(1){														//Remain in loop until a valid character is received
 keypress = waitforkeypress_A();
 if ((!(decimal_digit(keypress)))
 && (keypress != '-')
-&& (keypress != '.'))continue;                                //Ignore keypress if it is not OK
+&& (keypress != '.'))continue;                                	//Ignore keypress if it is not OK
 display_buffer[0] = keypress;
 break;}
 
-if (keypress == '.')display_buffer[0] = '0' | 0x80;
+if (keypress == '.')display_buffer[0] = '0' | 0x80;				//Bit 7 is set if the digit includes the decimal point
 
+Float_num_string_to_display;                                      //Update display with the first key press
 
-Float_num_string_to_display;                                        //Update display with the first key press
 while(1){
-if ((keypress = wait_for_return_key_A())  =='\r')break;           //Detect return key press (i.e \r or\r\n)
+if ((keypress = wait_for_return_key_A())  =='\r')break;           //Stay in loop until return key press is detected
 
-if ((decimal_digit(keypress)) || (keypress == '.')
+if ((decimal_digit(keypress)) || (keypress == '.')				//Check for valid keypresses
 || (keypress == '\b')|| (keypress == '-')
 || (keypress == 'E') || (keypress == 'e'))
 
-{if(keypress == '\b'){for (int n = 0; n <= 7; n++)
+{if(keypress == '\b'){for (int n = 0; n <= 7; n++)				//Backspace keypress
 display_buffer[n] = display_buffer[n + 1];}
 
 else
@@ -100,35 +93,16 @@ else
 {for(int n = 7; n>=1; n--)                                    	//Shift display for each new keypress except '.'
 display_buffer[n] = display_buffer[n-1];
 display_buffer[0] = keypress;}                                  //Add new keypress           
-else display_buffer[0] |= 0x80;}                				//Intregrate decimal point and its parent digit
+else display_buffer[0] |= 0x80;}                				//Integrate decimal point with its parent digit
 
-Float_num_string_to_display;}}                                      	//Update display includes "cr_keypress"                                                 
+Float_num_string_to_display;}}                                  //Update display includes "cr_keypress"                                                 
 cr_keypress = 1;                                              	//End of string; return key pressed
 Float_num_string_to_display;
 cr_keypress = 0;
 
-f_number_from_mini_OS;
+f_number_from_mini_OS;											//See "Chip2chip_comms\Display_driver_header.h"
 
-/*One_wire_Tx_char = 'E'; UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){
-UART_Rx_1_wire(); *Char_ptr_local = One_wire_Rx_char;
-Char_ptr_local += 1;}
-
-Float_from_mini_OS = *Flt_ptr_local;*/
-
-SREG = SREG_BKP;
 return Float_from_mini_OS;}
-
-
-/*
-#define f_number_from_mini_OS \
-One_wire_Tx_char = 'E'; UART_Tx_1_wire();\
-for(int m = 0; m <= 3; m++){wdr();\
-UART_Rx_1_wire(); *Char_ptr = One_wire_Rx_char;\
-Char_ptr_local += 1;}\
-Float_from_mini_OS = *Flt_ptr_local;
-*/
-
 
 
 
