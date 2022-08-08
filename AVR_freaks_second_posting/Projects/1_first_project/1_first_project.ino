31b4ab6fa03b7f9e87239a4e721024751423c55c
aclandmark <mark.osborne15@btinternet.com>
2022-08-08 18:14:58 +0100

    Update 1_first_project.ino

--- AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
+++ AVR_freaks_second_posting/Projects/1_first_project/1_first_project.ino
@@ -2,16 +2,22 @@
 
 
 
-/*A selection of short programs designed to shown what a simple C program might look like
- and give practice entering and compiling several of them.
+/*
+A selection of short programs designed to show what a simple C program might look like
+and give practice entering and compiling several of them.
 
- It introduces
- one_wire subroutines which send data to the mini_OS that resides on the ATtiny1606
- and drives the display
- A PRN generator that produces pseudo random numbers
- The setup_328_HW_Arduino_IO: This sets up the IO, the watch dog timer, calibrates the 8MHz clock etc...
- The SW_reset macro: This enables the Atmega328 to be reset at any time.
- */
+It introduces:
+one_wire subroutines which send data to the mini_OS that resides on the ATtiny1606
+and drives the display.
+ 
+The setup_328_HW_Arduino_IO: This sets up the IO, the watch dog timer, calibrates the 8MHz clock etc...
+The SW_reset macro: This enables the Atmega328 to be reset at any time.
+
+The purpose of the watch dog timer (WDT) is to reset the program if it crashes 
+wdr(): This is needed to reset the watchdog timer and prevent it from timing out and resetting the program unnecessarily. 
+
+A PRN generator that produces pseudo random numbers.
+*/
 
 
 
