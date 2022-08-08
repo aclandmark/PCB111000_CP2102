71f7dca2f9d1ce9279b644141b083af31e185b9b
aclandmark <mark.osborne15@btinternet.com>
2022-08-08 17:33:21 +0100

    Update First_project_subroutines.c

--- AVR_freaks_second_posting/Projects/1_first_project/Resources_first_project/First_project_subroutines.c
+++ AVR_freaks_second_posting/Projects/1_first_project/Resources_first_project/First_project_subroutines.c
@@ -1,24 +1,13 @@
 
 
-#define check_eep_addresses \
+/*#define check_eep_addresses \
 print_eep_address(0x3F3);\
 Char_to_PC(' ');\
 print_eep_address(0x3F2);\
 String_to_PC("\r\n");
 
-void print_eep_address (unsigned int);
+void print_eep_address (unsigned int);*/
 
-
-/*void One_wire_Tx_2_integers(unsigned int NUM_1, unsigned int NUM_2){	//Transaction: Send type and two 16 bit words to the ATtiny1608
-pause_pin_change_interrupt_on_PC5;										//Reset control not allowed during transaction
-One_wire_Tx_char = 'a';  UART_Tx_1_wire();								//Transaction type is 'a'
-One_wire_Tx_char = NUM_1;  UART_Tx_1_wire(); 							//Send lower 8 bits
-One_wire_Tx_char = NUM_2;  UART_Tx_1_wire(); 
-wdr();
-One_wire_Tx_char = NUM_1 >> 8;  UART_Tx_1_wire();						//Send upper 8 bits 
-One_wire_Tx_char = NUM_2 >> 8;  UART_Tx_1_wire(); 
-reinstate_pin_change_interrupt_on_PC5;}
-*/
 
 
 /************************************************************************************************************************************************/
@@ -41,7 +30,6 @@ if(*PRN_counter == 16)
 
 {eeprom_write_byte((uint8_t*)(0x3F2 - eep_offset),(lfsr>>8));
 eeprom_write_byte((uint8_t*)(0x3F3 - eep_offset),lfsr);
-//check_eep_addresses;
 Toggle_LED_1;
 *PRN_counter = 0;}
 
@@ -50,21 +38,11 @@ return lfsr;}
 
 
 /************************************************************************************************************************************************/
-/*void One_wire_comms_any_segment(char letter, char digit_num){
-pause_pin_change_interrupt_on_PC5;										//Reset control not allowed during transaction
-One_wire_Tx_char = 'b';			UART_Tx_1_wire(); wdr();
-One_wire_Tx_char = letter;		UART_Tx_1_wire(); wdr();
-One_wire_Tx_char = digit_num;	UART_Tx_1_wire(); wdr();
-reinstate_pin_change_interrupt_on_PC5;}*/
-
-
-
-/************************************************************************************************************************************************/
-void print_eep_address (unsigned int eep_address)
+/*void print_eep_address (unsigned int eep_address)
 {char num_string[10];
 eep_address -= eeprom_read_byte((uint8_t*)(0x3ED));
 itoa (eep_address, num_string, 16 );
-{int m = 0; while (num_string[m])Char_to_PC_Basic(num_string[m++]);}}
+{int m = 0; while (num_string[m])Char_to_PC_Basic(num_string[m++]);}}*/
 
 
 
