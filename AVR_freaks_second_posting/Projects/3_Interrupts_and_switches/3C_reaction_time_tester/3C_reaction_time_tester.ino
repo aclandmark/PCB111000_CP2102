e86b7811905bbef345aaa39e76b5734992a86f70
aclandmark <mark.osborne15@btinternet.com>
2022-08-09 15:35:52 +0100

    Update 3C_reaction_time_tester.ino

--- AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3C_reaction_time_tester/3C_reaction_time_tester.ino
+++ AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3C_reaction_time_tester/3C_reaction_time_tester.ino
@@ -1,6 +1,6 @@
 
 /*
- Vey similar to Proj 3B.  
+ Very similar to Project 3B.  
  Press sw2 when the led bars line up to make the display flash
  */
 
@@ -58,3 +58,8 @@ eeprom_write_byte((uint8_t*)(0x1FF),(lfsr>>8));
 eeprom_write_byte((uint8_t*)(0x1FE),lfsr);}
 
 return lfsr;}
+
+
+
+
+/******************************************************************/
