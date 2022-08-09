24a22bc17eda401985efd500920402af937d615e
aclandmark <mark.osborne15@btinternet.com>
2022-08-09 15:36:02 +0100

    Update 3D_timer_PCI_interrupts.ino

--- AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3D_timer_PCI_interrupts/3D_timer_PCI_interrupts.ino
+++ AVR_freaks_second_posting/Projects/3_Interrupts_and_switches/3D_timer_PCI_interrupts/3D_timer_PCI_interrupts.ino
@@ -3,14 +3,13 @@
  Timer interrupts drive the display with any one of thee patterns
  User switch SW1, 2 or 3 are used to select the pattern.
  
- Three interrupts are active
- The timer, the one wire comms and PCI
+ Three interrupts are active:
+ the timer, the one wire comms and PCI
  To simplify operation the PCI ISR resets the Atmega328.
  However before doing this it writes to the EEPROM.
  EEPROM is special memory the survives reset.
  The first thing the program does following a reset is to 
  read the EEPROM to determine which pattern is required.
- 
  */
 
 
@@ -127,17 +126,23 @@ void Inc_Display()
 
 
 
-void One_wire_Tx_2_integers_with_timer_control(unsigned int NUM_1, unsigned int NUM_2){      //Transaction: Send type and two 16 bit words to the ATtiny1608
-pause_pin_change_interrupt_on_PC5;                                //Reset control not allowed during transaction
+void One_wire_Tx_2_integers_with_timer_control
+(unsigned int NUM_1, unsigned int NUM_2){                             //Transaction: Send type and two 16 bit words to the ATtiny1608
+pause_pin_change_interrupt_on_PC5;                                    //Reset control not allowed during transaction
 pause_PCI;
-pause_timer;
+pause_timer;                                                          //No timer interrupt wanted during one wire comms
 One_wire_Tx_char = 'a';  UART_Tx_1_wire();                            //Transaction type is 'a'
 One_wire_Tx_char = NUM_1;  UART_Tx_1_wire();                          //Send lower 8 bits
 One_wire_Tx_char = NUM_2;  UART_Tx_1_wire(); 
 wdr();
-One_wire_Tx_char = NUM_1 >> 8;  UART_Tx_1_wire();                       //Send upper 8 bits 
+One_wire_Tx_char = NUM_1 >> 8;  UART_Tx_1_wire();                     //Send upper 8 bits 
 One_wire_Tx_char = NUM_2 >> 8;  UART_Tx_1_wire();
 reinstate_pin_change_interrupt_on_PC5;
 resume_timer;
 reinstate_PCI;
 }
+
+
+
+
+/*****************************************************************************************************************/
