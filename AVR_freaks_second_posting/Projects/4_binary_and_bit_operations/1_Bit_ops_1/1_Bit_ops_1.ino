feb52e48f0735086a1069773932adefe7744f3ea
aclandmark <mark.osborne15@btinternet.com>
2022-08-11 14:53:14 +0100

    Update 1_Bit_ops_1.ino

--- AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/1_Bit_ops_1/1_Bit_ops_1.ino
+++ AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/1_Bit_ops_1/1_Bit_ops_1.ino
@@ -9,28 +9,27 @@ int main (void){
 char comp;
 char digits[3];
 unsigned char lfsr;
-char BWop;                                  //bit wise operation and complement (i.e. swap ones and zeros)
-
+char BWop;                                                                   //bit wise operation and complement (i.e. swap ones and zeros)
 
 setup_328_HW_Basic_IO;
 
-
 String_to_PC_Basic("\r\nSelect OP:  |   ^   &   ~|  ~^  or  ~&");
 BWop = waitforkeypress_Basic();
-if (BWop == '~') {comp = 1; BWop = waitforkeypress_Basic();}else comp = 0;        //detect complement operator
+if (BWop == '~') 
+{comp = 1; BWop = waitforkeypress_Basic();}else comp = 0;                    //detect complement operator
 if ((BWop != '|') && (BWop != '^') && (BWop != '&'))
-SW_reset;                                                              //reset if duff char was sent 
+SW_reset;                                                                    //reset if duff char was sent 
 Reset_ATtiny1606;
 
   
-lfsr = PRN_8bit_GEN(0xF);
+lfsr = PRN_8bit_GEN(0xF);                                                   //8 bit random number
 do{
 digits[0] = PRN_8bit_GEN(lfsr);
-digits[1] = PRN_8bit_GEN(digits[0]);
-digits[2] =  Op(digits[0] , digits[1], comp, BWop);
+digits[1] = PRN_8bit_GEN(digits[0]);                                        //Second random number
+digits[2] =  Op(digits[0] , digits[1], comp, BWop);                         //Process the numbers
 lfsr = digits[1];
-One_wire_comms_3_bytes(digits);}  while (waitforkeypress_Basic() !='x');                
-//wdt_enable(WDTO_30MS); while(1);
+One_wire_comms_3_bytes(digits);}
+while (waitforkeypress_Basic() !='x');                                      //Press 'x' to ecscape               
 SW_reset;}
 
 
