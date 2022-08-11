1135dd654a063e3e8213b8e2e39d68c258038101
aclandmark <mark.osborne15@btinternet.com>
2022-08-11 21:54:05 +0100

    Update 1_Bit_ops_1.ino

--- AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/1_Bit_ops_1/1_Bit_ops_1.ino
+++ AVR_freaks_second_posting/Projects/4_binary_and_bit_operations/1_Bit_ops_1/1_Bit_ops_1.ino
@@ -11,26 +11,29 @@
 int main (void){
 
 char comp;
-char digits[3];
+unsigned char digits[3];
 unsigned char lfsr;
 char BWop;                                                                   //bit wise operation and complement (i.e. swap ones and zeros)
 
 setup_328_HW_Basic_IO;
 
 String_to_PC_Basic("\r\nSelect OP:  |   ^   &   ~|  ~^  or  ~&");
-BWop = waitforkeypress_Basic();
+BWop = waitforkeypress_Basic(); 
 if (BWop == '~') 
 {comp = 1; BWop = waitforkeypress_Basic();}else comp = 0;                    //detect complement operator
 if ((BWop != '|') && (BWop != '^') && (BWop != '&'))
 SW_reset;                                                                    //reset if duff char was sent 
 Reset_ATtiny1606;
-
-  
+//wdr();
 lfsr = PRN_8bit_GEN(0xF);                                                   //8 bit random number
+
 do{
 digits[0] = PRN_8bit_GEN(lfsr);
 digits[1] = PRN_8bit_GEN(digits[0]);                                        //Second random number
 digits[2] =  Op(digits[0] , digits[1], comp, BWop);                         //Process the numbers
+
+Char_to_PC_as_Binary(digits[0]);Char_to_PC_as_Binary(digits[1]);Char_to_PC_as_Binary(digits[2]);Char_to_PC_Basic ('\r');
+
 lfsr = digits[1];
 One_wire_comms_3_bytes(digits);}
 while (waitforkeypress_Basic() !='x');                                      //Press 'x' to escape               
@@ -40,7 +43,7 @@ SW_reset;}
 
 
 /*************************************************************************************/
-char Op(char A, char B, char comp, char BWOp)
+unsigned char Op(unsigned char A, unsigned char B, char comp, char BWOp)
 {char result=0;
 switch (BWOp){
 case '|': result = A | B; break;
@@ -64,3 +67,10 @@ return lfsr;}
 
 
 /******************************************************************/
+
+
+void Char_to_PC_as_Binary(unsigned char num){
+Char_to_PC_Basic ('\t');
+for(int m = 0; m <= 7; m++){
+  if (num & (0x80 >> m)) Char_to_PC_Basic ('1'); else Char_to_PC_Basic ('0');Char_to_PC_Basic (' ');}
+  }
