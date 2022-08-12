58ef3a2d7fc86bfa4a336e64e068a6ce7e107547
aclandmark <mark.osborne15@btinternet.com>
2022-08-12 17:49:33 +0100

    Update 5B_Prime_factors.ino

--- AVR_freaks_second_posting/Projects/5_Arduino_PC_comms_library/5B_Prime_factors/5B_Prime_factors.ino
+++ AVR_freaks_second_posting/Projects/5_Arduino_PC_comms_library/5B_Prime_factors/5B_Prime_factors.ino
@@ -25,45 +25,46 @@ int main (void)
  
  setup_328_HW_Arduino_IO;
     
-   if (reset_status == 1) User_prompt;
+   if (reset_status == 1) User_prompt;                            //Only required for a POR
  
    Serial.write("\r\nInteger number\t");
-number = Int_Num_from_PC(num_string, '\r');
-factor_counter = 0;
-{int  n = 0; int m=0;
-do{
-factor = Product_search(number);                                   //Get lowest factor
-number = number/factor;                                            //Next number to factorise
-Int_Num_to_PC(factor,num_string, '\t' );n++;
-factor_counter += 1;} 
-while (number != 1);  
+   number = Int_Num_from_PC(num_string, '\r');
+  factor_counter = 0;
+  {
+  do{
+  factor = Product_search(number);                                 //Get lowest factor
+  number = number/factor;                                          //Next number to factorise
+  Int_Num_to_PC(factor,num_string, '\t' );
+  factor_counter += 1;} 
+  while (number != 1);  
 
-if(factor_counter == 1){Serial.write("Prime\r");}}
-SW_reset;
- return 1;}
-
-
+  if(factor_counter == 1){Serial.write("Prime\r");}}
+  SW_reset;
+  return 1;}
 
 
+
+/************************************************************************************************************/
 long Product_search  (long number)  {
 long search_array[200]; 
 int j=0;
 long n=200;
 long L=0;
 
-//char test_array[12];
-
 while(1){
-for(long k =0; k < n; k++) 
+for(long k =0; k < n; k++)                              //Populate array (i.e. with 401, 402, 403, ..etc) 
 {search_array[k] = k+1+n*L;}
-prime_no_generator(L,n,search_array); 
-j=0; while(search_array[j] == 0)j += 1;
+prime_no_generator(L,n,search_array);                   //Set all non primes in array to zero
+j=0; 
+while(search_array[j] == 0)j += 1;                     //Select first prime number
 while (j<=200)
 {if ((search_array[j] >= 2) &&
-(number%search_array[j] == 0))
-{return search_array[j];}j += 1;}
-if(L*200 > number)return 0;
-L += 1;Serial.write('.');}}
+(number%search_array[j] == 0))                         //Scan array until a factor is found
+{return search_array[j];}                              //and return it
+j += 1;} 
+if(L*200 > number)return 0;                            //No factors found at all: Return zero for prime number
+L += 1;Serial.write('.');}}                            //No factor found in array: increment L and populate new arrray
+
 
 
 /************************************************************************************************************/
