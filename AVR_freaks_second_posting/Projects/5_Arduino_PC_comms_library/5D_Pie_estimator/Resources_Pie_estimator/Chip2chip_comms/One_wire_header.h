b26e5122d97d90eefc8f2e99e3f003985dd0972d
aclandmark <mark.osborne15@btinternet.com>
2022-08-12 16:25:28 +0100

    Update One_wire_header.h

--- AVR_freaks_second_posting/Projects/5_Arduino_PC_comms_library/5D_Pie_estimator/Resources_Pie_estimator/Chip2chip_comms/One_wire_header.h
+++ AVR_freaks_second_posting/Projects/5_Arduino_PC_comms_library/5D_Pie_estimator/Resources_Pie_estimator/Chip2chip_comms/One_wire_header.h
@@ -1,4 +1,11 @@
 
+
+/*
+Code segments required by the subroutines in "One_wire_transactions.c" and else where
+*/
+
+
+
 void UART_Tx_1_wire(void);
 void UART_Rx_1_wire(void);
 
@@ -19,7 +26,7 @@ if (((signed char)eeprom_read_byte((uint8_t*)0x3F6) > -50)\
 == (signed char)eeprom_read_byte((uint8_t*)0x3F7)))\
 {Comms_clock = 200 + (signed char) eeprom_read_byte((uint8_t*)0x3F6);}
 
-//A calibration byte for the one wire comms clock is stored in EEPROM loactons 0x3F6/7
+//A calibration byte for the one wire comms clock is stored in EEPROM locations 0x3F6/7
 //For zero calibration the clock which runs at 1MHz generates a baud rate of 200uS
 
 
@@ -32,9 +39,10 @@ PORTB &= (~(1 << PORTB4));
 
 //One wire comms uses port PB4.  This sets it up as input with interrupt on pin change (IPC)
 
-#define clear_display 						One_wire_Tx_char = 'c';  UART_Tx_1_wire();
 
-//Mini_OS responds to  "Transaction type 'c' by clearing the display
+
+#define clear_display 								One_wire_Tx_char = 'c';  UART_Tx_1_wire();
+#define Reset_ATtiny1606 							One_wire_Tx_char = 'F'; UART_Tx_1_wire();
 
 #define set_up_pin_change_interrupt_on_PC5  		PCICR |= (1 << PCIE1); PCMSK1 |= (1 << PCINT13); 
 #define pause_pin_change_interrupt_on_PC5  			PCICR &= (~(1 << PCIE1)); 
@@ -42,25 +50,19 @@ PORTB &= (~(1 << PORTB4));
 
 //The reset control switch uses PC5 (PCINT13).This sets it up for weak pull up (WPU) with IPC 
 
-#define Reset_ATtiny1606 \
-One_wire_Tx_char = 'F'; UART_Tx_1_wire();
-
-//Mini_OS responds to  "Transaction type 'c' by initialising a watch dog time out
-
-
 
 
 /************************************************************************************************************************************/
 #define Start_clock		    	TCNT0 = 0;  OCR0A = 0; TCCR0B = (1 << CS01);
 
-//Initialses Timer 0 Output compare and counter values to zero, sets TO clock to 1MHz and starts it
+//Initialises Timer 0 Output compare and counter values to zero, sets TO clock to 1MHz and starts it
 
 #define wait_for_comms_tick \
 OCR0A +=  Comms_clock;\
 while (!(TIFR0 & (1 << OCF0A)));\
 TIFR0 = 0xFF;
 
-//Increment Timer 0 Output compare register A and wait untill the value in the Timer 0 counter is the same 
+//Increment Timer 0 Output compare register A and wait until the value in the Timer 0 counter is the same 
 
 
 #define wait_for_half_comms_tick \
@@ -68,16 +70,17 @@ OCR0A =  Comms_clock/2;\
 while (!(TIFR0 & (1 << OCF0A)));\
 TIFR0 = 0xFF;  
 
+
+
+/************************************************************************************************************************************/
 #define boot_reset_ctl_reg			0x3FC
 #define 	set_Run_BL_bit			eeprom_write_byte((uint8_t*)boot_reset_ctl_reg, (eeprom_read_byte((uint8_t*)(0x3FC)) & (~4)));
 
-
-//if the hex_text_programmer reads bit 1 of EPPPROM loation 0x3FC as zero it will run the bootloader
+//if the hex_text_programmer reads bit 2 of EPPPROM location 0x3FC as zero it will run the bootloader
 
 #define PINB4_down	((PINB & 0x10)^0x10)
 #define PINC5_down	((PINC & 0x20)^0x20)
 #define PINC5_up	(PINC & 0x20)
-
 
 
 
@@ -96,4 +99,8 @@ LED_2_off;
 #define Toggle_LED_1 \
 if (PORTB & (1 << PB1)){LED_1_off;}\
 else {PORTB |= (1 << PB1);}
+
+
+
+
 
