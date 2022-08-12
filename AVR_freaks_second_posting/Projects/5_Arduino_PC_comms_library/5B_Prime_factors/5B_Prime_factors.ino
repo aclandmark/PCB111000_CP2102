9cd052415c0356cc2db9ea4e3462dd10217cd654
aclandmark <mark.osborne15@btinternet.com>
2022-08-12 17:14:20 +0100

    Update 5B_Prime_factors.ino

--- AVR_freaks_second_posting/Projects/5_Arduino_PC_comms_library/5B_Prime_factors/5B_Prime_factors.ino
+++ AVR_freaks_second_posting/Projects/5_Arduino_PC_comms_library/5B_Prime_factors/5B_Prime_factors.ino
@@ -54,7 +54,8 @@ long L=0;
 char test_array[12];
 
 while(1){
-for(long k =0; k < n; k++) {search_array[k] = k+1+n*L;}
+for(long k =0; k < n; k++) 
+{search_array[k] = k+1+n*L;}
 prime_no_generator(L,n,search_array); 
 j=0; while(search_array[j] == 0)j += 1;
 while (j<=200)
@@ -64,20 +65,22 @@ if(L*200 > number)return 0;
 L += 1;Serial.write('.');}}
 
 
-
-void prime_no_generator(long L, long n,  long reg[]){
-long i, m;
-i=2; 
-while (i*i < n*(L+1)){
+/************************************************************************************************************/
+void prime_no_generator
+(long L, long n,  long reg[]){                  //n the array size is 200, I is the array number (zero for the first one)
+  
+long i;                                         //We compute the product i * m
+long m;                                        //i always starts at 2 then increments to 3, 4, etc.
+i=2;                                           //m is set to take us to the beginning of the array 
+while (i*i < n*(L+1)){                         //Maximum value i can take
+//wdr();
 m=0;
-while(i*(i+m) <= n*L)m++; 
-while(i*(i+m) <= n*(1+L)){reg[i*(i+m) -1 - n*L] = 0; m++; wdr();}  
+while(i*(i+m) <= n*L)m++;                      //Go to start of the new array for example
+while(i*(i+m) <= n*(1+L))                      //2*101, 2*102, 2*103,... etc   3*67,3*68, 3*69,...etc    5*41, 5*42,...etc
+{reg[i*(i+m) -1 - n*L] = 0; m++; wdr();}      //set appropriate registers to zero (Note the offset of 1 place) 
 i++;}}
 
 
 
 
-
-
-
-/******************************************************************************************/
+/************************************************************************************************************/
