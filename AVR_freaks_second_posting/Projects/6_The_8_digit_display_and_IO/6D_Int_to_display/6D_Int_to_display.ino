4a78332a154cb62ff6f5c3941458493c4f21b39a
aclandmark <mark.osborne15@btinternet.com>
2022-08-13 16:40:36 +0100

    Update 6D_Int_to_display.ino

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_Int_to_display/6D_Int_to_display.ino
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_Int_to_display/6D_Int_to_display.ino
@@ -25,22 +25,23 @@ setup_328_HW_Arduino_IO;
 
 Serial.write("\r\nEnter positive number \
 & terminate with Return key.\r\n");
-Num_1 = Int_KBD_to_display(digits);                                         //Acquires data from keyboard
+Num_1 = Int_KBD_to_display(digits);                                  //Acquires data from keyboard
 
 do{
 Serial.print(++counter); Serial.write('\t');
 Serial.print(Num_1); newline; 
 
-Int_num_to_display(Num_1);                                                    //Sends number to the display
+Int_num_to_display(Num_1);                                           //Sends number to the display
 
 waitforkeypress_A();
 
-Num_1 = (Num_1 / 2) *3;} while (Num_1 < 66666666);                            //Do some arithmetic
+Num_1 = (Num_1 / 2) *3;} while (Num_1 < 66666666);                   //Do some arithmetic
 
 Num_1 = (Num_1 / 3) *2; 
 
-do{Num_1 = (Num_1 / 3) *2;                                                   //Do the arithmetic in reverse
-Serial.print(--counter); Serial.write('\t');Serial.print(Num_1); 
+do{Num_1 = (Num_1 / 3) *2;                                           //Do the arithmetic in reverse
+Serial.print(--counter); Serial.write('\t');
+Serial.print(Num_1); 
 newline;                                             
 Int_num_to_display(Num_1);
 waitforkeypress_A();}while (counter-1);
@@ -62,7 +63,7 @@ cr_keypress = 0;                                                    //Set to one
 for(int n = 0; n<=8; n++) display_buffer[n] = 0;                    //Clear the buffer used for the string
 
 while(1){
-if ((keypress = wait_for_return_key_A()) == '\r')break;               //Detect return key press (i.e \r or\r\n)
+if ((keypress = wait_for_return_key_A()) == '\r')break;             //Detect return key press (i.e \r or\r\n)
 
 if (decimal_digit(keypress))                                        //Ignore non decimal keypresses excepr cr.
 {
@@ -70,10 +71,10 @@ if (decimal_digit(keypress))                                        //Ignore non
 display_buffer[n] = display_buffer[n-1];
 display_buffer[0] = keypress;  }                                    //Add new keypress           
 
-Int_num_string_to_display;}}                                              //Update display includes "cr_keypress"                                                 
+Int_num_string_to_display;}}                                        //Update display includes "cr_keypress"                                                 
 
 cr_keypress = 1;                                                    //End of string; return key press detected
-Int_num_string_to_display;                                                //Send cr keypress
+Int_num_string_to_display;                                          //Send cr keypress
 cr_keypress = 0;
 Int_from_mini_OS;
 return Long_Num_from_mini_OS;}
