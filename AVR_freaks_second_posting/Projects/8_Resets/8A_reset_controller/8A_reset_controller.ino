951911cd2ec090c138266dc0bb34117a503b10cb
aclandmark <mark.osborne15@btinternet.com>
2022-08-22 20:24:54 +0100

    Update 8A_reset_controller.ino

--- AVR_freaks_second_posting/Projects/8_Resets/8A_reset_controller/8A_reset_controller.ino
+++ AVR_freaks_second_posting/Projects/8_Resets/8A_reset_controller/8A_reset_controller.ino
@@ -1,5 +1,13 @@
 
+/*
+ Program developed to check options for the reset function
+ The bootloader reads the MCUSR (Micro controller status register) to identify resets due to POR and WDT.
+ It uses writes to EEPROM to distinguish between WDT resets due to user programs and programming
+ It also identifies program initiation following programming or pressing r at the p/r/t/D prompt.
+ The reset control switch uses the WDT and EEPROM to trigger a SW reset.
 
+Post reset the micro reads the EEPROM to determine its source.
+ */
 
 #include "8A_reset_controller_header.h"
 
@@ -40,12 +48,12 @@ ISR(PCINT2_vect){
 if (switch_1_up)return;
   sei();
   disable_PCI_on_sw1;
-if((switch_2_up) && (switch_3_up));
-if((switch_2_down) && (switch_3_up)){Signal_flagged_WDTout;}
-if((switch_2_up) && (switch_3_down))while(1); 
-if((switch_2_down) && (switch_3_down)){cli();while(1);}
+if((switch_2_up) && (switch_3_up));                                             //Followed by SW_reset
+if((switch_2_down) && (switch_3_up)){Signal_flagged_WDTout;}                    //Followed by SW_reset
+if((switch_2_up) && (switch_3_down))while(1);                                   //WDT times out with interrupt                                 
+if((switch_2_down) && (switch_3_down)){cli();while(1);}                         //WDT times out but no interrupt is raised
 
-while(switch_1_down)wdr();
+while(switch_1_down)wdr();                                                      //Wait for switch to be released
 
 SW_reset;}
 
@@ -58,5 +66,8 @@ void Char_to_PC_as_binary(unsigned char num){
 Serial.write("\t\t");
 for(int m = 0; m <= 7; m++)
 {if (num & (1 << (7 - m)))Serial.write('1'); else Serial.write('0');}Serial.write("\t\t");}
+
+
+
 
 /*******************************************************************************************************************/
