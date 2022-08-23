315fa9b176a57d2ad9878dd28d6715819cca6f24
aclandmark <mark.osborne15@btinternet.com>
2022-08-23 21:23:12 +0100

    Update 8B_Pulse_train_generator.ino

--- AVR_freaks_second_posting/Projects/8_Resets/8B_Pulse_train_generator/8B_Pulse_train_generator.ino
+++ AVR_freaks_second_posting/Projects/8_Resets/8B_Pulse_train_generator/8B_Pulse_train_generator.ino
@@ -27,11 +27,11 @@ for(int p = 0; p <10; p++)newline; break;
 
 case 5:Serial.write("\r\n\r\nNumerical result too large for a 32 bit number.\r\n");     //WDTout with interrupt
 case 2:                                                                                   //SW_reset
-case 3: enable_PCI_on_sw1;                                                                //Post prtD
- eeprom_write_byte((uint8_t*)(0x0),'1');
- eeprom_write_byte((uint8_t*)(0x1),45); 
+case 3: //enable_PCI_on_sw1;                                                                //Post prtD
+ eeprom_write_byte((uint8_t*)(0x0),'1');                                                  //1 to 9 gives mark space ratio
+ eeprom_write_byte((uint8_t*)(0x1),45);                                                   //Waveform period < 65536
  eeprom_write_byte((uint8_t*)(0x2),0); 
- eeprom_write_byte((uint8_t*)(0x3),30); 
+ eeprom_write_byte((uint8_t*)(0x3),30);                                                   //Number of harmonics
  eeprom_write_byte((uint8_t*)(0x4),0);  
  
 Serial.write("\r\nEnter scientific number \
@@ -40,28 +40,40 @@ Serial.write("\r\nEnter scientific number \
 Num_1 = Float_KBD_to_display(digits); 
 float_to_EEPROM(Num_1, 0x5);
 
-while(1){if((switch_2_down) || (switch_3_down))break; else wdr();}break;}
+while(1){if((switch_2_down) || (switch_3_down))break; else wdr();}
+enable_PCI_on_sw1;break;}                                                                 //Press switch 2 or 3 to start
 
 
 duty_cycle = (float)(eeprom_read_byte((uint8_t*)(0x0))- '0')/10.0 * 1.5;
-num_time_slots = (eeprom_read_byte((uint8_t*)(0x2)) << 8) + eeprom_read_byte((uint8_t*)(0x1));
-num_harmonics = (eeprom_read_byte((uint8_t*)(0x4)) << 8) + eeprom_read_byte((uint8_t*)(0x3));
+num_time_slots = (eeprom_read_byte((uint8_t*)(0x2)) << 8) +
+eeprom_read_byte((uint8_t*)(0x1));
+num_harmonics = (eeprom_read_byte((uint8_t*)(0x4)) << 8) +
+eeprom_read_byte((uint8_t*)(0x3));
 
-for(int n = 0; n <= 100; n++){if (n<40)Serial.write (' '); else Serial.write('_');}
+for(int n = 0; n <= 100; n++){if (n<40)Serial.write (' '); 
+else Serial.write('_');}
 newline;
 
 
-{int counter = 0;
-while(counter < 25)
-{for(int m = 1; m; m++)
-{amplitude = sin(duty_cycle ) * (cos(pi * (float)m/(float)num_time_slots));
+{int counter = 0;                                               
+while(counter < 25)                                                           //Exit after 25 cycles without interrupt
+{for(int m = 1; m; m++)                                                       //Exit when m is zero
+
+{amplitude = sin(duty_cycle ) *
+(cos(pi * (float)m/(float)num_time_slots));
 for(int p = 3; p < num_harmonics; p+=2) 
-{amplitude += sin(duty_cycle * (float)p) * cos(pi*(float)p * (float)m/(float)num_time_slots)/(float) p;wdr();}
+
+{amplitude += sin(duty_cycle * (float)p) * cos(pi*(float)p
+* (float)m/(float)num_time_slots)/(float) p; 
+wdr();}
+
 print_spaces = 50 + (int)(100.0 * amplitude);
 for (int n = 0; n < print_spaces; n++){Serial.write(' ');}
 Serial.write('|');_delay_ms(20);
 newline;wdr();
+
 if(m == num_time_slots/2) m = -num_time_slots/2;} counter += 1;}}
+
 
 Serial.write("\r\nTimed out: Reset initiated\r\n");
 setup_watchdog; SW_reset;}
@@ -77,17 +89,20 @@ ISR(PCINT2_vect){
   disable_PCI_on_sw1;
  
 if((switch_2_up) && (switch_3_up)){
-  if(Serial.available()){eeprom_write_byte((uint8_t*)(0x0),Serial.read());}}
+  if(Serial.available()){eeprom_write_byte
+  ((uint8_t*)(0x0),Serial.read());}}                                    //Set duty cycle
   
- if((switch_2_down) && (switch_3_up)){data = PCI_triggers_data_from_PC(digits);
+ if((switch_2_down) && (switch_3_up))
+ {data = PCI_triggers_data_from_PC(digits);                             //Set period
  eeprom_write_byte((uint8_t*)(0x1),data);
  eeprom_write_byte((uint8_t*)(0x2), (data >> 8));}
 
-if((switch_3_down) && (switch_2_up)){ data = PCI_triggers_data_from_PC(digits);
- eeprom_write_byte((uint8_t*)(0x3),data);
+if((switch_3_down) && (switch_2_up))
+{ data = PCI_triggers_data_from_PC(digits);
+ eeprom_write_byte((uint8_t*)(0x3),data);                                //Set number of harmonics
  eeprom_write_byte((uint8_t*)(0x4), (data >> 8));}
 
-if((switch_2_down) && (switch_3_down)){  
+if((switch_2_down) && (switch_3_down)){                                 //Do some arithmetic
 Num_1 = float_from_EEPROM(0x5);
 Num_2 = pow(Num_1, 1.2);
 if(Num_2 == Num_1)while(1);
@@ -98,7 +113,7 @@ float_to_EEPROM (Num_2, 0x5);
 Timer_T1_sub_with_interrupt(T1_delay_250ms);
 return;}
 
-Signal_flagged_WDTout; 
+Signal_flagged_WDTout;                                                   //Return after requesting new waveform
 setup_watchdog; SW_reset;}
   
 
